234f952ae77c0cd8013548a53c7025876c5c2d32
BjornRo <54636059+BjornRo@users.noreply.github.com>
2023-12-03 16:17:24 +0100

    day3 done, fancy solution

--- day3.py
+++ day3.py
@@ -1,21 +1,21 @@
-# Part 1
 def help(i: int, s: str, digits = []) -> list:
     row = []
     index = i * 1000 - 1
-    for j in f".{s}.":
+    for j in s:
         if j.isdigit():
             digits.append(j)
             continue
         elif digits and not j.isdigit():
             index += 1
-            row.append(*[(index,int("".join(digits)) for x in range(len(digits)))])
+            row += [(index, int("".join(digits))) for _ in range(len(digits))]
             digits.clear()
         row.append(j)
     return row
 
+# Part 1
 with open("d3.txt", "rt") as f:
-    print(sum(dict((c for q in (j[1][l-1:l+2] for j in (m[i-1:i+2] for m in [[help(p, k) for p,k in enumerate("".join(z) for z in zip(*(["."] + x + ["."] for x in map(list,zip(*"".join(map(lambda x: x if x.isdigit() or x in (".","\n") else "*",f.read())).strip().split("\n"))))))]] for i in range(1,len(m)-1)) for l in range(1,len(j[0])-2) if (j[0][l] == "*" or j[1][l] == "*" or j[2][l] == "*")) for c in q if not isinstance(c, str))).values()))
+    print(sum(dict((c for q in (j[1][l-1:l+2] for j in (m[i-1:i+2] for m in [[help(p, k) for p,k in enumerate(map(lambda x: f".{x}.",("".join(z) for z in zip(*(["."] + x + ["."] for x in map(list,zip(*"".join(map(lambda x: x if x.isdigit() or x in (".","\n") else "*",f.read())).strip().split("\n"))))))))]] for i in range(1,len(m)-1)) for l in range(1,len(j[0])-2) if (j[0][l] == "*" or j[1][l] == "*" or j[2][l] == "*")) for c in q if not isinstance(c, str))).values()))
 
 # Part 2
 with open("d3.txt", "rt") as f:
-    print(sum(list(e.values())[0]*list(e.values())[1] for e in (dict(list(filter(lambda t: isinstance(t, tuple), j[0][l-1:l+2])) + list(filter(lambda t: isinstance(t, tuple), j[1][l-1:l+2])) + list(filter(lambda t: isinstance(t, tuple), j[2][l-1:l+2]))) for j in (m[i-1:i+2] for m in [[help(p, k) for p,k in enumerate("".join(z) for z in zip(*(["."] + x + ["."] for x in map(list,zip(*"".join(map(lambda x: x if x.isdigit() or x in (".","\n") else "*",f.read())).strip().split("\n"))))))]] for i in range(1,len(m)-1)) for l in range(1,len(j[0])-2) if j[1][l] == "*") if len(e) == 2))
+    print(sum(list(e.values())[0]*list(e.values())[1] for e in (dict(list(filter(lambda t: isinstance(t, tuple), j[0][l-1:l+2])) + list(filter(lambda t: isinstance(t, tuple), j[1][l-1:l+2])) + list(filter(lambda t: isinstance(t, tuple), j[2][l-1:l+2]))) for j in (m[i-1:i+2] for m in [[help(p, k) for p,k in enumerate(map(lambda x: f".{x}.",("".join(z) for z in zip(*(["."] + x + ["."] for x in map(list,zip(*"".join(map(lambda x: x if x.isdigit() or x in (".","\n") else "*",f.read())).strip().split("\n"))))))))]] for i in range(1,len(m)-1)) for l in range(1,len(j[0])-2) if j[1][l] == "*") if len(e) == 2))
