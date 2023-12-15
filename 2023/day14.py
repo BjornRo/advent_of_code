5fa7a5413716aadee4ebca0e4c264ac3d51b3db9
BjornRo <54636059+BjornRo@users.noreply.github.com>
2023-12-15 03:13:51 +0100

    day14 final final

--- 2023/day14.py
+++ 2023/day14.py
@@ -3,13 +3,11 @@ from time import time as time_it
 start_it = time_it()
 
 
-def rock_it(mat: tuple[tuple[int, ...], ...]) -> tuple[tuple[int, ...], ...]:
-    new_matrix = []
-    rlen = len(mat[0]) - 1
-    for row in mat:
-        stack = []
-        waiting_rocks = []
-        for i in range(rlen, -1, -1):
+def rock_it(matrix: tuple[tuple[int, ...], ...]) -> tuple[tuple[int, ...], ...]:
+    new_matrix, stack, waiting_rocks = [], [], []
+    row_len = len(matrix[0]) - 1
+    for row in matrix:
+        for i in range(row_len, -1, -1):
             c = row[i]
             if c == 2:
                 waiting_rocks.append(c)
@@ -19,14 +17,15 @@ def rock_it(mat: tuple[tuple[int, ...], ...]) -> tuple[tuple[int, ...], ...]:
                 waiting_rocks.clear()
             stack.append(c)
         stack.extend(waiting_rocks)
-        waiting_rocks.clear()
         new_matrix.append(tuple(stack))
+        stack.clear()
+        waiting_rocks.clear()
     return tuple(new_matrix)
 
 
-def count_it(mat: tuple[tuple[int, ...], ...]) -> int:
+def count_it(matrix: tuple[tuple[int, ...], ...]) -> int:
     total = 0
-    for row in mat:
+    for row in matrix:
         for i, j in enumerate(row, 1):
             if j == 2:
                 total += i
@@ -36,19 +35,19 @@ def count_it(mat: tuple[tuple[int, ...], ...]) -> int:
 trans_it = lambda x: tuple(zip(*x))
 
 
-def recycle_it(mat: tuple[tuple[int, ...], ...]) -> int:
+def recycle_it(matrix: tuple[tuple[int, ...], ...]) -> int:
     index, cycling = 0, {}
     while True:
         index += 1
-        mat = trans_it(rock_it(trans_it(rock_it(trans_it(rock_it(trans_it(rock_it(mat))))))))
-        if mat in cycling and (1_000_000_000 - index) % (index - cycling[mat]) == 0:
-            return count_it(row[::-1] for row in mat)  # type: ignore - Generators are ok
-        cycling[mat] = index
+        matrix = trans_it(rock_it(trans_it(rock_it(trans_it(rock_it(trans_it(rock_it(matrix))))))))
+        if matrix in cycling and (1_000_000_000 - index) % (index - cycling[matrix]) == 0:
+            return count_it(row[::-1] for row in matrix)  # type: ignore - Generators are ok
+        cycling[matrix] = index
 
 
 with open("in/d14.txt") as f:
-    raw_mat = trans_it((0 if c == "#" else 1 if c == "." else 2 for c in x.rstrip()) for x in f)
+    raw_matrix = trans_it((0 if c == "#" else 1 if c == "." else 2 for c in x.rstrip()) for x in f)
 
-print("Part 1:", count_it(rock_it(raw_mat)))
-print("Part 2:", recycle_it(raw_mat))
+print("Part 1:", count_it(rock_it(raw_matrix)))
+print("Part 2:", recycle_it(raw_matrix))
 print("Finished in:", round(time_it() - start_it, 4), "secs")
