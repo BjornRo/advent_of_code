8d9b6030e073306b7f52105c9135407bb1ab92af
BjornRo <54636059+BjornRo@users.noreply.github.com>
2023-12-18 01:34:56 +0100

    day17, statebased solution sucked

---
+++ 2023/day17.py
@@ -0,0 +1,65 @@
+from enum import IntEnum, auto
+from queue import PriorityQueue
+
+with open("in/d17.txt") as f:
+    p = [0]  # padding
+    raw_mat = tuple(zip(*(p + list(y) + p for y in zip(*(p + [int(c) for c in x.rstrip()] + p for x in f)))))
+
+
+class Dir(IntEnum):
+    UP = auto()
+    DOWN = auto()
+    LEFT = auto()
+    RIGHT = auto()
+
+
+END = (len(raw_mat) - 2, len(raw_mat[0]) - 2)
+PathSum = Row = Col = Steps = int
+State = tuple[Row, Col, Dir, Steps]
+
+
+def crucial(max_steps: int, part2: bool):
+    visited = set()
+    queue: PriorityQueue[tuple[PathSum, State]] = PriorityQueue()
+    start: list[State] = [(1, 1, Dir.RIGHT, 1), (1, 1, Dir.DOWN, 1)]
+    for s in start:
+        queue.put((0, s))
+
+    while not queue.empty():
+        path_sum, (row, col, dir, steps) = queue.get()
+
+        if (row, col) == END:
+            if part2 and steps < 4:
+                continue
+            return path_sum
+
+        if (row, col, dir, steps) in visited:
+            continue
+        visited.add((row, col, dir, steps))
+
+        for i in Dir:
+            if i == {Dir.UP: Dir.DOWN, Dir.DOWN: Dir.UP, Dir.LEFT: Dir.RIGHT, Dir.RIGHT: Dir.LEFT}[dir]:
+                continue
+            _row, _col = row, col
+            match i:
+                case Dir.UP:
+                    _row -= 1
+                case Dir.DOWN:
+                    _row += 1
+                case Dir.LEFT:
+                    _col -= 1
+                case Dir.RIGHT:
+                    _col += 1
+            if index_value := raw_mat[_row][_col]:
+                _steps = 1
+                if i == dir:
+                    if steps >= max_steps:
+                        continue
+                    _steps = steps + 1
+                elif part2 and i != dir and steps < 4:
+                    continue
+                queue.put((path_sum + index_value, (_row, _col, i, _steps)))
+
+
+print("Part 1:", crucial(max_steps=3, part2=False))
+print("Part 2:", crucial(max_steps=10, part2=True))
---
+++ 2023/day17p1state.py
@@ -0,0 +1,59 @@
+from enum import IntEnum, auto
+
+with open("in/d17.txt") as f:
+    p = [0]  # padding
+    raw_mat = tuple(zip(*(p + list(y) + p for y in zip(*(p + [int(c) for c in x.rstrip()] + p for x in f)))))
+
+
+class Dir(IntEnum):
+    UP = auto()
+    DOWN = auto()
+    LEFT = auto()
+    RIGHT = auto()
+
+
+END = (len(raw_mat) - 2, len(raw_mat[0]) - 2)
+
+PathSum = int
+Path = list[tuple[int, int]]
+State = tuple[int, int, Dir, int]
+
+finished_value: int | float = float("inf")
+current_states: dict[State, PathSum] = {((1, 1, Dir.RIGHT, 1)): 0, ((1, 1, Dir.DOWN, 1)): 0}
+next_states: dict[State, PathSum] = {}
+
+while current_states:
+    for ((row, col, dir, steps)), path_sum in current_states.items():
+        if (row, col) == END:
+            finished_value = min(path_sum, finished_value)
+            continue
+        if path_sum >= finished_value:
+            continue
+        for i in Dir:
+            if i == {Dir.UP: Dir.DOWN, Dir.DOWN: Dir.UP, Dir.LEFT: Dir.RIGHT, Dir.RIGHT: Dir.LEFT}[dir]:
+                continue
+            _row, _col = row, col
+            match i:
+                case Dir.UP:
+                    _row -= 1
+                case Dir.DOWN:
+                    _row += 1
+                case Dir.LEFT:
+                    _col -= 1
+                case Dir.RIGHT:
+                    _col += 1
+            index_value = raw_mat[_row][_col]
+            if index_value:
+                if i == dir:
+                    if steps >= 3:
+                        continue
+                    _steps = steps + 1
+                else:
+                    _steps = 1
+                existing_sum = next_states.get((_row, _col, i, _steps))
+                if existing_sum is None or existing_sum > index_value + path_sum:
+                    next_states[(_row, _col, i, _steps)] = index_value + path_sum
+    current_states.clear()
+    current_states.update(sorted(next_states.items(), key=lambda x: x[1]))
+    next_states.clear()
+print(finished_value)
