2b937a9ea33c118f1a37fe487263073745716fe6
BjornRo <54636059+BjornRo@users.noreply.github.com>
2023-12-20 20:35:20 +0100

    day20 simplified

--- 2023/day20.py
+++ 2023/day20.py
@@ -47,42 +47,31 @@ def floppyness() -> dict[Name, FlipFlop | Conjunct]:
     return modules
 
 
-def tmi(presses: int) -> int:
-    modules, queue, low_high_count = floppyness(), deque(), [0, 0]
-    for _ in range(presses):
-        low_high_count[0] += 1 + len(broadcaster)
-        for cmd in broadcaster:
-            queue.append(cmd)
-            modules[cmd].recv("", False)
-        while queue and (cmd := queue.popleft()):
-            for next_cmd in onlycast[cmd][1]:
-                low_high_count[modules[cmd].state] += 1
-                if next_cmd in onlycast and modules[next_cmd].recv(cmd, modules[cmd].state):
-                    queue.append(next_cmd)
-    return low_high_count[0] * low_high_count[1]
-
-
-def bmi() -> int:
+def tmi(part1: int) -> int:
     k = [c for c, (_, i) in onlycast.items() if "rx" in i][0]
-    h, e, l, p = floppyness(), deque(), 0, [0] * sum([k in c for _, (_, c) in onlycast.items()])
+    s, h, e, l, p = [0, 0], floppyness(), deque(), 0, [0] * sum([k in c for _, (_, c) in onlycast.items()])
     if (c := h[k]) and isinstance(c, Conjunct) and (d := c.inputs):
         while l := l + 1:
+            s[0] += 1 + len(broadcaster)
             for cmd in broadcaster:
                 e.append(cmd)
                 h[cmd].recv("", False)
             while e and (cmd := e.popleft()):
                 for next_cmd in onlycast[cmd][1]:
+                    s[h[cmd].state] += 1
                     if next_cmd in onlycast:
                         if h[next_cmd].recv(cmd, h[cmd].state):
                             e.append(next_cmd)
-                        if next_cmd == k and any(d.values()):
+                        if not part1 and next_cmd == k and any(d.values()):
                             for i, v in enumerate(d.values()):
                                 if v and not p[i]:
                                     p[i] = l
                                     if all(p):
                                         return lcm(*p)
-    assert False
+            if part1 and l == 1000:
+                break
+    return s[0] * s[1]
 
 
 print("Part 1:", tmi(1000))
-print("Part 2:", bmi())
+print("Part 2:", tmi(0))
