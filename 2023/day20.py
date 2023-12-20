c958a020cd86227af05886cfbf72e6981dd83fe1
BjornRo <54636059+BjornRo@users.noreply.github.com>
2023-12-20 20:37:31 +0100

    day20 simplified

--- 2023/day20.py
+++ 2023/day20.py
@@ -33,34 +33,30 @@ class FlipFlop:
         return False
 
 
-def floppyness() -> dict[Name, FlipFlop | Conjunct]:
-    modules: dict[Name, FlipFlop | Conjunct] = {}
+def tmi(part1: int) -> int:
+    m: dict[Name, FlipFlop | Conjunct] = {}
     to_inverters: list[tuple[Name, Name]] = []
     for cmd, (pfx, cmds) in onlycast.items():
-        modules[cmd] = FlipFlop() if pfx == "%" else Conjunct()
+        m[cmd] = FlipFlop() if pfx == "%" else Conjunct()
         for next_cmd in cmds:
             if next_cmd in onlycast and onlycast[next_cmd][0] == "&":
                 to_inverters.append((next_cmd, cmd))
     for conj, cmd in to_inverters:
-        if (c := modules[conj]) and isinstance(c, Conjunct):
+        if (c := m[conj]) and isinstance(c, Conjunct):
             c.inputs[cmd] = False
-    return modules
-
-
-def tmi(part1: int) -> int:
     k = [c for c, (_, i) in onlycast.items() if "rx" in i][0]
-    s, h, e, l, p = [0, 0], floppyness(), deque(), 0, [0] * sum([k in c for _, (_, c) in onlycast.items()])
-    if (c := h[k]) and isinstance(c, Conjunct) and (d := c.inputs):
+    h, e, l, p = [0, 0], deque(), 0, [0] * sum([k in c for _, (_, c) in onlycast.items()])
+    if (c := m[k]) and isinstance(c, Conjunct) and (d := c.inputs):
         while l := l + 1:
-            s[0] += 1 + len(broadcaster)
+            h[0] += 1 + len(broadcaster)
             for cmd in broadcaster:
                 e.append(cmd)
-                h[cmd].recv("", False)
+                m[cmd].recv("", False)
             while e and (cmd := e.popleft()):
                 for next_cmd in onlycast[cmd][1]:
-                    s[h[cmd].state] += 1
+                    h[m[cmd].state] += 1
                     if next_cmd in onlycast:
-                        if h[next_cmd].recv(cmd, h[cmd].state):
+                        if m[next_cmd].recv(cmd, m[cmd].state):
                             e.append(next_cmd)
                         if not part1 and next_cmd == k and any(d.values()):
                             for i, v in enumerate(d.values()):
@@ -70,7 +66,7 @@ def tmi(part1: int) -> int:
                                         return lcm(*p)
             if part1 and l == 1000:
                 break
-    return s[0] * s[1]
+    return h[0] * h[1]
 
 
 print("Part 1:", tmi(1000))
