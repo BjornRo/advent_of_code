757460e41b671d3c7a9350fa4b1a599c4bb3579e
BjornRo <54636059+BjornRo@users.noreply.github.com>
2023-12-22 02:24:22 +0100

    cleaned up

--- 2023/day21.py
+++ 2023/day21.py
@@ -1,47 +1,24 @@
-import math
-from dataclasses import dataclass
-from heapq import heappop, heappush
-
 with open("in/d21.txt") as f:
     _p = [-1]  # padding
     _g = zip(*(_p + [1 if c == "." else 2 if c == "S" else 0 for c in x.strip()] + _p for x in f))
     chart: tuple[tuple[int, ...], ...] = tuple(zip(*(_p + list(y) + _p for y in _g)))
 
 
-Row = int
-Col = int
-Coords = tuple[Row, Col]
-
-
-@dataclass
-class Queue:
-    q: list  # = field(default_factory=list)
-
-    def push(self, item) -> None:
-        heappush(self.q, item)
-
-    def pop(self) -> tuple:
-        return heappop(self.q)
-
-    def replace(self, list: list):
-        self.q *= 0
-        self.q.extend(list)
-
-
 def find_nodes(graph: tuple[tuple[int, ...], ...] | list[list[int]], steps: int):
-    start: Coords = next(((i, j) for i, r in enumerate(graph) for j, s in enumerate(r) if s == 2))
+    start = next(((i, j) for i, r in enumerate(graph) for j, s in enumerate(r) if s == 2))
     visited = {start: 0}
-    current_epoch = Queue([start])
-    next_epoch = Queue([])
+    current_epoch = [start]  # Since each epoch only moves certain steps
+    next_epoch = []  # stack/queue does not matter for this type of bfs.
     for s in range(1, steps + 1):
-        while current_epoch.q:
+        while current_epoch:
             x, y = current_epoch.pop()
             for new_xy in (x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1):
                 if graph[new_xy[0]][new_xy[1]] >= 1 and new_xy not in visited:
                     visited[new_xy] = s
-                    next_epoch.push(new_xy)
-        current_epoch.replace(next_epoch.q)
-        next_epoch.q *= 0
+                    next_epoch.append(new_xy)
+        current_epoch *= 0
+        current_epoch.extend(next_epoch)
+        next_epoch *= 0
     return visited
 
 
@@ -57,18 +34,18 @@ def expand_chart(graph):
 
 
 def find_nodex(graph: tuple[tuple[int, ...], ...] | list[list[int]], steps: int):
-    start: Coords = next(((i, j) for i, r in enumerate(graph) for j, s in enumerate(r) if s == 2))
+    start = next(((i, j) for i, r in enumerate(graph) for j, s in enumerate(r) if s == 2))
     mrow, mcol = len(graph) - 1, len(graph[0]) - 1
-    visited = {(start, 0, 0): 0}
-    current_epoch = Queue([(start, 0, 0)])
-    next_epoch = Queue([])
+    visited = {(start, *[0] * 2): 0}
+    current_epoch = [(start, 0, 0)]
+    next_epoch = []
     for s in range(1, steps + 1):
-        while current_epoch.q:
+        while current_epoch:
             (x, y), dimx, dimy = current_epoch.pop()
             for new_xy in (x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1):
-                dimxx, dimyy = dimx, dimy
                 if not graph[new_xy[0]][new_xy[1]]:
                     continue  # wall
+                dimxx, dimyy = dimx, dimy
                 if graph[new_xy[0]][new_xy[1]] == -1:
                     xx, yy = new_xy
                     if xx == 0:
@@ -86,35 +63,34 @@ def find_nodex(graph: tuple[tuple[int, ...], ...] | list[list[int]], steps: int)
                     new_xy = (xx, yy)
                 if (new_xy, dimxx, dimyy) not in visited:
                     visited[(new_xy, dimxx, dimyy)] = s
-                    next_epoch.push((new_xy, dimxx, dimyy))
-        current_epoch.replace(next_epoch.q)
-        next_epoch.q *= 0
+                    next_epoch.append((new_xy, dimxx, dimyy))
+        current_epoch *= 0
+        current_epoch.extend(next_epoch)
+        next_epoch *= 0
     return visited
 
 
-def fn(graph: tuple[tuple[int, ...], ...] | list[list[int]]):
-    start: Coords = next(((i, j) for i, r in enumerate(graph) for j, s in enumerate(r) if s == 2))
-    visited = {start: 0}
-    current_epoch = Queue([start])
-    next_epoch = Queue([])
-    s = 0
-    while True:
-        s += 1
-        while current_epoch.q:
-            x, y = current_epoch.pop()
-            for new_xy in (x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1):
-                if graph[new_xy[0]][new_xy[1]] >= 1 and new_xy not in visited:
-                    visited[new_xy] = s
-                    next_epoch.push(new_xy)
-        current_epoch.replace(next_epoch.q)
-        next_epoch.q *= 0
-        if not current_epoch.q:
-            break
-    return visited
-
-
-def e(steps):
-    return sum(1 for y in find_nodex(chart, steps).values() if y % 2 == 0)
-
+e = lambda steps: sum(1 for y in find_nodes(chart, steps).values() if y % 2 == 0)
 
 print("Part 1:", e(64))
+
+
+# def fn(graph: tuple[tuple[int, ...], ...] | list[list[int]]):
+#     start: Coords = next(((i, j) for i, r in enumerate(graph) for j, s in enumerate(r) if s == 2))
+#     visited = {start: 0}
+#     current_epoch = Queue([start])
+#     next_epoch = Queue([])
+#     s = 0
+#     while True:
+#         s += 1
+#         while current_epoch.q:
+#             x, y = current_epoch.pop()
+#             for new_xy in (x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1):
+#                 if graph[new_xy[0]][new_xy[1]] >= 1 and new_xy not in visited:
+#                     visited[new_xy] = s
+#                     next_epoch.push(new_xy)
+#         current_epoch.replace(next_epoch.q)
+#         next_epoch.q *= 0
+#         if not current_epoch.q:
+#             break
+#     return visited
