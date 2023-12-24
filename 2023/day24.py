98d524f475d5e9d3d767047e1ad7a242f789f01d
BjornRo <54636059+BjornRo@users.noreply.github.com>
2023-12-24 19:33:15 +0100

    day24 simplify b4 try part2

--- 2023/day24.py
+++ 2023/day24.py
@@ -1,4 +1,4 @@
-from decimal import Decimal
+import math
 
 with open("in/d24.txt") as f:
     _g = (map(int, x.replace("@", ",").split(",")) for x in f)
@@ -11,8 +11,16 @@ TEST_MIN_XY = 200_000_000_000_000
 TEST_MAX_XY = 400_000_000_000_000
 
 cross_prod_vec3 = lambda u, v: (u[1] * v[2] - u[2] * v[1], u[2] * v[0] - u[0] * v[2], u[0] * v[1] - u[1] * v[0])
-dot_prod = lambda u, v: Decimal(sum(ui * vi for ui, vi in zip(u, v)))
-vec_len = lambda u: Decimal(sum(map(lambda x: x**2, u))).sqrt()
+dot_prod = lambda u, v: sum(ui * vi for ui, vi in zip(u, v))
+vec_len = lambda u: math.sqrt(sum(map(lambda x: x * x, u)))
+
+
+# def is_parallel(a: Isboll, b: Isboll, ignore_z=True, precision=10) -> bool:
+#     da, db = (a[1][:2], b[1][:2]) if ignore_z else (a[1], b[1])
+#     dot = dot_prod(da, db)
+#     len_a = vec_len(da)
+#     len_b = vec_len(db)
+#     return round(abs(dot / (len_a * len_b)), precision) == 1.0
 
 
 def cross_position(a: Isboll, b: Isboll, ignore_z=True) -> tuple[float, ...]:
@@ -21,7 +29,7 @@ def cross_position(a: Isboll, b: Isboll, ignore_z=True) -> tuple[float, ...]:
     pos_a2 = tuple(posi + deltai for posi, deltai in zip(pos_a1, da))
     pos_b2 = tuple(posi + deltai for posi, deltai in zip(pos_b1, db))
 
-    if ignore_z:  # Sarrus rule
+    if ignore_z:
         stack = (pos_a1, pos_a2, pos_b1, pos_b2)
         homo = tuple((*x, 1) for x in stack)
         l1 = cross_prod_vec3(homo[0], homo[1])
@@ -36,28 +44,20 @@ def intersects(a: Isboll, b: Isboll, ignore_z=True) -> bool:
     pos_b, db = (b[0][:2], b[1][:2]) if ignore_z else b
 
     if ignore_z:
-        dx = pos_b[0] - pos_a[0]
-        dy = pos_b[1] - pos_a[1]
         det = db[0] * da[1] - db[1] * da[0]
-        u = (dy * db[0] - dx * db[1]) / det
-        v = (dy * da[0] - dx * da[1]) / det
-        return u >= 0 and v >= 0
-    return True
+        if det != 0:
+            dx = pos_b[0] - pos_a[0]
+            dy = pos_b[1] - pos_a[1]
+            u = (dy * db[0] - dx * db[1]) / det
+            v = (dy * da[0] - dx * da[1]) / det
+            return u >= 0 and v >= 0
+    return False
 
 
-def is_parallel(a: Isboll, b: Isboll, ignore_z=True, precision=10) -> bool:
-    da, db = (a[1][:2], b[1][:2]) if ignore_z else (a[1], b[1])
-    dot = dot_prod(da, db)
-    len_a = vec_len(da)
-    len_b = vec_len(db)
-    return round(abs(dot / (len_a * len_b)), precision) == 1.0
-
-
-def part1(isbollar: tuple[Isboll, ...], test_min_xy: int, test_max_xy: int) -> int:
-    total = 0
+def part1(isbollar: tuple[Isboll, ...], test_min_xy: int, test_max_xy: int, total=0) -> int:
     for i, a in enumerate(isbollar):
         for b in isbollar[i + 1 :]:
-            if not is_parallel(a, b) and intersects(a, b):
+            if intersects(a, b):
                 x, y = cross_position(a, b)
                 if test_min_xy <= x <= test_max_xy and test_min_xy <= y <= test_max_xy:
                     total += 1
