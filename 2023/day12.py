75f2011ecf85c934d5dd4984de7bdb8878f03e26
BjornRo <54636059+BjornRo@users.noreply.github.com>
2023-12-26 23:18:17 +0100

    i have no clue about day12 anymore

--- 2023/day12.py
+++ 2023/day12.py
@@ -2,7 +2,7 @@ cache = {}
 
 
 # I have no clue why this works.
-def nfa(spring: str, records: tuple[int, ...], counter=0):
+def solver(spring: str, records: tuple[int, ...], counter=0):
     if (k := (spring, records, counter)) in cache:
         return cache[k]
     if not spring:
@@ -15,17 +15,17 @@ def nfa(spring: str, records: tuple[int, ...], counter=0):
         case ".":
             if counter:  # There is a counter counting. We have encountered "."
                 if records and records[0] == counter:
-                    return nfa(spring[1:], records[1:], 0)  # Progress to next state if match
+                    return solver(spring[1:], records[1:], 0)  # Progress to next state if match
                 return False
-            return nfa(spring[1:], records, 0)
+            return solver(spring[1:], records, 0)
         case "#":
-            return nfa(spring[1:], records, counter + 1)
+            return solver(spring[1:], records, counter + 1)
         case "?":
-            n = nfa(spring[1:], records, counter + 1)
+            n = solver(spring[1:], records, counter + 1)
             if not counter:
-                n += nfa(spring[1:], records, 0)
+                n += solver(spring[1:], records, 0)
             elif records and records[0] == counter:
-                n += nfa(spring[1:], records[1:], 0)
+                n += solver(spring[1:], records[1:], 0)
             cache[(spring, records, counter)] = n
             return n
     assert False
@@ -37,8 +37,8 @@ with open("in/d12.txt") as f:
         (s.replace("..", ".").replace("..", "."), tuple(map(int, r.split(",")))) for s, r in (x.split() for x in f)
     ):
         cache.clear()
-        p1 += nfa(s, r)
+        p1 += solver(s, r)
         cache.clear()
-        p2 += nfa("?".join([s] * 5), r * 5)
+        p2 += solver("?".join([s] * 5), r * 5)
 print("Part 1:", p1)
 print("Part 2:", p2)
