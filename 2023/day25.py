8c9859861e4f19f69bfdd464cdffe144ecd59831
BjornRo <54636059+BjornRo@users.noreply.github.com>
2023-12-26 18:38:13 +0100

    redid day25

--- 2023/day25.py
+++ 2023/day25.py
@@ -4,31 +4,31 @@ from collections import defaultdict, deque
 graph, count_crossings = defaultdict(list), defaultdict(int)
 with open("in/d25.txt") as f:
     for e1, v in (x.rstrip().split(": ") for x in f):
-        for e2 in v.split(" "):
+        for e2 in v.split():
             graph[e1].append(e2)
             graph[e2].append(e1)
 
 
-def dfs(ignore=False):  # Mutates graph, so it cannot be re-used after.
+def dfs():  # Mutates graph, so it cannot be re-used after.
     visited, queue = set(), deque()
-    for k in graph:
-        visited.add(k)
-        queue.append(k)
-        while queue:
-            node = queue.popleft()
-            for next_node in graph[node]:
-                if next_node not in visited:
-                    visited.add(next_node)
-                    queue.append(next_node)
-                    if not ignore:
-                        count_crossings[tuple(sorted((node, next_node)))] += 1
-        if ignore:
-            return (len(graph) - len(visited)) * len(visited)
-        visited.clear()
-    for (e1, e2), _ in sorted(count_crossings.items(), key=lambda x: x[1])[-3:]:
-        graph[e1].remove(e2)
-        graph[e2].remove(e1)
-    return dfs(True)
+    for ignore in (False, True):
+        for k in graph:
+            visited.add(k)
+            queue.append(k)
+            while queue:
+                node = queue.popleft()
+                for next_node in graph[node]:
+                    if next_node not in visited:
+                        visited.add(next_node)
+                        queue.append(next_node)
+                        if not ignore:
+                            count_crossings[tuple(sorted((node, next_node)))] += 1
+            if ignore:
+                return (len(graph) - len(visited)) * len(visited)
+            visited.clear()
+        for (e1, e2), _ in sorted(count_crossings.items(), key=lambda x: x[1])[-3:]:
+            graph[e1].remove(e2)
+            graph[e2].remove(e1)
 
 
 print("Part 1:", dfs())
