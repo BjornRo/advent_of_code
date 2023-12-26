a09151bf0dd60b63528c1af6b0c1b3ce21b6b4ea
BjornRo <54636059+BjornRo@users.noreply.github.com>
2023-12-26 13:11:33 +0100

    day24 simplified

--- 2023/day24.py
+++ 2023/day24.py
@@ -4,21 +4,19 @@ with open("in/d24.txt") as f:
     ishall: list[tuple[Vec3, Vec3]] = list(([x, y, z], [dx, dy, dz]) for x, y, z, dx, dy, dz in _g)
 
 
-def part1(isbollar: list[tuple[Vec3, Vec3]], test_min: int, test_max: int) -> int:
+def part1(isbollar: list[tuple[Vec3, Vec3]], test_min: int, test_max: int, v=0) -> int:
     def crosses(a: tuple[Vec3, Vec3], b: tuple[Vec3, Vec3]) -> None | tuple[float, float]:
         ((pa1, pa2, _), (da1, da2, _)), ((pb1, pb2, _), (db1, db2, _)) = a, b
         if det := da2 * db1 - da1 * db2:  # Linear dependent vectors equals zero.
             dx, dy = pb1 - pa1, pb2 - pa2
             if (u := (dy * db1 - dx * db2) / det) >= 0 and (dy * da1 - dx * da2) / det >= 0:
                 return (pa1 + da1 * u, pa2 + da2 * u)
-        return None
 
-    return sum(
-        test_min <= p[0] <= test_max and test_min <= p[1] <= test_max
-        for i, a in enumerate(isbollar)
-        for b in isbollar[i + 1 :]
-        if (p := crosses(a, b))
-    )
+    for i, a in enumerate(isbollar):
+        for b in isbollar[i + 1 :]:
+            if (p := crosses(a, b)) and test_min <= p[0] <= test_max and test_min <= p[1] <= test_max:
+                v += 1
+    return v
 
 
 import time
