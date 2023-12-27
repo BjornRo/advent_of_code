18ac097efe2a16ce63b8851cd837ce5846d51c9f
BjornRo <54636059+BjornRo@users.noreply.github.com>
2023-12-27 03:22:41 +0100

    sD

--- 2023/day12.py
+++ 2023/day12.py
@@ -1,43 +1,34 @@
-cache = {}
-
-
-# I have no clue why this works.
-def solver(spring: str, records: tuple[int, ...], counter=0):
+def springtime(spring: str, records: tuple[int, ...], cache: dict, counter=0):  # I have no clue why this works.
     if (k := (spring, records, counter)) in cache:
         return cache[k]
     if not spring:
-        if len(records) > 1 or counter and not records:
+        if len(records) > 1 or (counter and not records):
             return False  # Too many records left or there is a counter but no records
-        if not counter and not records:  # No counter, No records. We are done
+        if not (counter or records):  # No counter, No records. We are done
             return True
         return records[0] == counter  # One last record, and this is the last check
     match spring[0]:
         case ".":
             if counter:  # There is a counter counting. We have encountered "."
                 if records and records[0] == counter:
-                    return solver(spring[1:], records[1:], 0)  # Progress to next state if match
+                    return springtime(spring[1:], records[1:], cache, 0)  # Progress to next state if match
                 return False
-            return solver(spring[1:], records, 0)
-        case "#":
-            return solver(spring[1:], records, counter + 1)
+            return springtime(spring[1:], records, cache, 0)
         case "?":
-            n = solver(spring[1:], records, counter + 1)
+            n = springtime(spring[1:], records, cache, counter + 1)
             if not counter:
-                n += solver(spring[1:], records, 0)
+                n += springtime(spring[1:], records, cache, 0)
             elif records and records[0] == counter:
-                n += solver(spring[1:], records[1:], 0)
+                n += springtime(spring[1:], records[1:], cache, 0)
             cache[(spring, records, counter)] = n
             return n
-    assert False
+    return springtime(spring[1:], records, cache, counter + 1)
 
 
 p1 = p2 = 0
 with open("in/d12.txt") as f:
     for s, r in ((s, tuple(map(int, r.split(",")))) for s, r in (x.split() for x in f)):
         s = s.replace("..", ".").replace("..", ".")
-        cache.clear()
-        p1 += solver(s, r)
-        cache.clear()
-        p2 += solver("?".join([s] * 5), r * 5)
+        p1, p2 = p1 + springtime(s, r, {}), p2 + springtime("?".join([s] * 5), r * 5, {})
 print("Part 1:", p1)
 print("Part 2:", p2)
