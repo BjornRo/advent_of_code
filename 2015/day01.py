fe2fc694fe81a8de0aeb54c3337499f565f03307
BjornRo <54636059+BjornRo@users.noreply.github.com>
2023-12-28 00:42:11 +0100

    bored, 2015 day1

---
+++ 2015/day01.py
@@ -0,0 +1,8 @@
+with open("in/1.txt") as f:
+    n = j = 0
+    for i, c in enumerate(f.read().rstrip(), 1):
+        n += 1 if c == "(" else -1
+        if not j and n < 0:
+            j = i
+    print("Part 1:", n)
+    print("Part 2:", j)
