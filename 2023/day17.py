f1b44258438dd96ffa6c0c64e5ff9dcfdc86a7d9
BjornRo <54636059+BjornRo@users.noreply.github.com>
2023-12-27 15:16:26 +0100

    clean

--- 2023/day17.py
+++ 2023/day17.py
@@ -7,58 +7,48 @@ with open("in/d17.txt") as f:
 
 
 class Dir(IntEnum):
-    UP = auto()
-    DOWN = auto()
-    LEFT = auto()
-    RIGHT = auto()
+    U = auto()
+    D = auto()
+    L = auto()
+    R = auto()
 
 
-END = (len(raw_mat) - 2, len(raw_mat[0]) - 2)
 PathSum = Row = Col = Steps = int
-State = tuple[Row, Col, Dir, Steps]
+END, State = ((len(raw_mat) - 2, len(raw_mat[0]) - 2)), tuple[Row, Col, Dir, Steps]
 
 
 def crucial(max_steps: int, part2: bool):
+    queue: list[tuple[PathSum, State]] = [(0, (1, 1, Dir.R, 1)), (0, (1, 1, Dir.D, 1))]
     visited = set()
-    queue: list[tuple[PathSum, State]] = []
-    start: list[State] = [(1, 1, Dir.RIGHT, 1), (1, 1, Dir.DOWN, 1)]
-    for s in start:
-        heappush(queue, (0, s))
-
     while queue:
         path_sum, (row, col, dir, steps) = heappop(queue)
-
         if (row, col) == END:
             if part2 and steps < 4:
                 continue
             return path_sum
-
-        if (row, col, dir, steps) in visited:
-            continue
-        visited.add((row, col, dir, steps))
-
-        for i in Dir:
-            if i == {Dir.UP: Dir.DOWN, Dir.DOWN: Dir.UP, Dir.LEFT: Dir.RIGHT, Dir.RIGHT: Dir.LEFT}[dir]:
-                continue
-            _row, _col = row, col
-            match i:
-                case Dir.UP:
-                    _row -= 1
-                case Dir.DOWN:
-                    _row += 1
-                case Dir.LEFT:
-                    _col -= 1
-                case Dir.RIGHT:
-                    _col += 1
-            if index_value := raw_mat[_row][_col]:
-                _steps = 1
-                if i == dir:
-                    if steps >= max_steps:
-                        continue
-                    _steps = steps + 1
-                elif part2 and i != dir and steps < 4:
-                    continue
-                heappush(queue, (path_sum + index_value, (_row, _col, i, _steps)))
+        if (k := (row, col, dir, steps)) not in visited:
+            visited.add(k)
+            for i in Dir:
+                if i != {Dir.U: Dir.D, Dir.D: Dir.U, Dir.L: Dir.R, Dir.R: Dir.L}[dir]:
+                    _row, _col = row, col
+                    match i:
+                        case Dir.U:
+                            _row -= 1
+                        case Dir.D:
+                            _row += 1
+                        case Dir.L:
+                            _col -= 1
+                        case Dir.R:
+                            _col += 1
+                    if index_value := raw_mat[_row][_col]:
+                        _steps = 1
+                        if i == dir:
+                            if steps >= max_steps:
+                                continue
+                            _steps = steps + 1
+                        elif part2 and i != dir and steps < 4:
+                            continue
+                        heappush(queue, (path_sum + index_value, (_row, _col, i, _steps)))
 
 
 print("Part 1:", crucial(max_steps=3, part2=False))
