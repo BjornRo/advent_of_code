0d7eb4b954519021eb0da8ef577408ee25fa3090
BjornRo <54636059+BjornRo@users.noreply.github.com>
2023-12-28 14:20:59 +0100

    day4

--- 2015/day04.py
+++ 2015/day04.py
@@ -1,12 +1,14 @@
 from hashlib import md5
 
-input, found = "bgvyzdsv", False
+with open("in/d4.txt") as f:
+    string = f.read().strip()
+
+found = False
 for i in range(10_000_000):
-    a, b, c = md5((input + str(i)).encode()).digest()[:3]
+    a, b, c = md5((string + str(i)).encode()).digest()[:3]
     if not a and not b and not c:
         print("Part 2:", i)
         break
     if not found and not a and not b and 15 >= c:
         print("Part 1:", i)
-        print(md5((input + str(i)).encode()).digest())
         found = True
