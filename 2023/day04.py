824c8065b7a7e633530d6f7c44436e0387fcc2cf
BjornRo <54636059+BjornRo@users.noreply.github.com>
2023-12-28 22:12:17 +0100

    forgot 2

--- 2023/day04.py
+++ 2023/day04.py
@@ -1,5 +1,5 @@
 # Part 1
-with open("d4.txt", "rt") as f:
+with open("in/d4.txt", "rt") as f:
     print("Part 1:", sum((1 << (len(v+h) - len(set(v+h)))) // 2 for v,h in list(map(lambda x: list(map(lambda y: list(filter(lambda z: z, y.strip().split(" "))),x.split(":")[-1].strip().split("|"))), f.read().strip().split("\n")))))
 
 # Part 2
@@ -10,4 +10,4 @@ with open("d4.txt", "rt") as f:
     for i,j in enumerate(len(v+h) - len(set(v+h)) for v,h in map(s, a)):
         for k in range(j):
             d[i+k+1] += d[i]
-    print("Part :", sum(d))
+    print("Part 2:", sum(d))
