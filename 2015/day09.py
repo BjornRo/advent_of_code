25c6f40b8237e8d437f56b5b7a90bb4de6e295fd
BjornRo <54636059+BjornRo@users.noreply.github.com>
2023-12-28 23:18:04 +0100

    day9 done

---
+++ 2015/day09.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+graph = defaultdict(dict)
+with open("in/d9.txt") as f:
+    for toto, weight in (x.rstrip().split(" = ") for x in f):
+        src, dst = toto.split(" to ")
+        graph[src][dst] = int(weight)
+        graph[dst][src] = int(weight)
+
+
+def galaxytrotter(graph: dict[str, dict[str, int]], max_min):
+    all_places, stack, route_length = set(graph), [], float("inf") if max_min.__name__ == "min" else 0
+    for place in graph:
+        if place not in graph:
+            continue
+        stack.append((place, set(), 0))
+        while stack:
+            place, visited, cost = stack.pop()
+            if place in visited:
+                continue
+            visited.add(place)
+            if visited == all_places:
+                route_length = max_min(route_length, cost)
+                continue
+            for next_place, weight in graph[place].items():
+                stack.append((next_place, visited.copy(), weight + cost))
+    return route_length
+
+
+print("Part 1:", galaxytrotter(graph, min))
+print("Part 2:", galaxytrotter(graph, max))
