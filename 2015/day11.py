9f97e3580a2ea2c3790e3a677f0a38e3aac5abeb
BjornRo <54636059+BjornRo@users.noreply.github.com>
2023-12-29 14:43:12 +0100

    day11

--- 2015/day11.py
+++ 2015/day11.py
@@ -1,3 +1,43 @@
-with open("in/d10.txt") as f:
+def valid_pw(pw: bytes) -> bool:
+    if (blen := len(pw)) == 8:
+        if not (pw[blen - 2] in {105, 108, 111} or pw[blen - 1] in {105, 108, 111}):
+            asc, twice = pw[blen - 1] - pw[blen - 2] == 1 and pw[blen - 2] - pw[blen - 3] == 1, pw[0] == pw[1]
+            flip = twice
+            for i in range(blen - 2):
+                if pw[i] in {105, 108, 111}:
+                    return False
+                if not asc and pw[i + 2] - pw[i + 1] == 1 and pw[i + 1] - pw[i] == 1:
+                    asc = True
+                if not flip and pw[i + 1] == pw[i + 2]:
+                    twice += 1
+                    flip = True
+                elif flip:
+                    flip = False
+            return asc and twice >= 2
+    return False
+
+
+def pw_generator(old_pw: str, skip=False):  # a:97, z:122
+    pw, plen, last = bytearray(old_pw.encode()), len(old_pw), len(old_pw) - 1
+    if plen != 8:
+        assert False
+    while not valid_pw(pw) or skip:
+        skip = False
+        pw[last] += 1
+        if not (97 <= pw[last] <= 122):  # We are outside the range
+            pw[last] = 97  # Reset last index to "a"
+            pw[last - 1] += 1  # Overflow to next
+            for j in range(last - 1, -1, -1):
+                if 97 <= pw[j] <= 122:
+                    break
+                if j != 0:
+                    pw[j - 1] += 1
+                pw[j] = 97
+    return pw.decode()
+
+
+with open("in/d11.txt") as f:
     for l in (x.strip() for x in f):
-        pass
+        new_pw = pw_generator(l)
+        print("Part 1:", new_pw)
+        print("Part 2:", pw_generator(new_pw, True))
