65fdb226da38a94a2f17da3b71470f85d3327306
BjornRo <54636059+BjornRo@users.noreply.github.com>
2023-12-29 02:07:32 +0100

    s

--- 2015/day04.py
+++ 2015/day04.py
@@ -3,12 +3,12 @@ from hashlib import md5
 with open("in/d4.txt") as f:
     string = f.read().strip()
 
-found = False
+found = False # just to stop printing part1.
 for i in range(10_000_000):
     a, b, c = md5((string + str(i)).encode()).digest()[:3]
-    if not a and not b and not c:
-        print("Part 2:", i)
-        break
     if not found and not a and not b and 15 >= c:
         print("Part 1:", i)
         found = True
+    if not a and not b and not c:
+        print("Part 2:", i)
+        break
---
+++ 2015/day11.py
@@ -0,0 +1,3 @@
+with open("in/d10.txt") as f:
+    for l in (x.strip() for x in f):
+        pass
