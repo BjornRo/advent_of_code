8c2bef2b86a0a67a9fc23c02f8946577ec7a49e8
BjornRo <54636059+BjornRo@users.noreply.github.com>
2023-12-30 02:42:41 +0100

    d

--- 2015/day15.py
+++ 2015/day15.py
@@ -9,7 +9,7 @@ part1 = part2 = 0
 for comb in (combo for combo in it.permutations(range(1, 101), len(chemicals[0])) if sum(combo) == 100):
     tmp = math.prod(max(0, sum(x * y for x, y in zip(comb, c))) for c in chemicals[:-1])
     part1 = max(tmp, part1)
-    if max(sum(x * y for x, y in zip(comb, chemicals[-1])), 0) == 500:
+    if sum(x * y for x, y in zip(comb, chemicals[-1])) == 500:
         part2 = max(tmp, part2)
 print("Part 1:", part1)
 print("Part 2:", part2)
