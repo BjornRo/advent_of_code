e1442ac05d450623ba0b79352cd6e3fb7ea8afc1
BjornRo <54636059+BjornRo@users.noreply.github.com>
2023-12-30 02:58:31 +0100

    s

--- 2015/day15.py
+++ 2015/day15.py
@@ -6,7 +6,7 @@ with open("in/d15.txt") as f:
     chemicals = tuple(zip(*(map(int, re.findall(r"-?\d+", s)) for s in f)))
 
 part1 = part2 = 0
-for comb in (combo for combo in it.permutations(range(1, 101), len(chemicals[0])) if sum(combo) == 100):
+for comb in (c for c in it.product(range(1, 102 - len(chemicals[0])), repeat=len(chemicals[0])) if sum(c) == 100):
     tmp = math.prod(max(0, sum(x * y for x, y in zip(comb, c))) for c in chemicals[:-1])
     part1 = max(tmp, part1)
     if sum(x * y for x, y in zip(comb, chemicals[-1])) == 500:
