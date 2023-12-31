d536b2981e6324220aaad0daab9f6549a017ef4d
BjornRo <54636059+BjornRo@users.noreply.github.com>
2023-12-31 15:57:30 +0100

    s

--- 2015/day21.py
+++ 2015/day21.py
@@ -1,5 +1,5 @@
 import itertools as it
-from re import findall as fa
+from re import findall
 
 _weapons = """
 Dagger        8     4       0
@@ -24,27 +24,20 @@ Defense +2   40     0       2
 Defense +3   80     0       3
 """
 
-parse = lambda ix: {(b, c): a for x in ix.strip().split("\n") for a, b, c in [list(map(int, fa(r"\s\d+", x)))]}
+parse = lambda ix: {(b, c): a for x in ix.strip().split("\n") for a, b, c in [list(map(int, findall(r"\s\d+", x)))]}
 wps, ars, rgs = parse(_weapons), parse(_armors) | {(0, 0): 0}, parse(_rings) | {(0, 0): 0}
 
 with open("in/d21.txt") as f:
     bhp, bdmg, barm = (int(x.split(": ")[1]) for x in f.read().strip().split("\n"))
 
-
-def arena(least_gold=float("inf"), max_gold=0):
-    for w, a, (r1, r2) in it.product(wps, ars, it.permutations((*rgs, (0, 0)), 2)):
-        (fdmg, farm), gold = map(sum, zip(*(w, a, r1, r2))), wps[w] + ars[a] + rgs[r1] + rgs[r2]
-        freal_dmg, breal_dmg = max(0, fdmg - barm), max(0, bdmg - farm)
-        if freal_dmg:
-            fhp, bohp = 100, bhp
-            while True:
-                if (bohp := bohp - freal_dmg) <= 0:
-                    least_gold = min(least_gold, gold)
-                    break
-                if (fhp := fhp - breal_dmg) <= 0:
-                    max_gold = max(max_gold, gold)
-                    break
-    return int(least_gold), max_gold
-
-
-print("Part 1: {}\nPart 2: {}".format(*arena()))
+least_gold, max_gold = 1 << 32, 0
+for w, a, (r1, r2) in it.product(wps, ars, it.permutations((*rgs, (0, 0)), 2)):
+    (fdmg, farm) = map(sum, zip(*(w, a, r1, r2)))
+    freal_dmg, breal_dmg, fhp, bohp = fdmg - barm, bdmg - farm, 100, bhp
+    while fhp > 0 and bohp > 0:
+        if (bohp := bohp - freal_dmg) <= 0:
+            least_gold = min(least_gold, wps[w] + ars[a] + rgs[r1] + rgs[r2])
+        if (fhp := fhp - breal_dmg) <= 0:
+            max_gold = max(max_gold, wps[w] + ars[a] + rgs[r1] + rgs[r2])
+print("Part 1:", least_gold)
+print("Part 2:", max_gold)
