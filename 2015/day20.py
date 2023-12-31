dcc0f14f77e413e714bdf294c9cf1c75f0fea86e
BjornRo <54636059+BjornRo@users.noreply.github.com>
2023-12-31 09:51:59 +0100

    d20

---
+++ 2015/day20.py
@@ -0,0 +1,23 @@
+from math import sqrt
+
+
+def divisors(n: int) -> list[int]:
+    d1 = [i for i in range(1, int(sqrt(n)) + 1) if n % i == 0]
+    return d1 + [n // d for d in d1 if n != d**2]
+
+
+with open("in/d20.txt") as f:
+    num = int(f.read().strip())
+
+part1 = part2 = 0
+for i in range(1, num // 18):
+    divs = divisors(i)
+    if not part1:
+        if sum(divs) * 10 >= num:
+            part1 = i
+    elif not part2 and sum(d for d in divs if i // d <= 50) * 11 >= num:
+        part2 = i
+        break
+
+print("Part 1:", part1)
+print("Part 2:", part2)
