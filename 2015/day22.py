e1acc3442809bc9b30fa1950e7b15dac7d743be0
BjornRo <54636059+BjornRo@users.noreply.github.com>
2023-12-31 17:13:02 +0100

    day22 start

--- 2015/day22.py
+++ 2015/day22.py
@@ -2,10 +2,32 @@ from dataclasses import dataclass, field
 
 
 @dataclass
-class Spell:
-    dmg: int
-    mana: int
-    ticks: int
+class Shield:
+    mana = 113
+    ticks = 6
+    curr_tick = 0
+
+    def active(self) -> bool:
+        self.curr_tick += 1
+        return self.curr_tick != self.ticks
+
+
+@dataclass
+class Recharge:
+    mana = 229
+    ticks = 5
+    curr_tick = 0
+
+    def active(self) -> bool:
+        self.curr_tick += 1
+        return self.curr_tick != self.ticks
+
+
+@dataclass
+class Poison:
+    dmg = 3
+    mana = 173
+    ticks = 6
     curr_tick = 0
 
     def tick(self) -> None | int:
@@ -17,22 +39,39 @@ class Spell:
 @dataclass
 class Boss:
     hp: int
-    curse: list[Spell] = field(default_factory=list)
+    curse: None | Poison = None
 
     def alive(self) -> bool:
-        remaining = []
-        while self.curse:
-            c = self.curse.pop()
+        if c := self.curse:
             if dmg := c.tick():
                 self.hp -= dmg
-                remaining.append(c)
-        self.curse.extend(remaining)
+            else:
+                self.curse = None
         return self.hp > 0
+
 
 @dataclass
 class Character:
     hp: int
+    mana: int
     shield = 0
+    actives: list[Shield | Recharge] = field(default_factory=list)
+
+    def alive(self) -> bool:
+        remaining = []
+        while self.actives:
+            c = self.actives.pop()
+            res = c.active()
+            match c:
+                case Shield():
+                    self.shield = 7 if res else 0
+                case Recharge():
+                    self.mana += 101 if res else 0
+            if res:
+                remaining.append(c)
+        self.actives.extend(remaining)
+        return self.hp > 0
+
 
 with open("in/d22.txt") as f:
     hp, dmg = (int(x.split()[-1]) for x in f)
