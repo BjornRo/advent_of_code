5d862399456c111918a60c290b1914e10aa3439d
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-01-02 00:37:52 +0100

    d25

--- 2015/day24.py
+++ 2015/day24.py
@@ -1,7 +1,3 @@
-with open("in/d24.txt") as f:
-    numbers = list(map(int, f.read().split()))
-
-
 def find_qe(group_size: int, nums: list[int], min_qe: list[int], qe: int = 1, counter: int = 0) -> int:
     if counter == group_size:
         min_qe[0] = qe
@@ -12,6 +8,8 @@ def find_qe(group_size: int, nums: list[int], min_qe: list[int], qe: int = 1, co
     return min_qe[0]
 
 
+with open("in/d24.txt") as f:
+    numbers = list(map(int, f.read().split()))
 group_size = sum(numbers)
 print("Part 1:", find_qe(group_size // 3, numbers, [1 << 64]))
 print("Part 2:", find_qe(group_size // 4, numbers, [1 << 64]))
