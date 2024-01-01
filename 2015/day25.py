e120a741d7d355fba24e1ff6c3342ce61a4da30d
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-01-02 00:21:38 +0100

    d25 done

--- 2015/day25.py
+++ 2015/day25.py
@@ -4,6 +4,6 @@ with open("in/d25.txt") as f:
     row, col = map(int, re.findall(r"\d+", f.read().strip()))
 
 next_code, arit_sum, c = lambda number: (number * 252533) % 33554393, lambda n: (n * (n + 1)) // 2, 20151125
-for i in range(arit_sum(row + col - 2) + col - 1):
+for _ in range(arit_sum(row + col - 2) + col - 1):
     c = next_code(c)
 print("Part 1:", c)
