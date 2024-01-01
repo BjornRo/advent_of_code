1cf90f3e7d461f3cc087710201fbceaefd368c43
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-01-01 23:20:16 +0100

    optimized d24

--- 2015/day24.py
+++ 2015/day24.py
@@ -2,16 +2,15 @@ with open("in/d24.txt") as f:
     numbers = list(map(int, f.read().split()))
 
 
-def find_qe(group_size: int, nums: list[int], min_qe: list[int], qe: int = 1, counter: int = 0):
+def find_qe(group_size: int, nums: list[int], min_qe: list[int], qe: int = 1, counter: int = 0) -> int:
     if counter == group_size:
         if qe < min_qe[0]:
             min_qe[0] = qe
-        return
+        return min_qe[0]
 
     for n in range(len(nums)):
-        if (new_counter := counter + nums[n]) <= group_size:
-            if (new_qe := nums[n] * qe) < min_qe[0]:
-                find_qe(group_size, nums[n + 1 :], min_qe, new_qe, new_counter)
+        if (new_counter := counter + nums[n]) <= group_size and (new_qe := nums[n] * qe) < min_qe[0]:
+            find_qe(group_size, nums[n + 1 :], min_qe, new_qe, new_counter)
     return min_qe[0]
 
 
