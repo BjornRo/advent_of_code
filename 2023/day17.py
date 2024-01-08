cb53164df5ef96fb98af8c00e5a625720cb8c976
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-01-08 11:50:31 +0100

    slight optimization

--- 2023/day17.py
+++ 2023/day17.py
@@ -1,17 +1,17 @@
 from heapq import heappop, heappush
 
 with open("in/d17.txt") as f:
-    p = lambda x: (0, *x, 0)  # Padding
-    raw_mat = tuple(zip(*(p(y) for y in zip(*(p(int(c) for c in x.rstrip()) for x in f)))))
+    _p = lambda x: (0, *x, 0)  # Padding
+    raw_mat = tuple(zip(*(_p(y) for y in zip(*(_p(int(c) for c in x.rstrip()) for x in f)))))
 
 
-PathSum = Row = Col = Steps = int
-END, State = ((len(raw_mat) - 2, len(raw_mat[0]) - 2)), tuple[Row, Col, int, Steps]
+PathSum = Row = Col = Steps = int  #  Up, Down, Left, Right
+END, DIRS, State = ((len(raw_mat) - 2, len(raw_mat[0]) - 2)), {0: 1, 1: 0, 2: 3, 3: 2}, tuple[Row, Col, int, Steps]
 
 
 def crucial(max_steps: int, part2: bool):
     queue: list[tuple[PathSum, State]] = [(0, (1, 1, 3, 1)), (0, (1, 1, 1, 1))]
-    visited, m = set(), {0: 1, 1: 0, 2: 3, 3: 2}  #  Up, Down, Left, Right
+    visited = set()
     while queue:
         path_sum, (row, col, dir, steps) = heappop(queue)
         if (row, col) == END:
@@ -20,8 +20,8 @@ def crucial(max_steps: int, part2: bool):
             return path_sum
         if (k := (row, col, dir, steps)) not in visited:
             visited.add(k)
-            for i in m:
-                if i != m[dir]:  # No 180s
+            for i in DIRS:
+                if i != DIRS[dir]:  # No 180s
                     _row, _col = row, col
                     match i:
                         case 0:
