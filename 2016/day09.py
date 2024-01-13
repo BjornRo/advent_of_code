87e9fcd901b8be05514b0da23c53d053403f96ca
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-01-14 00:43:18 +0100

    d9 cleaned up

--- 2016/day09.py
+++ 2016/day09.py
@@ -1,6 +1,5 @@
-def expand_data(comp_data: str, part2: bool, total=0, i=0, start_paren=False):
-    comp_len, data = len(comp_data), ""
-    while i < comp_len:
+def expand_data(comp_data: str, part2: bool, total=0, i=0, start_paren=False, data=""):
+    while i < len(comp_data):
         if comp_data[i] == ")":
             x, y = map(int, data.split("x"))
             total += (expand_data(comp_data[i + 1 : i + x + 1], part2) if part2 else x) * y
