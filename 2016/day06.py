7dfb2eabaa074911ad0b02c9c651adf1b7a736b9
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-01-13 17:05:26 +0100

    d6

--- 2016/day06.py
+++ 2016/day06.py
@@ -1,7 +1,7 @@
 from collections import Counter
 
 with open("in/d6.txt") as f:
-    hidden_msg = tuple(Counter(col).most_common() for col in (zip(*(x.rstrip() for x in f))))
+    p1, p2 = zip(*((c[0][0], c[-1][0]) for c in (Counter(y).most_common() for y in (zip(*(x.rstrip() for x in f))))))
 
-print("Part 1:", "".join(c[0][0] for c in hidden_msg))
-print("Part 2:", "".join(c[-1][0] for c in hidden_msg))
+print("Part 1:", "".join(p1))
+print("Part 2:", "".join(p2))
