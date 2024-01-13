a387710c7b8f3d5c00e144ae17ebe7a8b7fb94f4
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-01-13 13:39:34 +0100

    d4

--- 2016/day04.py
+++ 2016/day04.py
@@ -1,2 +1,17 @@
+from collections import Counter
+
+
+def roomizer(wroom, total=0, pid=-1):
+    for r, _rid, chksm in wroom:
+        r, rid = r.replace("-", ""), int(_rid)
+        if pid == -1 and "northpole" in "".join(chr((ord(c) + rid - 97) % 26 + 97) for c in r):
+            pid = rid
+        if "".join([*zip(*Counter(sorted(r)).most_common(5))][0]) == chksm:
+            total += rid
+    return total, pid
+
+
 with open("in/d4.txt") as f:
-    wroom = tuple(tuple(r.rstrip()[:-1].replace("[", "-").split("-")) for r in f)
+    total, pid = roomizer(r.rstrip()[:-1].replace("[", "-").rsplit("-", 2) for r in f)
+    print("Part 1:", total)
+    print("Part 2:", pid)
