e6c81ca018a0147af8eb185231bdf7d977156ac4
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-01-13 10:18:33 +0100

    day2 done

--- 2016/day02.py
+++ 2016/day02.py
@@ -3,7 +3,7 @@ with open("in/d2.txt") as f:
 
 
 def mission_improbable(keypad: tuple[tuple[str, ...], ...]):
-    pos, code = next((i, j) for i, row in enumerate(keypad) for j, col in enumerate(row) if col == "5"), ""
+    pos, code = next([i, j] for i, row in enumerate(keypad) for j, col in enumerate(row) if col == "5"), ""
     for move in KEYCODE:
         for key in move:
             row, col = pos
@@ -17,7 +17,7 @@ def mission_improbable(keypad: tuple[tuple[str, ...], ...]):
                 case "R":
                     col += 1
             if keypad[row][col] != "0":
-                pos = (row, col)
+                pos[:] = row, col
         code += keypad[pos[0]][pos[1]]
     return code
 
