fd45b0e61a6d6bcf48d722ba284c0c193b2d2c0f
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-01-13 09:22:01 +0100

    d2

--- 2016/day01.py
+++ 2016/day01.py
@@ -1,19 +1,17 @@
-with open("in/d1.txt") as f:
-    maze = [(y[0] == "R", int(y[1:])) for y in (x.strip() for x in f.read().split(","))]
-
-position, current_turn = complex(0, 0), complex(-1, 0) # facing north
+position, current_turn = complex(0, 0), complex(-1, 0)  # facing north
 visited, part2 = {position}, None
-for r_turn, steps in maze:
-    current_turn *= -1j if r_turn else 1j
-    if part2 is None:
+with open("in/d1.txt") as f:
+    for r_turn, steps in ((y[0] == "R", int(y[1:])) for y in (x.strip() for x in f.read().split(","))):
+        current_turn *= -1j if r_turn else 1j
+        if part2 is not None:
+            position += current_turn * steps
+            continue
         for _ in range(steps):
             position += current_turn
             if position in visited and part2 is None:
                 part2 = position
                 continue
             visited.add(position)
-    else:
-        position += current_turn * steps
 
 assert isinstance(part2, complex)
 complex_sum = lambda x: sum(map(abs, map(int, (x.real, x.imag))))
