547d68c662241520613efff6fe7a24ceca8c6136
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-01-13 21:40:22 +0100

    d9 p1 done

--- 2016/day09.py
+++ 2016/day09.py
@@ -18,6 +18,11 @@ while i < MAX_LEN:
 print("Part 1", total)
 
 # Part 2
+"""
+(27x12)(20x12)(13x14)(7x10)(1x12)A
+12*12*14*10*12 * (A) = 241920
+Parse len of 27(first), then "collapse"
+"""
 data, start_paren, total, i = "", False, 0, 0
 while i < MAX_LEN:
     if compressed[i] == ")":
