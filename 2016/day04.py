a2dabeeedda514e8ab488ab301debffa8605d77d
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-01-13 16:44:52 +0100

    d5

--- 2016/day04.py
+++ 2016/day04.py
@@ -4,9 +4,10 @@ from collections import Counter
 def roomizer(wroom, total=0, pid=-1):
     for r, _rid, chksm in wroom:
         r, rid = r.replace("-", ""), int(_rid)
-        if pid == -1 and "northpole" in "".join(chr((ord(c) + rid - 97) % 26 + 97) for c in r):
+        key = rid - 97
+        if pid == -1 and "northpole" in "".join(chr((ord(c) + key) % 26 + 97) for c in r):
             pid = rid
-        if "".join([*zip(*Counter(sorted(r)).most_common(5))][0]) == chksm:
+        if "".join(cf[0] for cf in Counter(sorted(r)).most_common(5)) == chksm:
             total += rid
     return total, pid
 
