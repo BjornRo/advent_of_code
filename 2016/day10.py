cd8eec5ffd520e2146a0449aac5b4828d1cf51ec
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-01-14 17:35:16 +0100

    d10

--- 2016/day10.py
+++ 2016/day10.py
@@ -7,28 +7,24 @@ def botnet(string: str):
         _, value, _, _, _, bot_id = sp_str
         return int(value), int(bot_id)
     _bot, id1, _, _lowto, _, outbot1, id2, _, _highto, _, outbot2, id3 = sp_str  # len 12
-    return int(id1), outbot1[0], int(id2), outbot2[0], int(id3)
+    return int(id1), outbot1[0] == "b", int(id2), outbot2[0] == "b", int(id3)
 
 
 with open("in/d10.txt") as f:
-    queue = deque(sorted((botnet(s) for s in f), key=len))
-chips, outputs, part1 = defaultdict(list[int]), defaultdict(list[int]), None
+    queue, chip, out, p1 = deque(sorted((botnet(s) for s in f), key=len)), defaultdict(list), defaultdict(list), -1
 while queue:
-    i = queue.popleft()
-    match i:
+    match queue.popleft():
         case value, bot_id:
-            chips[bot_id].append(value)
-        case bot_id, outbot1, outbot_id1, outbot2, outbot_id2 if len(vals := chips[bot_id]) == 2:
+            chip[bot_id].append(value)
+        case bot_id, outbot1, outbot_id1, outbot2, outbot_id2 if len(vals := chip[bot_id]) == 2:
             low, high = sorted(vals)
             vals *= 0
             if low == 17 and high == 61:
-                part1 = bot_id
-            for ob, ob_id, val in (outbot1, outbot_id1, low), (outbot2, outbot_id2, high):
-                target = chips if ob == "b" else outputs
-                target[ob_id].append(val)
-        case _:
+                p1 = bot_id
+            for is_bot, ob_id, val in (outbot1, outbot_id1, low), (outbot2, outbot_id2, high):
+                (chip if is_bot else out)[ob_id].append(val)
+        case i:
             queue.append(i)
 
-
-print("Part 1:", part1)
-print("Part 2:", outputs[0][0] * outputs[1][0] * outputs[2][0])
+print("Part 1:", p1)
+print("Part 2:", out[0][0] * out[1][0] * out[2][0])
