afbfba98a4879216ea99e563de40df3ed183e202
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-01-15 23:24:41 +0100

    d14

--- 2016/day14.py
+++ 2016/day14.py
@@ -2,41 +2,38 @@ from binascii import hexlify
 from collections import defaultdict
 from hashlib import md5
 
-with open("in/d14.txt") as f:
-    salty = f.read().rstrip()
-...
+
+def hashinator(part2: bool, valid=0):
+    triples: dict[int, list[tuple[int, int]]] = defaultdict(list)
+    for i in range(1_000_000):
+        hashish = hexlify(md5((SALTY + str(i)).encode()).digest())
+        if part2:
+            for _ in range(2016):
+                hashish = hexlify(md5(hashish).digest())
+        if (val := n_tester(hashish, 3)) != -1:
+            triples[val].append((i, i + 1000))
+        if (val := n_tester(hashish, 5)) != -1:
+            for s, e in list(triples[val]):
+                if i <= e:
+                    if s < i:
+                        valid += 1
+                        if valid == 64:
+                            return s
+                else:
+                    triples[val].remove((s, e))
 
 
-def triple(b: bytes) -> None | int:
-    for i in range(len(b) - 2):
-        if b[i] == b[i + 1] == b[i + 2]:
-            return b[i]
-    return None
-
-
-def quin(b: bytes) -> None | int:
-    for i in range(len(b) - 4):
-        if b[i] == b[i + 1] == b[i + 2] == b[i + 3] == b[i + 4]:
-            return b[i]
-    return None
-
-
-Index, Number = [int] * 2
-
-valid = 0
-triples: dict[Number, list[Index]] = defaultdict(list)
-salty = "abc"
-for i in range(1_000_000_000):
-    hashish = hexlify(md5((salty + str(i)).encode()).digest())
-    if (val := triple(hashish)) is not None:
-        if (valq := quin(hashish)) is not None:
-            for j in triples[valq][:]:
-                if i <= j + 1000:
-                    valid += 1
-                if valid == 64:
-                    print(i)
-                triples[valq].remove(j)
+def n_tester(b: bytes, length: int) -> int:
+    for i in range(len(b) - length + 1):
+        for j in range(1, length):
+            if b[i] != b[i + j]:
+                break
         else:
-            triples[val].append(i)
-    if i > 40004:
-        break
+            return b[i]
+    return -1
+
+
+with open("in/d14.txt") as f:
+    SALTY = f.read().rstrip()
+print("Part 1:", hashinator(part2=False))
+print("Part 2:", hashinator(part2=True))
