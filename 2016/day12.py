c19756ddc30b18c90ea5a1fd30c657c3ceae3055
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-01-15 09:56:04 +0100

    d12 opt

--- 2016/day12.py
+++ 2016/day12.py
@@ -1,5 +1,4 @@
-def assembly(a: int):
-    ins_len, pc, registers = len(instructions), 0, {k: a if k == "c" else 0 for k in "abcd"}
+def assembly(ins_len: int, registers: dict[str, int], pc=0):
     while pc < ins_len:
         match instructions[pc]:
             case "cpy", val, reg:
@@ -8,14 +7,13 @@ def assembly(a: int):
                 registers[reg] += 1
             case "dec", reg:
                 registers[reg] -= 1
-            case "jnz", reg, val:
-                if (reg in registers and registers[reg] != 0) or (reg.isdigit() and int(reg) != 0):
-                    pc += int(val) - 1
+            case "jnz", reg, val if (reg.isdigit() and int(reg) != 0) or (reg in registers and registers[reg] != 0):
+                pc += int(val) - 1
         pc += 1
     return registers["a"]
 
 
 with open("in/d12.txt") as f:
-    instructions = tuple(x.rstrip().split() for x in f)
-print("Part 1:", assembly(0))
-print("Part 2:", assembly(1))
+    instructions = tuple(tuple(x.rstrip().split()) for x in f)
+print("Part 1:", assembly(len(instructions), {k: 0 for k in "abcd"}))
+print("Part 2:", assembly(len(instructions), {k: 1 if k == "c" else 0 for k in "abcd"}))
