1bf396925983be98e21e3847cab0039df63353ff
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-01-15 23:42:59 +0100

    d1

--- 2016/day01.py
+++ 2016/day01.py
@@ -3,15 +3,15 @@ visited, part2 = {position}, None
 with open("in/d1.txt") as f:
     for r_turn, steps in ((y[0] == "R", int(y[1:])) for y in (x.strip() for x in f.read().split(","))):
         current_turn *= -1j if r_turn else 1j
-        if part2 is not None:
+        if part2 is None:
+            for _ in range(steps):
+                position += current_turn
+                if position in visited and part2 is None:
+                    part2 = position
+                    continue
+                visited.add(position)
+        else:
             position += current_turn * steps
-            continue
-        for _ in range(steps):
-            position += current_turn
-            if position in visited and part2 is None:
-                part2 = position
-                continue
-            visited.add(position)
 
 assert isinstance(part2, complex)
 complex_sum = lambda x: sum(map(abs, map(int, (x.real, x.imag))))
