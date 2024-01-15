0a3a92e90a35b06f005c05e2b97f1810d99d4db5
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-01-15 23:47:04 +0100

    d1

--- 2016/day01.py
+++ 2016/day01.py
@@ -1,5 +1,4 @@
-position, current_turn = complex(0, 0), complex(-1, 0)  # facing north
-visited, part2 = {position}, None
+visited, part2, position, current_turn = set(), None, complex(0, 0), complex(-1, 0)  # facing north
 with open("in/d1.txt") as f:
     for r_turn, steps in ((y[0] == "R", int(y[1:])) for y in (x.strip() for x in f.read().split(","))):
         current_turn *= -1j if r_turn else 1j
@@ -13,7 +12,6 @@ with open("in/d1.txt") as f:
         else:
             position += current_turn * steps
 
-assert isinstance(part2, complex)
 complex_sum = lambda x: sum(map(abs, map(int, (x.real, x.imag))))
 print("Part 1:", complex_sum(position))
 print("Part 2:", complex_sum(part2))
