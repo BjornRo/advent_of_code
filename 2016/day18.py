ea876e5f2ec25ea8dfdd7b63f0c25f6fcba7de66
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-01-16 23:50:06 +0100

    d18

--- 2016/day18.py
+++ 2016/day18.py
@@ -1,7 +1,6 @@
-def its_a_bait(rows: int, total: int):
+def its_a_bait(rows: int, total: int, next_trap=[False, False]):
     trapper, (t_len, tless, tr_len) = [False, False, *its_a_trap, False, False], (len(its_a_trap) + i for i in range(3))
     for _ in range(rows - 1):
-        next_trap = [False, False]
         for j in range(1, tless):
             match trapper[j : j + 3]:
                 case [True, True, False] | [False, True, True] | [False, False, True] | [True, False, False]:
@@ -11,6 +10,7 @@ def its_a_bait(rows: int, total: int):
                     if j <= t_len:
                         total += 1
         trapper[2:tr_len] = next_trap[2:tr_len]
+        next_trap[2:] *= 0
     return total
 
 
