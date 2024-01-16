a0a4f26b3b5e3e3659d372255a0d2d656a239942
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-01-16 14:40:42 +0100

    d16 done

--- 2016/day16.py
+++ 2016/day16.py
@@ -1,4 +1,14 @@
-with open("in/d16.txt") as f:
-    binner = f.read().rstrip()
+def curvature(disk_size: int, data: list[bool]):
+    while len(data) < disk_size:
+        data = data + [False] + [not x for x in data[::-1]]
+    data = data[:disk_size]
+    while len(data) % 2 == 0:
+        data = [a == b for a, b in zip(data[::2], data[1::2])]
+    return "".join(map(str, map(int, data)))
 
-disk_size = 272
+
+with open("in/d16.txt") as f:
+    binner = list(map(bool, map(int, f.read().rstrip())))
+
+print("Part 1:", curvature(272, binner))
+print("Part 2:", curvature(35651584, binner))
