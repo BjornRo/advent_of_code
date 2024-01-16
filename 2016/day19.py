7059be63850028bbb47df2c772080e550fe268bd
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-01-17 00:25:28 +0100

    d19 p1 done

--- 2016/day19.py
+++ 2016/day19.py
@@ -1,2 +1,26 @@
+from dataclasses import dataclass
+
 with open("in/d19.txt") as f:
-    dining_philosophers = int(f.read().rstrip())
+    stealing_philosophers = int(f.read().rstrip())
+
+
+@dataclass
+class LinkedPhilosopher:
+    id: int
+    next: "None | LinkedPhilosopher" = None
+
+
+start = LinkedPhilosopher(1)
+curr = start
+for i in range(2, stealing_philosophers + 1):
+    curr.next = LinkedPhilosopher(i)
+    curr = curr.next
+curr.next = start
+curr = start
+
+while True:
+    if curr.id == curr.next.id:  # type:ignore
+        print(curr.id)  # type:ignore
+        break
+    curr.next = curr.next.next  # type:ignore
+    curr = curr.next  # type:ignore
