3be39b22e51f09deb93c184c186b41e1148cc0b9
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-01-16 22:32:53 +0100

    d17 done

--- 2016/day17.py
+++ 2016/day17.py
@@ -5,7 +5,7 @@ from hashlib import md5
 
 def in_the_closet(part2: bool):
     m_step = "" if part2 else ("B" * 1000)
-    q: deque[tuple[int, int, str]] = deque([(1, 1, d) for i, x in enumerate(C("")) if x in O and (d := DIR[i]) in "DR"])
+    q: deque[tuple[int, int, str]] = deque([(1, 1, d) for i, x in enumerate(C("")) if x in E and (d := L[i]) in "DR"])
     while q:
         row, col, steps = q.popleft()
         match steps[-1]:
@@ -17,8 +17,8 @@ def in_the_closet(part2: bool):
                 col -= 1
             case "R":
                 col += 1
-        if GRID[row][col]:
-            if (row, col) == END:
+        if H[row][col]:
+            if (row, col) == P:
                 if len(steps) > len(m_step) if part2 else (len(steps) < len(m_step)):
                     m_step = steps
                 continue
@@ -26,14 +26,13 @@ def in_the_closet(part2: bool):
                 if len(steps) >= len(m_step):
                     continue
             for i, b in enumerate(C(steps)):
-                if b in O:
-                    q.append((row, col, steps + DIR[i]))
+                if b in E:
+                    q.append((row, col, steps + L[i]))
     return m_step
 
 
 with open("in/d17.txt") as f:
-    closeted = f.read().rstrip()
-GRID, O, DIR = tuple((0, *x, 0) for x in zip(*((0, *(1,) * 4, 0) for _ in range(4)))), set(b"bcdef"), "UDLR"
-C, END = lambda x: hexlify(md5((closeted + x).encode()).digest())[:4], (4, 4)
+    C, loseted = lambda x: hexlify(md5((loseted + x).encode()).digest())[:4], f.read().rstrip()
+H, E, L, P = tuple((0, *x, 0) for x in zip(*((0, *(1,) * 4, 0) for _ in range(4)))), set(b"bcdef"), "UDLR", (4, 4)
 print("Part 1:", in_the_closet(False))
 print("Part 2:", len(in_the_closet(True)))
