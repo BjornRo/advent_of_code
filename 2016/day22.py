170f6e24ab93cff4aadecffe05c0ce92be6c92e2
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-01-18 00:33:55 +0100

    d22 p1 done

--- 2016/day22.py
+++ 2016/day22.py
@@ -1,4 +1,15 @@
+from itertools import permutations
+
 with open("in/d22.txt") as f:
     f.readline()
     f.readline()
     fs = [x.rstrip().rsplit("/", 1)[1].replace("node-x", "").replace("-y", " ").split() for x in f]
+
+noodle = lambda row, col, size, use, avail, *_: (int(row), int(col), int(size[:-1]), int(use[:-1]), int(avail[:-1]))
+
+# row_a, col_a, size_a, used_a, avail_a = noodle(*node_a)
+# row_b, col_b, size_b, used_b, avail_b = noodle(*node_b)
+
+
+viability = lambda node_a, node_b: (used := noodle(*node_a)[-2]) and (used <= noodle(*node_b)[-1])
+print("Part 1:", sum(viability(a, b) for a, b in permutations(fs, 2)))
