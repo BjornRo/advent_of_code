ba6ea94747d8dc7fca55412ec1fe47acaa3473fa
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-11-30 17:25:14 +0100

    s

--- 2024/d01.zig
+++ 2024/d01.zig
@@ -1,20 +1,23 @@
 const std = @import("std");
 const myf = @import("myfunc.zig");
 const expect = std.testing.expect;
+const time = std.time;
 
 pub fn main() !void {
+    const start = time.nanoTimestamp();
+    defer {
+        const end = time.nanoTimestamp();
+        const elapsed = @as(f128, @floatFromInt(end - start)) / @as(f128, 1_000_000_000);
+        std.io.getStdOut().writer().print("Time taken: {d:.10}s\n", .{elapsed}) catch {};
+    }
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer if (gpa.deinit() == .leak) expect(false) catch @panic("TEST FAIL");
     const allocator = gpa.allocator();
 
     const filename = try myf.getFirstAppArg(allocator);
-    defer allocator.free(filename);
-
     const target_file = try std.mem.concat(allocator, u8, &.{ "in/", filename });
-    defer allocator.free(target_file);
-
     const input = try myf.readFile(allocator, target_file);
-    defer allocator.free(input);
+    defer inline for (.{ filename, target_file, input }) |res| allocator.free(res);
     // End setup
 
     std.debug.print("{s}\n", .{input});
