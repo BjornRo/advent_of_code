051ca0c7cef2eccfaf081593c7287ec239722ae6
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-11-30 17:28:57 +0100

    boilerplate file

---
+++ 2024/boiler.zig
@@ -0,0 +1,25 @@
+const std = @import("std");
+const myf = @import("myfunc.zig");
+const expect = std.testing.expect;
+const time = std.time;
+
+pub fn main() !void {
+    const start = time.nanoTimestamp();
+    const writer = std.io.getStdOut().writer();
+    defer {
+        const end = time.nanoTimestamp();
+        const elapsed = @as(f128, @floatFromInt(end - start)) / @as(f128, 1_000_000_000);
+        writer.print("Time taken: {d:.10}s\n", .{elapsed}) catch {};
+    }
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer if (gpa.deinit() == .leak) expect(false) catch @panic("TEST FAIL");
+    const allocator = gpa.allocator();
+
+    const filename = try myf.getFirstAppArg(allocator);
+    const target_file = try std.mem.concat(allocator, u8, &.{ "in/", filename });
+    const input = try myf.readFile(allocator, target_file);
+    defer inline for (.{ filename, target_file, input }) |res| allocator.free(res);
+    // End setup
+
+    std.debug.print("{s}\n", .{input});
+}
