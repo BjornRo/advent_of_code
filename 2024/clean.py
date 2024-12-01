4c9bb1558663c7d4d42d3a1294102d516d37e66b
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-12-01 11:31:05 +0100

    added cleaning script

---
+++ 2024/clean.py
@@ -0,0 +1,8 @@
+import os
+
+ignore = {"in", "zig_ver0_13_0"}
+
+for item in os.listdir():
+    if os.path.isfile(item) and item not in ignore:
+        if not (".py" in item or ".zig" in item):
+            os.remove(item)
