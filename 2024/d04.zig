faf28a35c96bf843e4d6dae747d76b50e72606a8
BjornRo <shovitswe@gmail.com>
2024-12-04 14:02:39 +0100

    d4 cleaning up

--- 2024/d04.zig
+++ 2024/d04.zig
@@ -11,17 +11,16 @@ pub fn main() !void {
         const elapsed = @as(f128, @floatFromInt(end - start)) / @as(f128, 1_000_000_000);
         writer.print("\nTime taken: {d:.10}s\n", .{elapsed}) catch {};
     }
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer if (gpa.deinit() == .leak) expect(false) catch @panic("TEST FAIL");
-    const allocator = gpa.allocator();
-    // var buffer: [70_000]u8 = undefined;
-    // var fba = std.heap.FixedBufferAllocator.init(&buffer);
-    // const allocator = fba.allocator();
+    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    // defer if (gpa.deinit() == .leak) expect(false) catch @panic("TEST FAIL");
+    // const allocator = gpa.allocator();
+    var buffer: [23_000]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    const allocator = fba.allocator();
 
     const filename = try myf.getAppArg(allocator, 1);
     const target_file = try std.mem.concat(allocator, u8, &.{ "in/", filename });
     const input = try myf.readFile(allocator, target_file);
-    std.debug.print("input len: {d}\n\n", .{input.len});
     defer inline for (.{ filename, target_file, input }) |res| allocator.free(res);
     // End setup
 
@@ -35,75 +34,69 @@ pub fn main() !void {
     const mat = matrix.items;
     const rows = mat.len;
     const cols = input_attributes.row_len;
-    var p1_sum: u64 = 0;
-    inline for (.{ "XMAS", "SAMX" }) |word| {
-        for (0..rows - 3) |i| {
-            for (0..cols - 3) |j| {
-                if (std.mem.eql(u8, mat[i][j .. j + 4], word)) p1_sum += 1;
+    try writer.print("Part 1: {d}\nPart 2: {d}\n", .{
+        part1(mat, rows, cols),
+        part2(mat, rows, cols),
+    });
+}
 
-                const vert: [4]u8 = .{
-                    mat[i][j],
-                    mat[i + 1][j],
-                    mat[i + 2][j],
-                    mat[i + 3][j],
-                };
-                if (std.mem.eql(u8, &vert, word)) p1_sum += 1;
-                const diag: [4]u8 = .{
-                    mat[i][j],
-                    mat[i + 1][j + 1],
-                    mat[i + 2][j + 2],
-                    mat[i + 3][j + 3],
-                };
-                if (std.mem.eql(u8, &diag, word)) p1_sum += 1;
-                const diag2: [4]u8 = .{
-                    mat[i][j + 3],
-                    mat[i + 1][j + 2],
-                    mat[i + 2][j + 1],
-                    mat[i + 3][j + 0],
-                };
-                if (std.mem.eql(u8, &diag2, word)) p1_sum += 1;
+inline fn part2(mat: [][]const u8, rows: usize, cols: usize) u32 {
+    var sum: u32 = 0;
+    for (0..rows - 2) |i| {
+        for (0..cols - 2) |j| {
+            const diagL = .{ mat[i][j], mat[i + 1][j + 1], mat[i + 2][j + 2] };
+            const diagR = .{ mat[i][j + 2], mat[i + 1][j + 1], mat[i + 2][j] };
+            if ((std.mem.eql(u8, &diagL, "MAS") or std.mem.eql(u8, &diagL, "SAM")) and
+                (std.mem.eql(u8, &diagR, "MAS") or std.mem.eql(u8, &diagR, "SAM"))) sum += 1;
+        }
+    }
+    return sum;
+}
+
+fn part1(mat: [][]const u8, rows: usize, cols: usize) u32 {
+    const F4 = struct {
+        inline fn getRow(m: [][]const u8, i: usize, j: usize) [4]u8 {
+            return .{ m[i][j], m[i][j + 1], m[i][j + 2], m[i][j + 3] };
+        }
+        inline fn getDiagLeft(m: [][]const u8, i: usize, j: usize) [4]u8 {
+            return .{ m[i][j], m[i + 1][j + 1], m[i + 2][j + 2], m[i + 3][j + 3] };
+        }
+        inline fn getDiagRight(m: [][]const u8, i: usize, j: usize) [4]u8 {
+            return .{ m[i][j + 3], m[i + 1][j + 2], m[i + 2][j + 1], m[i + 3][j] };
+        }
+        inline fn getVert(m: [][]const u8, i: usize, j: usize) [4]u8 {
+            return .{ m[i][j], m[i + 1][j], m[i + 2][j], m[i + 3][j] };
+        }
+    };
+
+    var sum: u32 = 0;
+    for (0..rows - 3) |i| {
+        for (0..cols - 3) |j| {
+            inline for (.{
+                F4.getRow(mat, i, j),
+                F4.getDiagLeft(mat, i, j),
+                F4.getDiagRight(mat, i, j),
+                F4.getVert(mat, i, j),
+            }) |arr| {
+                inline for (.{ "XMAS", "SAMX" }) |word| {
+                    if (std.mem.eql(u8, &arr, word)) sum += 1;
+                }
             }
-            for (cols - 3..cols) |k| {
-                const vert: [4]u8 = .{
-                    mat[i][k],
-                    mat[i + 1][k],
-                    mat[i + 2][k],
-                    mat[i + 3][k],
-                };
-                if (std.mem.eql(u8, &vert, word)) p1_sum += 1;
+        }
+        for (cols - 3..cols) |k| {
+            const vert = F4.getVert(mat, i, k);
+            inline for (.{ "XMAS", "SAMX" }) |word| {
+                if (std.mem.eql(u8, &vert, word)) sum += 1;
             }
         }
     }
     for (rows - 3..rows) |i| {
         for (0..cols - 3) |j| {
+            const row = F4.getRow(mat, i, j);
             inline for (.{ "XMAS", "SAMX" }) |word| {
-                // Cannot figure out how to fix runtime/comptime slices/arrays.
-                if (std.mem.eql(u8, mat[i][j .. j + 4], word)) p1_sum += 1;
+                if (std.mem.eql(u8, &row, word)) sum += 1;
             }
         }
     }
-    myf.printAny(p1_sum);
-
-    var p2_sum: u64 = 0;
-    for (0..rows - 2) |i| {
-        for (0..cols - 2) |j| {
-            var x: u8 = 0;
-            inline for (.{ "MAS", "SAM" }) |word| {
-                const diag: [3]u8 = .{
-                    mat[i][j],
-                    mat[i + 1][j + 1],
-                    mat[i + 2][j + 2],
-                };
-                if (std.mem.eql(u8, &diag, word)) x += 1;
-                const diag2: [3]u8 = .{
-                    mat[i][j + 2],
-                    mat[i + 1][j + 1],
-                    mat[i + 2][j],
-                };
-                if (std.mem.eql(u8, &diag2, word)) x += 1;
-            }
-            if (x == 2) p2_sum += 1;
-        }
-    }
-    myf.printAny(p2_sum);
+    return sum;
 }
