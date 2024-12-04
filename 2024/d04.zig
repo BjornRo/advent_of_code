c452f2ef6f576472ba04e8004650602b120ee4b7
BjornRo <shovitswe@gmail.com>
2024-12-04 17:50:57 +0100

    d4, testing samuels idea of bit-foolery

--- 2024/d04.zig
+++ 2024/d04.zig
@@ -3,6 +3,15 @@ const myf = @import("mylib/myfunc.zig");
 const expect = std.testing.expect;
 const time = std.time;
 
+const XMAS = dirtyConvert(u32, .{ 'X', 'M', 'A', 'S' });
+const SAMX = dirtyConvert(u32, .{ 'S', 'A', 'M', 'X' });
+const MAS = dirtyConvert(u24, .{ 'M', 'A', 'S' });
+const SAM = dirtyConvert(u24, .{ 'S', 'A', 'M' });
+
+fn dirtyConvert(comptime T: type, buffer: [@divExact(@typeInfo(T).Int.bits, 8)]u8) T {
+    return @bitCast(buffer);
+}
+
 pub fn main() !void {
     const start = time.nanoTimestamp();
     const writer = std.io.getStdOut().writer();
@@ -40,19 +49,6 @@ pub fn main() !void {
     });
 }
 
-inline fn part2(mat: [][]const u8, rows: usize, cols: usize) u32 {
-    var sum: u32 = 0;
-    for (0..rows - 2) |i| {
-        for (0..cols - 2) |j| {
-            const diagL = .{ mat[i][j], mat[i + 1][j + 1], mat[i + 2][j + 2] };
-            const diagR = .{ mat[i][j + 2], mat[i + 1][j + 1], mat[i + 2][j] };
-            if ((std.mem.eql(u8, &diagL, "MAS") or std.mem.eql(u8, &diagL, "SAM")) and
-                (std.mem.eql(u8, &diagR, "MAS") or std.mem.eql(u8, &diagR, "SAM"))) sum += 1;
-        }
-    }
-    return sum;
-}
-
 fn part1(mat: [][]const u8, rows: usize, cols: usize) u32 {
     const F4 = struct {
         inline fn getRow(m: [][]const u8, i: usize, j: usize) [4]u8 {
@@ -73,29 +69,42 @@ fn part1(mat: [][]const u8, rows: usize, cols: usize) u32 {
     for (0..rows - 3) |i| {
         for (0..cols - 3) |j| {
             inline for (.{
-                F4.getRow(mat, i, j),
-                F4.getDiagLeft(mat, i, j),
-                F4.getDiagRight(mat, i, j),
-                F4.getVert(mat, i, j),
+                dirtyConvert(u32, F4.getRow(mat, i, j)),
+                dirtyConvert(u32, F4.getDiagLeft(mat, i, j)),
+                dirtyConvert(u32, F4.getDiagRight(mat, i, j)),
+                dirtyConvert(u32, F4.getVert(mat, i, j)),
             }) |arr| {
-                inline for (.{ "XMAS", "SAMX" }) |word| {
-                    if (std.mem.eql(u8, &arr, word)) sum += 1;
+                inline for (.{ XMAS, SAMX }) |word| {
+                    if (arr == word) sum += 1;
                 }
             }
         }
         for (cols - 3..cols) |k| {
-            const vert = F4.getVert(mat, i, k);
-            inline for (.{ "XMAS", "SAMX" }) |word| {
-                if (std.mem.eql(u8, &vert, word)) sum += 1;
+            const vert = dirtyConvert(u32, F4.getVert(mat, i, k));
+            inline for (.{ XMAS, SAMX }) |word| {
+                if (vert == word) sum += 1;
             }
         }
     }
     for (rows - 3..rows) |i| {
         for (0..cols - 3) |j| {
-            const row = F4.getRow(mat, i, j);
-            inline for (.{ "XMAS", "SAMX" }) |word| {
-                if (std.mem.eql(u8, &row, word)) sum += 1;
+            const row = dirtyConvert(u32, F4.getRow(mat, i, j));
+            inline for (.{ XMAS, SAMX }) |word| {
+                if (row == word) sum += 1;
             }
+        }
+    }
+    return sum;
+}
+
+inline fn part2(mat: [][]const u8, rows: usize, cols: usize) u32 {
+    var sum: u32 = 0;
+    for (0..rows - 2) |i| {
+        for (0..cols - 2) |j| {
+            const diagL = dirtyConvert(u24, .{ mat[i][j], mat[i + 1][j + 1], mat[i + 2][j + 2] });
+            const diagR = dirtyConvert(u24, .{ mat[i][j + 2], mat[i + 1][j + 1], mat[i + 2][j] });
+            if ((diagL == MAS or diagL == SAM) and
+                (diagR == MAS or diagR == SAM)) sum += 1;
         }
     }
     return sum;
