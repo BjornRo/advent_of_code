8937d92e56328e36d6d8498218da40b8483e7404
BjornRo <shovitswe@gmail.com>
2024-12-06 15:40:38 +0100

    d6 optimize

--- 2024/d06.zig
+++ 2024/d06.zig
@@ -13,12 +13,12 @@ pub fn main() !void {
         const elapsed = @as(f128, @floatFromInt(end - start)) / @as(f128, 1_000_000);
         writer.print("\nTime taken: {d:.7}ms\n", .{elapsed}) catch {};
     }
-    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    // defer if (gpa.deinit() == .leak) expect(false) catch @panic("TEST FAIL");
-    // const allocator = gpa.allocator();
-    var buffer: [550_000]u8 = undefined;
-    var fba = std.heap.FixedBufferAllocator.init(&buffer);
-    const allocator = fba.allocator();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer if (gpa.deinit() == .leak) expect(false) catch @panic("TEST FAIL");
+    const allocator = gpa.allocator();
+    // var buffer: [550_000]u8 = undefined;
+    // var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    // const allocator = fba.allocator();
 
     const filename = try myf.getAppArg(allocator, 1);
     const target_file = try std.mem.concat(allocator, u8, &.{ "in/", filename });
@@ -27,7 +27,7 @@ pub fn main() !void {
     // End setup
 
     const input_attributes = try myf.getDelimType(input);
-    var matrix = std.ArrayList([]const u8).init(allocator);
+    var matrix = std.ArrayList([]u8).init(allocator);
     defer matrix.deinit();
 
     // Imaginary numbers are usually mapped as Y = i, but I patternmatch REAL as Rows instead,..
@@ -40,7 +40,7 @@ pub fn main() !void {
     const cols: i16 = @intCast(input_attributes.row_len);
     var in_iter = std.mem.tokenizeSequence(u8, input, if (input_attributes.delim == .CRLF) "\r\n" else "\n");
     while (in_iter.next()) |row| {
-        try matrix.append(row);
+        try matrix.append(@constCast(row));
         if (std.mem.indexOf(u8, row, "^")) |idx| {
             start_row = rows;
             start_col = @intCast(idx);
@@ -59,6 +59,9 @@ pub fn main() !void {
         inline fn u32Tou16(n: u32) [2]u16 {
             return @bitCast(n);
         }
+        inline fn u32Toi16(n: u32) [2]i16 {
+            return @bitCast(n);
+        }
         inline fn complexTo64(c: ComplexT, d: ComplexT) u64 {
             return @bitCast([4]i16{ c.re, c.im, d.re, d.im });
         }
@@ -67,15 +70,18 @@ pub fn main() !void {
     const rot_right = ComplexT.init(0, -1);
     // // const rot_left = ComplexT.init(0, 1);
 
-    var visited = std.AutoArrayHashMap(u32, void).init(allocator);
+    var visited = std.AutoArrayHashMap(u32, u32).init(allocator);
     defer visited.deinit();
 
     var direction = ComplexT.init(-1, 0); // Going upwards "^"
     var position = ComplexT.init(start_row, start_col);
-    const mat = matrix.items;
+    var mat = matrix.items;
     while (true) {
-        try visited.put(F.complexTo32(position), {});
         const next_pos = position.add(direction);
+        const res = try visited.getOrPut(F.complexTo32(position));
+        if (!res.found_existing) {
+            res.value_ptr.* = F.complexTo32(direction);
+        }
         if (!F.inBounds(next_pos, rows, cols)) break;
 
         const row: u16 = @bitCast(next_pos.re);
@@ -86,17 +92,22 @@ pub fn main() !void {
             position = next_pos;
         }
     }
-    const visited_slice = visited.keys();
     var visited_dir = std.AutoHashMap(u64, void).init(allocator);
     defer visited_dir.deinit();
 
     var p2_sum: u16 = 0;
-    for (visited_slice[1..]) |key| {
+    const visited_slice = visited.keys();
+    const value_slice = visited.values();
+    for (0..visited_slice.len - 1) |i| {
         defer visited_dir.clearRetainingCapacity();
+        const start_r, const start_c = F.u32Toi16(visited_slice[i]);
+        const dir_a, const dir_b = F.u32Toi16(value_slice[i]);
+        const next_r, const next_c = F.u32Tou16(visited_slice[i + 1]);
+        mat[next_r][next_c] = '#';
+        defer mat[next_r][next_c] = '.';
 
-        const i, const j = F.u32Tou16(key);
-        position = ComplexT.init(start_row, start_col);
-        direction = ComplexT.init(-1, 0); // Going upwards "^"
+        position = ComplexT.init(start_r, start_c);
+        direction = ComplexT.init(dir_a, dir_b);
         while (true) {
             const res = try visited_dir.getOrPut(F.complexTo64(position, direction));
             if (res.found_existing) {
@@ -108,7 +119,7 @@ pub fn main() !void {
 
             const row: u16 = @bitCast(next_pos.re);
             const col: u16 = @bitCast(next_pos.im);
-            if (mat[row][col] == '#' or (row == i and col == j)) {
+            if (mat[row][col] == '#') {
                 direction = direction.mul(rot_right);
             } else {
                 position = next_pos;
