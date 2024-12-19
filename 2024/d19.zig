3fc090912f2677c453355dd336f869a2cc77ec23
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-12-19 22:38:52 +0100

    d19 removed redudant code

--- 2024/d19.zig
+++ 2024/d19.zig
@@ -10,31 +10,24 @@ const Allocator = std.mem.Allocator;
 
 const Patterns = std.StringArrayHashMap(void);
 
-fn part1(str: []const u8, built_towel: []const u8, patterns: Patterns, index: u8, max_len: u8, early_exit: *bool) bool {
+fn part1(str: []const u8, patterns: Patterns, index: u8, max_len: u8, early_exit: *bool) bool {
     if (early_exit.*) return true;
     if (index == max_len) {
-        if (std.mem.eql(u8, str, built_towel)) {
-            early_exit.* = true;
-            return true;
-        }
-        return false;
+        early_exit.* = true;
+        return true;
     }
-    var disjunct: bool = false;
     for (index + 1..max_len + 1) |i| {
         if (patterns.contains(str[index..i])) {
-            const res = part1(str, str[0..i], patterns, @intCast(i), max_len, early_exit);
-            if (res) return true;
-            disjunct = disjunct or res;
+            if (part1(str, patterns, @intCast(i), max_len, early_exit))
+                return true;
         }
     }
-    return disjunct;
+    return false;
 }
 
-fn part2(str: []const u8, built_towel: []const u8, patterns: Patterns, index: u8, max_len: u8, memo: *[61][61]?u64) !u64 {
-    if (index >= max_len) {
-        if (std.mem.eql(u8, str, built_towel)) return 1;
-        return 0;
-    }
+fn part2(str: []const u8, patterns: Patterns, index: u8, max_len: u8, memo: *[61][61]?u64) !u64 {
+    if (index >= max_len) return 1;
+
     var sum: u64 = 0;
     for (index + 1..max_len + 1) |i| {
         const value = memo[index][@intCast(i - index)];
@@ -43,7 +36,7 @@ fn part2(str: []const u8, built_towel: []const u8, patterns: Patterns, index: u8
             continue;
         }
         if (patterns.contains(str[index..i])) {
-            const res = try part2(str, str[0..i], patterns, @intCast(i), max_len, memo);
+            const res = try part2(str, patterns, @intCast(i), max_len, memo);
             memo[index][@intCast(i - index)] = res;
             sum += res;
         }
@@ -94,50 +87,11 @@ pub fn main() !void {
     var desire_iter = std.mem.tokenizeSequence(u8, raw_desire, input_attributes.delim);
     while (desire_iter.next()) |desire| {
         var early_exit = false;
-        if (part1(desire, &[0]u8{}, patterns, @truncate(0), @truncate(desire.len), &early_exit)) {
+        if (part1(desire, patterns, @truncate(0), @truncate(desire.len), &early_exit))
             p1_sum += 1;
-        }
+
         var memo = mmemo.*;
-        const res = try part2(desire, &[0]u8{}, patterns, @truncate(0), @truncate(desire.len), &memo);
-        p2_sum += res;
+        p2_sum += try part2(desire, patterns, @truncate(0), @truncate(desire.len), &memo);
     }
     try writer.print("Part 1: {d}\nPart 2: {d}\n", .{ p1_sum, p2_sum });
-}
-
-test "example" {
-    const allocator = std.testing.allocator;
-    var list = std.ArrayList(i8).init(allocator);
-    defer list.deinit();
-
-    const input = @embedFile("in/d19.txt");
-    const input_attributes = try myf.getInputAttributes(input);
-
-    const double_delim = try std.mem.concat(allocator, u8, &.{ input_attributes.delim, input_attributes.delim });
-    defer allocator.free(double_delim);
-
-    var in_iter = std.mem.tokenizeSequence(u8, input, double_delim);
-
-    const raw_towel_patterns = in_iter.next().?;
-    const raw_desire = in_iter.next().?;
-
-    var patterns = Patterns.init(allocator);
-    defer patterns.deinit();
-
-    var patterns_iter = std.mem.tokenizeSequence(u8, raw_towel_patterns, ", ");
-    while (patterns_iter.next()) |pattern| try patterns.put(pattern, {});
-
-    var sum: u64 = 0;
-    var sum2: u64 = 0;
-
-    var desire_iter = std.mem.tokenizeSequence(u8, raw_desire, input_attributes.delim);
-    while (desire_iter.next()) |desire| {
-        var early_exit = false;
-        if (part1(desire, &[0]u8{}, patterns, @truncate(0), @truncate(desire.len), &early_exit)) {
-            sum += 1;
-        }
-        sum2 += part2(desire, &[0]u8{}, patterns, @truncate(0), @truncate(desire.len));
-        printa(sum2);
-    }
-    printa(sum);
-    printa(sum2);
 }
