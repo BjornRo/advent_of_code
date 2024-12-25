e1ed80671bc33b8d4119eee2b941f31f1804f044
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-12-25 13:04:25 +0100

    d25 cleaning

--- 2024/d25.zig
+++ 2024/d25.zig
@@ -9,6 +9,7 @@ const expect = std.testing.expect;
 const time = std.time;
 const Allocator = std.mem.Allocator;
 
+const Vec5 = @Vector(5, u8);
 pub fn main() !void {
     const start = time.nanoTimestamp();
     const writer = std.io.getStdOut().writer();
@@ -17,31 +18,16 @@ pub fn main() !void {
         const elapsed = @as(f128, @floatFromInt(end - start)) / @as(f128, 1_000_000);
         writer.print("\nTime taken: {d:.7}ms\n", .{elapsed}) catch {};
     }
-    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    // defer if (gpa.deinit() == .leak) expect(false) catch @panic("TEST FAIL");
-    // const allocator = gpa.allocator();
-    // var buffer: [70_000]u8 = undefined;
-    // var fba = std.heap.FixedBufferAllocator.init(&buffer);
-    // const allocator = fba.allocator();
+    var buffer: [40_000]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    const allocator = fba.allocator();
 
-    // const filename = try myf.getAppArg(allocator, 1);
-    // const target_file = try std.mem.concat(allocator, u8, &.{ "in/", filename });
-    // const input = try myf.readFile(allocator, target_file);
-    // std.debug.print("Input size: {d}\n\n", .{input.len});
-    // defer inline for (.{ filename, target_file, input }) |res| allocator.free(res);
-    // const input_attributes = try myf.getInputAttributes(input);
-    // End setup
-
-    // std.debug.print("{s}\n", .{input});
-}
-
-const Vec5 = @Vector(5, u8);
-
-test "example" {
-    const allocator = std.testing.allocator;
-    const input = @embedFile("in/d25.txt");
-
+    const filename = try myf.getAppArg(allocator, 1);
+    const target_file = try std.mem.concat(allocator, u8, &.{ "in/", filename });
+    const input = try myf.readFile(allocator, target_file);
+    defer inline for (.{ filename, target_file, input }) |res| allocator.free(res);
     const input_attributes = try myf.getInputAttributes(input);
+    // End setup
 
     const double_delim = try std.mem.concat(allocator, u8, &.{ input_attributes.delim, input_attributes.delim });
     defer allocator.free(double_delim);
@@ -56,16 +42,16 @@ test "example" {
         const list = if (result.is_lock) &locks else &keys;
         try list.*.append(result.vector);
     }
-    const res = combinations(locks.items, keys.items);
-    printa(res);
+
+    try writer.print("Part 1: {d}", .{combinations(locks.items, keys.items)});
 }
 
-fn combinations(locks: []Vec5, keys: []Vec5) u32 {
-    var pairs: u32 = 0;
+fn combinations(locks: []Vec5, keys: []Vec5) u16 {
+    var pairs: u16 = 0;
     for (locks) |lock| {
         for (keys) |key| {
-            const res: [5]bool = (lock + key) <= Vec5{ 5, 5, 5, 5, 5 };
-            if (myf.all(&res)) {
+            const res: [5]bool = (lock + key) > Vec5{ 5, 5, 5, 5, 5 };
+            if (!myf.any(&res)) {
                 pairs += 1;
             }
         }
