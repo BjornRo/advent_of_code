4430bfc2655c1b72f85b14a0806c5a8585433228
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-12-29 00:31:01 +0100

    d4

--- 2017/d04.py
+++ 2017/d04.py
@@ -5,5 +5,5 @@ with open("in/d04.txt") as f:
 
 f = lambda x: Counter(x).most_common(1)[0][1] == 1
 
-print(f"Part 1: {sum(1 for x in data if f(x))}")
-print(f"Part 2: {sum(1 for x in data if f(map(lambda y: "".join(sorted(y)), x)))}")
+print(f"Part 1: {sum(f(x) for x in data)}")
+print(f"Part 2: {sum(f(map(lambda y: "".join(sorted(y)), x)) for x in data)}")
