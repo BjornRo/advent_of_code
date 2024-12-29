8a0b383595551b3697bf17b62410377b223db7f5
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-12-30 00:09:31 +0100

    d12

--- 2017/d13.py
+++ 2017/d13.py
@@ -1,38 +1,5 @@
-import re
-from collections import defaultdict
+with open("in/d13.txt") as f:
+    pass
 
-graph = defaultdict(set)
-
-with open("in/d12.txt") as f:
-    for row in f:
-        pid, *pids = map(int, re.findall(r"\d+", row))
-        graph[pid].update(pids)
-        for p in pids:
-            graph[p].add(pid)
-
-
-def solver(graph: dict[int, set[int]]) -> tuple[int, int]:
-    pid0 = 0
-    groups = 0
-    while len(graph) != 0:
-        visited = set()
-        stack = [next(iter(graph))]
-        while stack:
-            pid = stack.pop()
-
-            if pid in visited:
-                continue
-            visited.add(pid)
-
-            stack.extend(graph[pid])
-        groups += 1
-        if 0 in visited:
-            pid0 = len(visited)
-        for v in visited:
-            graph.pop(v)
-    return pid0, groups
-
-
-p1, p2 = solver(graph)
-print(f"Part 1: {p1}")
-print(f"Part 2: {p2}")
+# print(f"Part 1: {p1}")
+# print(f"Part 2: {p2}")
