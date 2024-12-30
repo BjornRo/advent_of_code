c8e9df40a5f465806ef52fccd5798782295cef36
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-12-30 13:34:28 +0100

    d14

--- 2017/d14.py
+++ 2017/d14.py
@@ -1,7 +1,28 @@
-from d10 import part2 as knot_hash
+from itertools import batched
 
 with open("in/d14.txt") as f:
     data = f.read().rstrip()
+
+
+def knot_hash(data: str) -> str:
+    circ_list = list(range(256))
+    skip_size = 0
+    curr_position = 0
+    for _ in range(64):
+        for i in list(data.encode()) + [17, 31, 73, 47, 23]:
+            for j in range(i // 2):
+                k = (curr_position + j) % len(circ_list)
+                l = (curr_position + i - j - 1) % len(circ_list)
+                circ_list[k], circ_list[l] = circ_list[l], circ_list[k]
+            curr_position = (curr_position + i + skip_size) % len(circ_list)
+            skip_size += 1
+    dense_hash = []
+    for i in batched(circ_list, 16):
+        dhash = 0
+        for j in i:
+            dhash ^= j
+        dense_hash.append(dhash)
+    return bytes(dense_hash).hex()
 
 
 def part2(matrix: list[list[int]]):
