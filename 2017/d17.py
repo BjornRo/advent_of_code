dff3aa8cda15846a5a011903ea5398d2ce237ff8
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-12-31 00:29:17 +0100

    d17

--- 2017/d17.py
+++ 2017/d17.py
@@ -1,6 +1,17 @@
+from collections import deque
+
 with open("in/d17.txt") as f:
     data = int(f.read().rstrip())
 
 
-# print(f"Part 1: {}")
-# print(f"Part 2: {}")
+def solver(end: int):
+    queue = deque([0])
+    for i in range(1, end + 1):
+        queue.rotate(-data)
+        queue.append(i)
+    return queue
+
+
+q = solver(50_000_000)  # Bruteforce
+print(f"Part 1: {solver(2017)[0]}")
+print(f"Part 2: {q[q.index(0) + 1]}")
---
+++ 2017/d18.py
@@ -0,0 +1,6 @@
+with open("in/d18.txt") as f:
+    data = [x.rstrip().split(" ") for x in f]
+
+
+# print(f"Part 1: {}")
+# print(f"Part 2: {}")
