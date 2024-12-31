117f2f854ddf2189ec3e41a7641bc7781f806876
BjornRo <54636059+BjornRo@users.noreply.github.com>
2024-12-31 03:24:07 +0100

    d17

--- 2017/d17.py
+++ 2017/d17.py
@@ -4,14 +4,24 @@ with open("in/d17.txt") as f:
     data = int(f.read().rstrip())
 
 
-def solver(end: int):
+def part1():
     queue = deque([0])
-    for i in range(1, end + 1):
+    for i in range(1, 2017 + 1):
         queue.rotate(-data)
         queue.append(i)
-    return queue
+    return queue.popleft()
 
 
-q = solver(50_000_000)  # Bruteforce
-print(f"Part 1: {solver(2017)[0]}")
-print(f"Part 2: {q[q.index(0) + 1]}")
+def part2():
+    pos_zero = 0
+    pos_zero_plus = 0
+    for i in range(1, 50_000_000 + 1):
+        new_pos = (pos_zero + data) % i
+        if new_pos == 0:
+            pos_zero_plus = i
+        pos_zero = new_pos + 1
+    return pos_zero_plus
+
+
+print(f"Part 1: {part1()}")
+print(f"Part 2: {part2()}")
