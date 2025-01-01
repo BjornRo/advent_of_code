e789f6712c56d5364ea71bdbea6ef1601547e8b0
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-01-01 15:05:19 +0100

    d19

--- 2017/d19.py
+++ 2017/d19.py
@@ -1,9 +1,34 @@
-import asyncio
-from collections import defaultdict
+with open("in/d19.txt") as f:
+    matrix = [x.rstrip("\n\r") for x in f]
 
-with open("in/d18.txt") as f:
-    data = [x.rstrip().split(" ") for x in f]
+coord2elem = lambda c: matrix[int(c.real)][int(c.imag)]
+curr_pos = complex(0, matrix[0].index("|"))
+direction = complex(1, 0)
 
+steps = 0
+letters: list[str] = []
+while True:
+    curr_tile = coord2elem(curr_pos)
+    if curr_tile.isalpha():
+        letters.append(curr_tile)
+    next_pos = curr_pos + direction
+    match coord2elem(next_pos):
+        case " ":
+            steps += 1
+            break
+        case "+":
+            for rotation in complex(0, 1), complex(0, -1):
+                new_dir = direction * rotation
+                new_step = next_pos + new_dir
+                if 0 <= new_step.real < len(matrix) and 0 <= new_step.imag < len(matrix[0]):
+                    if coord2elem(new_step) != " ":
+                        direction = new_dir
+                        curr_pos = new_step
+                        steps += 2
+                        break
+        case _:
+            curr_pos = next_pos
+            steps += 1
 
-print(f"Part 1: {1}")
-print(f"Part 2: {2}")
+print(f"Part 1: {"".join(letters)}")
+print(f"Part 2: {steps}")
---
+++ 2017/d20.py
@@ -0,0 +1,5 @@
+with open("in/d19.txt") as f:
+    matrix = [x.rstrip("\n\r") for x in f]
+
+print(f"Part 1: {1}")
+print(f"Part 2: {2}")
