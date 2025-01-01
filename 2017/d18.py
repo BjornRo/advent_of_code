efd5aae14cd005cf9fb26071b91245079a46c797
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-01-01 13:20:42 +0100

    d18 cleanup

--- 2017/d18.py
+++ 2017/d18.py
@@ -5,12 +5,7 @@ with open("in/d18.txt") as f:
     data = [x.rstrip().split(" ") for x in f]
 
 
-async def program(
-    uid: int,
-    signal: asyncio.Event,
-    send_queue: asyncio.Queue,
-    recv_queue: asyncio.Queue,
-):
+async def program(uid: int, send_queue: asyncio.Queue, recv_queue: asyncio.Queue):
     reg_or_val = lambda registers, value: registers[value] if value.isalpha() else int(value)
     registers = defaultdict(int)
     registers["p"] = uid
@@ -35,32 +30,22 @@ async def program(
                 await send_queue.put(registers[reg])
                 sent += 1
             case "rcv", reg:
-                if sent != 0:
-                    if recv_queue.empty() and send_queue.empty():
-                        signal.set()
+                if sent != 0 and recv_queue.empty() and send_queue.empty():
+                    return sent
                 if send_queue is recv_queue:
                     return recv_queue._queue[-1]  # type:ignore
                 value = await recv_queue.get()
-                if value == -1:
-                    return sent
                 registers[reg] = value
-
-                signal.clear()
 
 
 async def solver():
-    signal = asyncio.Event()
     a_queue = asyncio.Queue()
-    p1_result = await program(0, signal, send_queue=a_queue, recv_queue=a_queue)
+    p1_result = await program(0, send_queue=a_queue, recv_queue=a_queue)
 
     a_queue = asyncio.Queue()
     b_queue = asyncio.Queue()
-    task_a = asyncio.create_task(program(0, signal, send_queue=b_queue, recv_queue=a_queue))
-    task_b = asyncio.create_task(program(1, signal, send_queue=a_queue, recv_queue=b_queue))
-
-    await signal.wait()
-    await b_queue.put(-1)
-    return p1_result, await task_b
+    task_a = asyncio.create_task(program(0, send_queue=b_queue, recv_queue=a_queue))
+    return p1_result, await program(1, send_queue=a_queue, recv_queue=b_queue)
 
 
 p1, p2 = asyncio.run(solver())
---
+++ 2017/d19.py
@@ -0,0 +1,9 @@
+import asyncio
+from collections import defaultdict
+
+with open("in/d18.txt") as f:
+    data = [x.rstrip().split(" ") for x in f]
+
+
+print(f"Part 1: {1}")
+print(f"Part 2: {2}")
