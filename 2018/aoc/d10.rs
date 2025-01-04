fbad5043172c2553d5f939fc23d26d3ec9a5be21
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-01-04 23:08:22 +0100

    d10

--- 2018/aoc/d10.rs
+++ 2018/aoc/d10.rs
@@ -1,21 +1,106 @@
-#[allow(unused_imports)]
 use regex::Regex;
-#[allow(unused_imports)]
-use std::{collections::HashMap, collections::VecDeque, fs};
+use std::{collections::HashMap, fs};
 
-#[allow(unused_mut)]
-#[allow(unused_variables)]
+#[derive(Debug, Clone)]
+struct Point {
+    pos: (isize, isize),
+    vel: (isize, isize),
+}
+
+impl Point {
+    fn step(&self, i: isize) -> (isize, isize) {
+        let (vr, vc) = self.vel;
+        let (r, c) = self.pos;
+        (r + vr * i, c + vc * i)
+    }
+}
+
+impl From<[(isize, isize); 2]> for Point {
+    fn from(i: [(isize, isize); 2]) -> Self {
+        Point {
+            pos: i[0],
+            vel: i[1],
+        }
+    }
+}
+
+fn printer(map: Vec<(isize, isize)>) {
+    let min_row = map.iter().map(|(r, _)| r).min().unwrap() - 1;
+    let max_row = map.iter().map(|(r, _)| r).max().unwrap() + 1;
+    let min_col = map.iter().map(|(_, c)| c).min().unwrap() - 1;
+    let max_col = map.iter().map(|(_, c)| c).max().unwrap() + 1;
+    let rows = (max_row - min_row + 1) as usize;
+    let cols = (max_col - min_col + 1) as usize;
+    let mut matrix = vec![vec![" ".to_string(); cols]; rows];
+    for (r, c) in map {
+        matrix[(r - min_row) as usize][(c - min_col) as usize] = "#".to_string();
+    }
+    for row in matrix {
+        println!("{}", row.concat());
+    }
+}
+
+fn dfs(
+    points: &Vec<Point>,
+    initial_points: usize,
+    threshold: usize,
+) -> (isize, Vec<(isize, isize)>) {
+    let mut visited: HashMap<(isize, isize), ()> = HashMap::new();
+
+    let mut i: isize = 0;
+    loop {
+        visited.clear();
+
+        let map: HashMap<(isize, isize), ()> = points.iter().map(|p| (p.step(i), ())).collect();
+        let mut stack: Vec<(isize, isize)> = map.keys().take(initial_points).cloned().collect();
+
+        while stack.len() != 0 {
+            let p @ (row, col) = stack.pop().unwrap();
+
+            if visited.contains_key(&p) {
+                continue;
+            }
+            visited.insert(p, ());
+
+            if visited.len() >= threshold {
+                return (i, map.keys().cloned().collect::<Vec<(isize, isize)>>());
+            }
+
+            for (nr, nc) in [(0, 1), (1, 0), (0, -1), (-1, 0)] {
+                let np = (row + nr, col + nc);
+                if let Some(_) = map.get(&np) {
+                    stack.push(np);
+                }
+            }
+        }
+        i += 1;
+    }
+}
+
 fn main() -> std::io::Result<()> {
-    let [players, points] = fs::read_to_string("in/d10.txt")?
-        .split_whitespace()
-        .filter(|x| x.chars().any(|c| c.is_ascii_digit()))
-        .map(|x| x.parse::<usize>().unwrap())
-        .collect::<Vec<usize>>()[..]
-    else {
-        panic!("nope")
-    };
+    let content = fs::read_to_string("in/d10.txt")?;
 
-    println!("Part 1: {}", 1);
-    println!("Part 2: {}", 2);
+    let re = Regex::new(r"<(.*)>.*<(.*)>").unwrap();
+
+    let points: Vec<Point> = re
+        .captures_iter(&content)
+        .map(|c| {
+            c.extract().1.map(|s| {
+                let value: Vec<isize> = s
+                    .split(',')
+                    .map(|v| v.trim().parse::<isize>().unwrap())
+                    .collect();
+                (value[1], value[0])
+            })
+        })
+        .map(|arr| arr.into())
+        .collect();
+
+    let (index, map) = dfs(&points, 8, 28);
+
+    println!("Part 1:");
+    printer(map);
+
+    println!("Part 2: {}", index);
     Ok(())
 }
