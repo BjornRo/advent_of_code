99af1a76ad200044bec00c82430c48788b2c079c
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-01-05 01:07:53 +0100

    d11 multithreading

--- 2018/aoc/d11.rs
+++ 2018/aoc/d11.rs
@@ -1,49 +1,10 @@
+use rayon::prelude::*;
 use std::fs;
 
 const DIMENSION: usize = 300;
 type Matrix = [[isize; DIMENSION]; DIMENSION];
 
-fn main() -> std::io::Result<()> {
-    let serial_num: usize = fs::read_to_string("in/d11.txt")?
-        .trim_end()
-        .parse()
-        .unwrap();
-
-    let mut matrix: Matrix = [[0; DIMENSION]; DIMENSION];
-    for x in 1..DIMENSION {
-        for y in 1..DIMENSION {
-            let rack_id = x + 10;
-            let power_level = rack_id * y + serial_num;
-            let fuel_cell = (((power_level * rack_id) / 100) % 10) as isize - 5;
-            matrix[x][y] = fuel_cell;
-        }
-    }
-
-    let (_, (p1x, p1y)) = scanner(matrix, 3);
-    let ((p2x, p2y), kernel) = part2(matrix);
-
-    println!("Part 1: {},{}", p1x, p1y);
-    println!("Part 2: {},{},{}", p2x, p2y, kernel);
-    Ok(())
-}
-
-fn part2(matrix: Matrix) -> ((usize, usize), usize) {
-    let mut max_power: isize = 0;
-    let mut best_kernel: usize = 0;
-    let mut coord: (usize, usize) = (0, 0);
-
-    for i in 1..=DIMENSION {
-        let (power, c) = scanner(matrix, i);
-        if power > max_power {
-            max_power = power;
-            coord = c;
-            best_kernel = i;
-        }
-    }
-    (coord, best_kernel)
-}
-
-fn scanner(matrix: Matrix, kernel_size: usize) -> (isize, (usize, usize)) {
+fn scanner(matrix: &Matrix, kernel_size: usize) -> (isize, (usize, usize)) {
     let mut max_power: isize = 0;
     let mut coord: (usize, usize) = (0, 0);
 
@@ -52,7 +13,7 @@ fn scanner(matrix: Matrix, kernel_size: usize) -> (isize, (usize, usize)) {
             let mut total_power = 0 as isize;
             for xk in x..x + kernel_size {
                 for yk in y..y + kernel_size {
-                    total_power += matrix[xk][yk];
+                    total_power += matrix[xk][yk] as isize;
                 }
             }
             if total_power > max_power {
@@ -63,3 +24,65 @@ fn scanner(matrix: Matrix, kernel_size: usize) -> (isize, (usize, usize)) {
     }
     (max_power, (coord.0, coord.1))
 }
+
+fn part2(matrix: &Matrix) -> ((usize, usize), usize) {
+    let results: Vec<(isize, (usize, usize), usize)> = (1..=DIMENSION)
+        .into_par_iter()
+        .map(|k| {
+            let (power, c) = scanner(matrix, k);
+            (power, c, k)
+        })
+        .collect();
+
+    let mut max_power: isize = 0;
+    let mut best_kernel: usize = 0;
+    let mut coord: (usize, usize) = (0, 0);
+    for (power, c, k) in results {
+        if power > max_power {
+            max_power = power;
+            best_kernel = k;
+            coord = c;
+        }
+    }
+    (coord, best_kernel)
+}
+fn main() -> std::io::Result<()> {
+    let serial_num: usize = fs::read_to_string("in/d11.txt")?
+        .trim_end()
+        .parse()
+        .unwrap();
+
+    let mut matrix: Matrix = [[0; DIMENSION]; DIMENSION];
+    for x in 0..DIMENSION {
+        for y in 0..DIMENSION {
+            let rack_id = x + 10;
+            let power_level = rack_id * y + serial_num;
+            let fuel_cell = (((power_level * rack_id) / 100) % 10) as isize - 5;
+            matrix[x][y] = fuel_cell;
+        }
+    }
+
+    let (_, (p1x, p1y)) = scanner(&matrix, 3);
+    let ((p2x, p2y), kernel) = part2(&matrix);
+
+    println!("Part 1: {},{}", p1x, p1y);
+    println!("Part 2: {},{},{}", p2x, p2y, kernel);
+    Ok(())
+}
+
+// fn part2(matrix: &Matrix) -> ((usize, usize), usize) {
+//     let mut max_power: isize = 0;
+//     let mut best_kernel: usize = 0;
+//     let mut coord: (usize, usize) = (0, 0);
+
+//     let results: Vec<(isize, (usize, usize), usize)> = (1..=DIMENSION)
+//         .into_par_iter()
+//         .map(|k| {
+//             let (power, c) = scanner(matrix, k);
+//             (power, c, k)
+//         })
+//         .collect();
+//     println!("{:?}", results);
+
+//     (coord, best_kernel)
+// }
