f1d71b96474d5889f85eabe55500746af463d7f7
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-01-07 12:01:01 +0100

    d17 starting

--- 2018/aoc/d17.rs
+++ 2018/aoc/d17.rs
@@ -3,26 +3,43 @@ use regex::Regex;
 #[allow(unused_imports)]
 use std::{collections::HashMap, collections::VecDeque, fs};
 
+// [from,to], inclusive range
+type Range = [usize; 2];
+
+#[derive(Debug, Clone)]
+#[allow(dead_code)]
+struct ClayRanges {
+    rows: Range,
+    cols: Range,
+}
+
 #[allow(unused_mut)]
 #[allow(unused_variables)]
 fn main() -> std::io::Result<()> {
-    let content = fs::read_to_string("in/d17t.txt")?;
+    let all_ranges: Vec<ClayRanges> = fs::read_to_string("in/d17t.txt")?
+        .trim_end()
+        .lines()
+        .map(|line| {
+            let (left, right) = line.split_once(", ").unwrap();
 
-    let re = Regex::new(r"<(.*)>.*<(.*)>").unwrap();
+            let (left_var, left_val) = left.split_once("=").unwrap();
+            let (_, right_val) = right.split_once("=").unwrap();
 
-    let points: Vec<Point> = re
-        .captures_iter(&content)
-        .map(|c| {
-            c.extract().1.map(|s| {
-                let value: Vec<isize> = s
-                    .split(',')
-                    .map(|v| v.trim().parse::<isize>().unwrap())
-                    .collect();
-                (value[1], value[0])
-            })
+            let left_range: Range = [left_val.parse().unwrap(); 2];
+            let right_range: Range = right_val
+                .split_once("..")
+                .map(|(l, r)| [l.parse().unwrap(), r.parse().unwrap()])
+                .unwrap();
+
+            let (rows, cols) = match left_var {
+                "y" => (left_range, right_range),
+                _ => (right_range, left_range),
+            };
+            ClayRanges { rows, cols }
         })
-        .map(|arr| arr.into())
         .collect();
+
+    println!("{:?}", all_ranges);
 
     println!("Part 1: {}", 1);
     println!("Part 2: {}", 2);
