ce25887bad8b039c8185ef68a16ae38e9d83918b
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-01-10 00:30:59 +0100

    d23 p1

--- 2018/aoc/d23.rs
+++ 2018/aoc/d23.rs
@@ -5,6 +5,7 @@
 #![allow(unused_assignments)]
 #![allow(unused_must_use)]
 use regex::Regex;
+use std::cmp::Ordering;
 use std::collections::{HashMap, HashSet, VecDeque};
 use std::fs;
 
@@ -13,27 +14,62 @@ fn print<T: std::fmt::Debug>(x: T) {
     println!("{:?}", x);
 }
 
+#[derive(Debug, Clone, Eq, PartialEq)]
+struct Point {
+    pos: (isize, isize, isize),
+    radius: isize,
+}
+impl Point {
+    fn manhattan(&self, o: &Point) -> isize {
+        let (a0, a1, a2) = self.pos;
+        let (b0, b1, b2) = o.pos;
+        (a0 - b0).abs() + (a1 - b1).abs() + (a2 - b2).abs()
+    }
+}
+impl Ord for Point {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.radius.cmp(&self.radius)
+    }
+}
+impl PartialOrd for Point {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl From<[isize; 4]> for Point {
+    fn from(i: [isize; 4]) -> Self {
+        let [a, b, c, d] = i;
+        Point {
+            pos: (a, b, c),
+            radius: d,
+        }
+    }
+}
+
+fn part1(points: &Vec<Point>) -> usize {
+    points
+        .iter()
+        .filter(|r| points[0].manhattan(r) <= points[0].radius)
+        .count()
+}
+
+fn part2(points: &Vec<Point>) {
+    //
+}
+
 fn main() -> std::io::Result<()> {
-    let content = fs::read_to_string("in/d10.txt")?;
+    let content = fs::read_to_string("in/d23tt.txt")?;
+    let re = Regex::new(r"(-*\d+).(-*\d+).(-*\d+).+r=(\d+)").unwrap();
 
-    let re = Regex::new(r"<(.*)>.*<(.*)>").unwrap();
-
-    let points: Vec<Point> = re
-        .captures_iter(&content)
-        .map(|c| {
-            c.extract().1.map(|s| {
-                let value: Vec<isize> = s
-                    .split(',')
-                    .map(|v| v.trim().parse::<isize>().unwrap())
-                    .collect();
-                (value[1], value[0])
-            })
-        })
-        .map(|arr| arr.into())
+    let mut points: Vec<Point> = re
+        .captures_iter(&content.trim_end())
+        .map(|c| c.extract().1.map(|s| s.parse().unwrap()).into())
         .collect();
+    points.sort_unstable();
 
-    println!("{:?}", content);
-    println!("Part 1: {}", 1);
-    println!("Part 2: {}", 2);
+    part2(&points);
+
+    // println!("Part 1: {}", part1(&points));
+    // println!("Part 2: {}", 2);
     Ok(())
 }
