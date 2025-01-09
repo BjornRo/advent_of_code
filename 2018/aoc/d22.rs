ef46c7e27093395cfadfc8773f55e8af49b839eb
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-01-09 18:01:03 +0100

    d22 p1

--- 2018/aoc/d22.rs
+++ 2018/aoc/d22.rs
@@ -8,32 +8,100 @@ use regex::Regex;
 use std::collections::{HashMap, HashSet, VecDeque};
 use std::fs;
 
+use RegionType::*;
+type Pos = (isize, isize);
+type Map = HashMap<Pos, isize>;
+
 #[allow(dead_code)]
 fn print<T: std::fmt::Debug>(x: T) {
     println!("{:?}", x);
 }
 
+#[derive(Debug)]
+enum RegionType {
+    Rocky,
+    Wet,
+    Narrow,
+}
+
+fn calc_geologic(pos: Pos, depth: isize, target: Pos, memo: &mut Map) -> isize {
+    if let Some(&res) = memo.get(&pos) {
+        return res;
+    }
+    let (row, col) = pos;
+    let value = if pos == (0, 0) || pos == target {
+        0
+    } else if col == 0 {
+        row * 16807
+    } else if row == 0 {
+        col * 48271
+    } else {
+        [(row - 1, col), (row, col - 1)]
+            .iter()
+            .map(|&pos| (calc_geologic(pos, depth, target, memo) + depth) % 20183)
+            .fold(1, |acc, geologic| acc * geologic)
+    };
+    memo.insert(pos, value);
+    value
+}
+
+fn calc_erosion(pos: Pos, depth: isize, target: Pos, memo: &mut Map) -> RegionType {
+    let geo_idx = (calc_geologic(pos, depth, target, memo) + depth) % 20183;
+    match geo_idx % 3 {
+        0 => Rocky,
+        1 => Wet,
+        _2 => Narrow,
+    }
+}
+
+fn region_value(region_type: RegionType) -> usize {
+    match region_type {
+        Rocky => 0,
+        Wet => 1,
+        Narrow => 2,
+    }
+}
+
+fn part1(depth: isize, target @ (row, col): Pos, memo: &mut Map) -> usize {
+    let mut sum: usize = 0;
+    for i in 0..=row {
+        sum += (0..=col)
+            .map(|j| region_value(calc_erosion((i, j), depth, target, memo)))
+            .sum::<usize>();
+    }
+    sum
+}
+
+fn part2(depth: isize, target: Pos, memo: &Map) -> Option<u8> {
+    //
+    None
+}
+
 fn main() -> std::io::Result<()> {
-    let content = fs::read_to_string("in/d10.txt")?;
-
-    let re = Regex::new(r"<(.*)>.*<(.*)>").unwrap();
-
-    let points: Vec<Point> = re
-        .captures_iter(&content)
-        .map(|c| {
-            c.extract().1.map(|s| {
-                let value: Vec<isize> = s
-                    .split(',')
-                    .map(|v| v.trim().parse::<isize>().unwrap())
-                    .collect();
-                (value[1], value[0])
-            })
+    let (depth, target) = fs::read_to_string("in/d22t.txt")?
+        .trim_end()
+        .split_once("\n")
+        .map(|(depth, target)| {
+            let depth: isize = depth.split_once(" ").unwrap().1.parse().unwrap();
+            let target: Pos = target
+                .split_once(" ")
+                .unwrap()
+                .1
+                .split_once(",")
+                .map(|(row, col)| (row.parse().unwrap(), col.parse().unwrap()))
+                .unwrap();
+            (depth, target)
         })
-        .map(|arr| arr.into())
-        .collect();
+        .unwrap();
 
-    println!("{:?}", content);
-    println!("Part 1: {}", 1);
-    println!("Part 2: {}", 2);
+    let mut memo: HashMap<Pos, isize> = HashMap::new();
+
+    let p1 = part1(depth, target, &mut memo);
+    print(p1);
+    print(&memo);
+    let p2 = part2(depth, target, &memo);
+
+    // println!("Part 1: {}", 1);
+    // println!("Part 2: {}", 2);
     Ok(())
 }
