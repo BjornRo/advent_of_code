900ee8fbce3c820d5cb659656ebee0054d8789e6
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-01-09 15:56:03 +0100

    d21 clean

--- 2018/aoc/d21.rs
+++ 2018/aoc/d21.rs
@@ -3,54 +3,41 @@ use std::fs;
 
 type Func = Box<dyn Fn(&mut Vec<usize>)>;
 
-struct Instruction {
-    func: Func,
-}
-impl Instruction {
-    fn apply(&self, regs: &mut Vec<usize>) {
-        (self.func)(regs);
-    }
-}
-
-impl From<&str> for Instruction {
-    #[rustfmt::skip]
-    fn from(value: &str) -> Self {
-        value
-            .split_once(" ")
-            .map(|(func, args)| {
-                let [a, b, c]: [usize; 3] = args
-                    .split_whitespace()
-                    .map(|s| s.parse().unwrap())
-                    .collect::<Vec<usize>>()
-                    .try_into()
-                    .expect("");
-                Instruction {
-                    func: match func {
-                        "seti" => Box::new(move |reg: &mut Vec<usize>| reg[c] = a),
-                        "setr" => Box::new(move |reg: &mut Vec<usize>| reg[c] = reg[a]),
-                        "addi" => Box::new(move |reg: &mut Vec<usize>| reg[c] = reg[a] + b),
-                        "addr" => Box::new(move |reg: &mut Vec<usize>| reg[c] = reg[a] + reg[b]),
-                        "muli" => Box::new(move |reg: &mut Vec<usize>| reg[c] = reg[a] * b),
-                        "mulr" => Box::new(move |reg: &mut Vec<usize>| reg[c] = reg[a] * reg[b]),
-                        "bani" => Box::new(move |reg: &mut Vec<usize>| reg[c] = reg[a] & b),
-                        "banr" => Box::new(move |reg: &mut Vec<usize>| reg[c] = reg[a] & reg[b]),
-                        "bori" => Box::new(move |reg: &mut Vec<usize>| reg[c] = reg[a] | b),
-                        "borr" => Box::new(move |reg: &mut Vec<usize>| reg[c] = reg[a] | reg[b]),
-                        "gtri" => Box::new(move |reg: &mut Vec<usize>| reg[c] = (reg[a] > b) as usize),
-                        "gtir" => Box::new(move |reg: &mut Vec<usize>| reg[c] = (a > reg[b]) as usize),
-                        "gtrr" => Box::new(move |reg: &mut Vec<usize>| reg[c] = (reg[a] > reg[b]) as usize),
-                        "eqir" => Box::new(move |reg: &mut Vec<usize>| reg[c] = (a == reg[b]) as usize),
-                        "eqri" => Box::new(move |reg: &mut Vec<usize>| reg[c] = (reg[a] == b) as usize),
-                        "eqrr" => Box::new(move |reg: &mut Vec<usize>| reg[c] = (reg[a] == reg[b]) as usize),
-                        _ => panic!(),
-                    },
-                }
-            })
-            .unwrap()
-    }
+#[rustfmt::skip]
+fn convert(value: &str) -> Func {
+    value
+        .split_once(" ")
+        .map(|(func, args)| {
+            let [a, b, c]: [usize; 3] = args
+                .split_whitespace()
+                .map(|s| s.parse().unwrap())
+                .collect::<Vec<usize>>()
+                .try_into()
+                .expect("");
+            let f: Func = match func {
+                "seti" => Box::new(move |reg: &mut Vec<usize>| reg[c] = a),
+                "setr" => Box::new(move |reg: &mut Vec<usize>| reg[c] = reg[a]),
+                "addi" => Box::new(move |reg: &mut Vec<usize>| reg[c] = reg[a] + b),
+                "addr" => Box::new(move |reg: &mut Vec<usize>| reg[c] = reg[a] + reg[b]),
+                "muli" => Box::new(move |reg: &mut Vec<usize>| reg[c] = reg[a] * b),
+                "mulr" => Box::new(move |reg: &mut Vec<usize>| reg[c] = reg[a] * reg[b]),
+                "bani" => Box::new(move |reg: &mut Vec<usize>| reg[c] = reg[a] & b),
+                "banr" => Box::new(move |reg: &mut Vec<usize>| reg[c] = reg[a] & reg[b]),
+                "bori" => Box::new(move |reg: &mut Vec<usize>| reg[c] = reg[a] | b),
+                "borr" => Box::new(move |reg: &mut Vec<usize>| reg[c] = reg[a] | reg[b]),
+                "gtri" => Box::new(move |reg: &mut Vec<usize>| reg[c] = (reg[a] > b) as usize),
+                "gtir" => Box::new(move |reg: &mut Vec<usize>| reg[c] = (a > reg[b]) as usize),
+                "gtrr" => Box::new(move |reg: &mut Vec<usize>| reg[c] = (reg[a] > reg[b]) as usize),
+                "eqir" => Box::new(move |reg: &mut Vec<usize>| reg[c] = (a == reg[b]) as usize),
+                "eqri" => Box::new(move |reg: &mut Vec<usize>| reg[c] = (reg[a] == b) as usize),
+                "eqrr" => Box::new(move |reg: &mut Vec<usize>| reg[c] = (reg[a] == reg[b]) as usize),
+                _ => panic!(),
+            };
+            f
+        }).unwrap()
 }
 
-fn looper(ip: usize, ins: &Vec<Instruction>) -> (usize, usize) {
+fn looper(ip: usize, ins: &Vec<Func>) -> (usize, usize) {
     let mut first_terminate = 0;
     let mut last_terminate = 0;
 
@@ -58,7 +45,7 @@ fn looper(ip: usize, ins: &Vec<Instruction>) -> (usize, usize) {
     let mut map: HashSet<usize> = HashSet::new();
     loop {
         let reg_ip = registers[ip];
-        ins[reg_ip].apply(&mut registers);
+        (ins[reg_ip])(&mut registers);
         if reg_ip == ins.len() - 1 {
             if first_terminate == 0 {
                 first_terminate = registers[5];
@@ -79,7 +66,7 @@ fn main() -> std::io::Result<()> {
         .split_once("\n")
         .map(|(rip, rins)| {
             let ip = (rip.chars().last().unwrap() as u8 - '0' as u8) as usize;
-            let ins = rins.lines().map(|r| r.into()).collect::<Vec<Instruction>>();
+            let ins = rins.lines().map(|r| convert(r)).collect::<Vec<Func>>();
             (ip, ins)
         })
         .unwrap();
