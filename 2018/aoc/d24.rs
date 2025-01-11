54be114cad06d7395f7e606272d8998f51dc968d
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-01-11 18:35:52 +0100

    d24 parsing, oh my..

--- 2018/aoc/d24.rs
+++ 2018/aoc/d24.rs
@@ -13,27 +13,113 @@ fn print<T: std::fmt::Debug>(x: T) {
     println!("{:?}", x);
 }
 
-fn main() -> std::io::Result<()> {
-    let content = fs::read_to_string("in/d10.txt")?;
+#[derive(Debug)]
+enum DamageType {
+    Bludgeoning,
+    Fire,
+    Cold,
+    Slashing,
+    Radiation,
+}
 
-    let re = Regex::new(r"<(.*)>.*<(.*)>").unwrap();
+#[derive(Debug)]
+struct Unit {
+    units: isize,
+    hp: isize,
+    damage: isize,
+    damage_type: DamageType,
+    weak: Vec<DamageType>,
+    immune: Vec<DamageType>,
+    initative: isize,
+}
 
-    let points: Vec<Point> = re
-        .captures_iter(&content)
-        .map(|c| {
-            c.extract().1.map(|s| {
-                let value: Vec<isize> = s
-                    .split(',')
-                    .map(|v| v.trim().parse::<isize>().unwrap())
-                    .collect();
-                (value[1], value[0])
+fn str_to_damagetype(raw_str: &str) -> DamageType {
+    use DamageType::*;
+    match raw_str {
+        "bludgeoning" => Bludgeoning,
+        "fire" => Fire,
+        "cold" => Cold,
+        "slashing" => Slashing,
+        "radiation" => Radiation,
+        x => panic!("{}", x),
+    }
+}
+
+fn str_to_damagetype_vec(raw_str: &str) -> Vec<DamageType> {
+    use DamageType::*;
+    raw_str
+        .split_whitespace()
+        .skip(2)
+        .map(|s| str_to_damagetype(s.trim_end_matches(",")))
+        .collect()
+}
+
+fn parse_line(regex_data: [&str; 6]) -> Unit {
+    let [units, hp, weak_immune, damage, damage_type, initative] = regex_data;
+    let units: isize = units.parse().unwrap();
+    let hp: isize = hp.parse().unwrap();
+    let damage: isize = damage.parse().unwrap();
+    let damage_type = str_to_damagetype(damage_type);
+    let initative: isize = initative.parse().unwrap();
+    let (weak, immune): (Vec<DamageType>, Vec<DamageType>) = if weak_immune.is_empty() {
+        (vec![], vec![])
+    } else {
+        if weak_immune.contains(";") {
+            let (mut left, mut right) = weak_immune.split_once("; ").unwrap();
+            if left.starts_with("immune") {
+                let tmp = left;
+                left = right;
+                right = tmp;
+            }
+            (str_to_damagetype_vec(left), str_to_damagetype_vec(right))
+        } else {
+            if weak_immune.starts_with("weak") {
+                (str_to_damagetype_vec(weak_immune), vec![])
+            } else {
+                (vec![], str_to_damagetype_vec(weak_immune))
+            }
+        }
+    };
+    Unit {
+        units,
+        hp,
+        damage,
+        damage_type,
+        weak,
+        immune,
+        initative,
+    }
+}
+
+fn parse_block(raw_block: &str) -> Vec<Unit> {
+    let re_par =
+        Regex::new(r"^(\d+)[a-z\s]+(\d+)[\w\s]+\(([a-z,;\s]+)\)[a-z\s]+(\d+)\s(\w+)[a-z\s]+(\d+)")
+            .unwrap();
+    let re = Regex::new(r"^(\d+)[a-z\s]+(\d+)[\w\s]+?[a-z\s]+(\d+) (\w+)[a-z\s]+(\d+)").unwrap();
+    raw_block
+        .trim_end()
+        .lines()
+        .skip(1)
+        .map(|line| {
+            parse_line(if line.contains("(") {
+                re_par.captures(line).map(|c| c.extract().1).unwrap()
+            } else {
+                let [units, hp, damage, damage_type, initative] =
+                    re.captures(line).map(|c| c.extract().1).unwrap();
+                [units, hp, "", damage, damage_type, initative]
             })
         })
-        .map(|arr| arr.into())
-        .collect();
+        .collect()
+}
 
-    println!("{:?}", content);
-    println!("Part 1: {}", 1);
-    println!("Part 2: {}", 2);
+fn main() -> std::io::Result<()> {
+    let binding = fs::read_to_string("in/d24.txt")?;
+    let (immune, infection) = binding.trim_end().split_once("\n\n").unwrap();
+
+    let immune = parse_block(immune);
+    let infection = parse_block(infection);
+    // println!("{:?}", content);
+    // println!("Part 1: {}", 1);
+    // println!("Part 2: {}", 2);
     Ok(())
 }
--- 2018/aoc/p.py
+++ 2018/aoc/p.py
@@ -3,5 +3,10 @@ import re
 with open("in/d04t.txt") as f:
     data = f.read()
 
+data = "2611 units each with 28641 hit points with an attack that does 21 fire damage at initiative 2"
+data = "1181 units each with 5833 hit points (weak to bludgeoning; immune to slashing, cold) with an attack that does 44 cold damage at initiative 6"
+re.findall( r"(\d+)[a-z\s]+(\d+)[\w\s]+points(?:\s+\((.*?)\))?[a-z\s]+(\d+)\s(\w+)[a-z\s]+(\d+)", data)
 
-re.findall(r"\[([0-9\-\s:]+)\].+(#\d+|falls|wakes)", data)
+
+re.findall(r"^(\d+)[a-z\s]+(\d+)[\w\s]+\(([a-z\s,;]+)\)[a-z\s]+(\d+) (\w+)[a-z\s]+(\d+)", data)
+re.findall(r"^(\d+)\s+units\s+each\s+with\s+(\d+)\s+hit\s+points(?:\s+\((.*?)\))?\s+with\s+an\s+attack\s+that\s+does\s+(\d+)\s+(\w+)\s+damage\s+at\s+initiative\s+(\d+)", data)
