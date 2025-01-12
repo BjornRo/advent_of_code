efecec7e21dddf620a8aa9d6bc3778e850e4cf3e
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-01-12 11:45:17 +0100

    aoc 2018 done

--- 2018/aoc/d25.rs
+++ 2018/aoc/d25.rs
@@ -13,15 +13,53 @@ fn print<T: std::fmt::Debug>(x: T) {
     println!("{:?}", x);
 }
 
+type Point = Vec<isize>;
+
+fn manhattan(a: &Point, b: &Point) -> isize {
+    a.iter().zip(b).fold(0, |acc, (a, b)| acc + (a - b).abs())
+}
+
+fn part1(points: &Vec<Point>) -> usize {
+    let mut graph: HashMap<&Point, Vec<&Point>> = HashMap::new();
+    for i in points {
+        graph.insert(i, vec![]);
+        for j in points {
+            if i == j {
+                continue;
+            }
+            if manhattan(i, j) <= 3 {
+                graph.get_mut(&i).unwrap().push(j);
+                graph.entry(j).or_insert_with(Vec::new).push(i);
+            }
+        }
+    }
+
+    let mut num_constellations: usize = 0;
+    let mut visited: HashSet<&Point> = HashSet::new();
+    for i in points {
+        if visited.contains(&i) {
+            continue;
+        }
+        let mut stack = vec![i];
+        while let Some(point) = stack.pop() {
+            if visited.contains(point) {
+                continue;
+            }
+            visited.insert(point);
+            stack.extend(graph.get(point).unwrap());
+        }
+        num_constellations += 1;
+    }
+    num_constellations
+}
+
 fn main() -> std::io::Result<()> {
-    let data: Vec<Vec<isize>> = fs::read_to_string("in/d25.txt")?
+    let mut data: Vec<Vec<isize>> = fs::read_to_string("in/d25.txt")?
         .trim_end()
         .lines()
         .map(|line| line.split(",").map(|x| x.parse().unwrap()).collect())
         .collect();
 
-    println!("{:?}", data);
-    println!("Part 1: {}", 1);
-    println!("Part 2: {}", 2);
+    println!("Part 1: {}", part1(&data));
     Ok(())
 }
