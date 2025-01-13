cb40c2c754c9e3e6356ec47f0038b18b31ec0770
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-01-13 21:56:54 +0100

    d2

--- 2019/d02.zig
+++ 2019/d02.zig
@@ -9,6 +9,26 @@ const expect = std.testing.expect;
 const time = std.time;
 const Allocator = std.mem.Allocator;
 
+fn machine(allocator: Allocator, ops: []u32, noun: u32, verb: u32) !u32 {
+    var op = try allocator.alloc(u32, ops.len);
+    defer allocator.free(op);
+    @memcpy(op, ops);
+
+    op[1] = noun;
+    op[2] = verb;
+
+    var i: u32 = 0;
+    while (true) {
+        op[op[i + 3]] = switch (op[i]) {
+            1 => op[op[i + 1]] + op[op[i + 2]],
+            2 => op[op[i + 1]] * op[op[i + 2]],
+            99 => return op[0],
+            else => unreachable,
+        };
+        i += 4;
+    }
+}
+
 pub fn main() !void {
     const start = time.nanoTimestamp();
     const writer = std.io.getStdOut().writer();
@@ -17,31 +37,34 @@ pub fn main() !void {
         const elapsed = @as(f128, @floatFromInt(end - start)) / @as(f128, 1_000_000);
         writer.print("\nTime taken: {d:.7}ms\n", .{elapsed}) catch {};
     }
-    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    // defer if (gpa.deinit() == .leak) expect(false) catch @panic("TEST FAIL");
-    // const allocator = gpa.allocator();
-    // var buffer: [70_000]u8 = undefined;
-    // var fba = std.heap.FixedBufferAllocator.init(&buffer);
-    // const allocator = fba.allocator();
+    var buffer: [3_000]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    const allocator = fba.allocator();
 
-    // const filename = try myf.getAppArg(allocator, 1);
-    // const target_file = try std.mem.concat(allocator, u8, &.{ "in/", filename });
-    // const input = try myf.readFile(allocator, target_file);
-    // defer inline for (.{ filename, target_file, input }) |res| allocator.free(res);
-    // const input_attributes = try myf.getInputAttributes(input);
+    const filename = try myf.getAppArg(allocator, 1);
+    const target_file = try std.mem.concat(allocator, u8, &.{ "in/", filename });
+    const input = try myf.readFile(allocator, target_file);
+    defer inline for (.{ filename, target_file, input }) |res| allocator.free(res);
     // End setup
 
-    // std.debug.print("{s}\n", .{input});
-    // std.debug.print("Part 1: {d}\nPart2: {d}\n", .{ 1, 2 });
+    var op_list = std.ArrayList(u32).init(allocator);
+    defer op_list.deinit();
 
-}
+    var in_iter = std.mem.tokenizeScalar(u8, std.mem.trimRight(u8, input, "\n"), ',');
+    while (in_iter.next()) |raw_value| try op_list.append(try std.fmt.parseInt(u32, raw_value, 10));
 
-test "example" {
-    const allocator = std.testing.allocator;
-    var list = std.ArrayList(i8).init(allocator);
-    defer list.deinit();
+    var p2: usize = 0;
+    outer: for (0..100) |i| {
+        for (0..100) |j| {
+            if (try machine(allocator, op_list.items, @intCast(i), @intCast(j)) == 19690720) {
+                p2 = 100 * i + j;
+                break :outer;
+            }
+        }
+    }
 
-    const input = @embedFile("in/d14t.txt");
-    const input_attributes = try myf.getInputAttributes(input);
-    printa(input_attributes);
+    std.debug.print("Part 1: {d}\nPart2: {d}\n", .{
+        try machine(allocator, op_list.items, 12, 2),
+        p2,
+    });
 }
