b9198f0787507f60421b05018db7bd905dca0d10
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-01-16 12:42:30 +0100

    d10 p2 fixing

--- 2019/d10.zig
+++ 2019/d10.zig
@@ -35,7 +35,7 @@ pub fn main() !void {
 
 }
 
-const CT = i8;
+const CT = i16;
 const Point = struct {
     row: CT,
     col: CT,
@@ -50,81 +50,95 @@ const Point = struct {
     fn add(self: Self, o: Point) Point {
         return Self.init(self.row + o.row, self.col + o.col);
     }
+    fn update(self: *Self, o: Point) void {
+        self.row += o.row;
+        self.col += o.col;
+    }
+    fn mul(self: Point, other: Point) Point {
+        return Point.init(self.row * other.row - self.col * other.col, self.row * other.col + self.col * other.row);
+    }
     fn toArr(self: Self) [2]CT {
         return .{ self.row, self.col };
     }
-    fn hash(self: Self) u64 {
-        // return std.hash.CityHash64.hash(&@as([2]u8, @bitCast(self.toArr())));
-        return @intCast(std.hash.uint32(@as(u16, @bitCast(self.toArr()))));
-    }
+    const HashCtx = struct {
+        pub fn hash(_: @This(), key: Self) u32 {
+            return std.hash.uint32(@as(u32, @bitCast(key.toArr())));
+        }
+        pub fn eql(_: @This(), a: Self, b: Self, _: usize) bool {
+            return a.eq(b);
+        }
+    };
 };
 
-const Map = std.HashMap(Point, void, HashCtx, 80);
-const PointList = std.ArrayList(Point);
+const Map = std.ArrayHashMap(Point, void, Point.HashCtx, true);
 
-const HashCtx = struct {
-    pub fn hash(_: @This(), key: Point) u64 {
-        return key.hash();
-    }
-    pub fn eql(_: @This(), a: Point, b: Point) bool {
-        return a.eq(b);
-    }
-};
+pub fn bresenham_collision(grid: Map, a: Point, b: Point) ?Point {
+    const drow: CT = @intCast(@abs(b.row - a.row));
+    const dcol: CT = -@as(CT, @intCast(@abs(b.col - a.col)));
+    const srow: CT = if (a.row < b.row) 1 else -1;
+    const scol: CT = if (a.col < b.col) 1 else -1;
 
-pub fn bresenham(allocator: Allocator, a: Point, b: Point) !PointList {
-    var ra, var ca = a.toArr();
-    const rb, const cb = b.toArr();
-
-    const dr: CT = -@as(CT, @intCast(@abs(rb - ra)));
-    const dc: CT = @as(CT, @intCast(@abs(cb - ca)));
-    const sr: CT = if (ra < rb) 1 else -1;
-    const sc: CT = if (ca < cb) 1 else -1;
-
-    var err = dr + dc;
-
-    var points = PointList.init(allocator);
+    var point = a;
+    var err = drow + dcol;
     while (true) {
-        try points.append(Point.init(ra, ca));
-
-        if (ra == rb and ca == cb) break;
-        const e2 = err * 2;
-        if (e2 >= dc) {
-            if (ra == rb) break; // maybe remove
-            err += dc;
-            ra += sr;
+        const e2 = 2 * err;
+        if (e2 >= dcol) {
+            err += dcol;
+            point.row += srow;
         }
-        if (e2 <= dr) {
-            if (ca == cb) break; // maybe remove
-            err += dr;
-            ca += sc;
+        if (e2 <= drow) {
+            err += drow;
+            point.col += scol;
         }
+        if (point.eq(b)) break;
+        if (grid.contains(point)) return point;
     }
-
-    return points;
+    return null;
 }
 
-fn part1(allocator: Allocator, point_list: []const Point) !void {
-    const p = Point.init(0, 0);
+fn part2(grid: *Map, grid_dim: CT, station_point: Point) !void {
+    var laser_aim: Point = Point.init(0, station_point.col);
+    var direction = Point.init(0, 1);
 
-    var grid = Map.init(allocator);
-    defer grid.deinit();
-    for (point_list) |point| try grid.put(point, {});
-
-    var total: u16 = 0;
-    for (point_list) |point| {
-        if (p.eq(point)) continue;
-
-        const line = try bresenham(allocator, p, point);
-        defer line.deinit();
-
-        for (line.items) |line_p| {
-            if (line_p.eq(p)) continue;
-            if (grid.contains(line_p)) break;
-        } else {
-            total += 1;
+    var vaporized: u16 = 0;
+    while (true) {
+        if (bresenham_collision(grid.*, station_point, laser_aim)) |point| {
+            print(laser_aim);
+            _ = grid.swapRemove(point);
+            vaporized += 1;
+            if (vaporized == 10) {
+                break;
+            }
+            // print(point);
+            std.debug.print("vapor: {d}: {d},{d}\n", .{
+                vaporized,
+                @divFloor(point.col, 9),
+                @divFloor(point.row, 9),
+            });
         }
+        const next_pos = laser_aim.add(direction);
+        if (!(0 <= next_pos.row and next_pos.row < grid_dim and
+            0 <= next_pos.col and next_pos.col < grid_dim))
+        {
+            direction = direction.mul(Point.init(0, -1));
+        }
+        laser_aim.update(direction);
     }
-    print(total);
+
+    // var max_visible: usize = 0;
+    // for (point_list) |p0| {
+    //     var total: u16 = 0;
+    //     for (point_list) |p1| {
+    //         if (p0.eq(p1)) continue;
+
+    //         if (!bresenham_collision(&grid, p0, p1)) total += 1;
+    //     }
+    //     if (total > max_visible) {
+    //         max_visible = total;
+    //         // print(p0);
+    //     }
+    //     // print(total);
+    // }
 }
 
 test "example" {
@@ -135,16 +149,35 @@ test "example" {
     const input = @embedFile("in/d10t.txt");
     const input_attributes = try myf.getInputAttributes(input);
 
-    var point_list = PointList.init(allocator);
-    defer point_list.deinit();
+    var grid = Map.init(allocator);
+    defer grid.deinit();
 
-    var i: i8 = 0;
+    const ENHANCE = 9;
+    var i: CT = 0;
     var in_iter = std.mem.tokenizeSequence(u8, input, input_attributes.delim);
     while (in_iter.next()) |row| {
         for (row, 0..) |e, j| {
-            if (e != '.') try point_list.append(Point.init(i, @intCast(j)));
+            if (e != '.') try grid.put(Point.init(i * ENHANCE, @intCast(j * ENHANCE)), {});
         }
         i += 1;
     }
-    try part1(allocator, point_list.items);
+    const stats = try part1(grid);
+    print(stats);
+    _ = try part2(&grid, @intCast(input_attributes.row_len * ENHANCE), stats.point);
+}
+
+fn part1(grid: Map) !struct { visible: u16, point: Point } {
+    var station_point: Point = undefined;
+    var max_visible: u16 = 0;
+    for (grid.keys()) |p0| {
+        var total: u16 = 0;
+        for (grid.keys()) |p1| {
+            if (!p0.eq(p1) and bresenham_collision(grid, p0, p1) == null) total += 1;
+        }
+        if (total > max_visible) {
+            max_visible = total;
+            station_point = p0;
+        }
+    }
+    return .{ .visible = max_visible, .point = station_point };
 }
