0a15c37bea548720a0d2d4a6b0cd8dd5214c234a
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-01-17 13:03:35 +0100

    d13 p1 finish

--- 2019/d13.zig
+++ 2019/d13.zig
@@ -8,7 +8,50 @@ const prints = myf.printStr;
 const expect = std.testing.expect;
 const Allocator = std.mem.Allocator;
 
+const CT = i16;
 const ProgT = i64;
+
+const Map = std.ArrayHashMap(Point, Tile, Point.HashCtx, true);
+
+const Tile = enum { Empty, Wall, Block, Paddle, Ball };
+
+const Point = struct {
+    row: CT,
+    col: CT,
+
+    const Self = @This();
+    fn init(row: CT, col: CT) Self {
+        return .{ .row = row, .col = col };
+    }
+    fn add(self: Self, o: Point) Point {
+        return Self.init(self.row + o.row, self.col + o.col);
+    }
+    fn eq(self: Self, o: Point) bool {
+        return self.row == o.row and self.col == o.col;
+    }
+    fn update(self: *Self, o: Point) void {
+        self.row += o.row;
+        self.col += o.col;
+    }
+    fn cast(self: Self) [2]u16 {
+        return .{ @intCast(self.row), @intCast(self.col) };
+    }
+    fn mul(self: Point, other: Point) Point {
+        return Point.init(
+            self.row * other.row - self.col * other.col,
+            self.row * other.col + self.col * other.row,
+        );
+    }
+    const HashCtx = struct {
+        pub fn hash(_: @This(), key: Self) u32 {
+            return std.hash.uint32(@bitCast([2]CT{ key.row, key.col }));
+        }
+        pub fn eql(_: @This(), a: Self, b: Self, _: usize) bool {
+            return a.eq(b);
+        }
+    };
+};
+
 const Machine = struct {
     registers: []ProgT,
     input_value: ProgT,
@@ -49,30 +92,53 @@ const Machine = struct {
         self.registers[@intCast(index)] = put_value;
     }
 
-    pub fn run(self: *Self) ProgT {
+    pub fn run(self: *Self) ?CT {
         while (true) {
             switch (self.set_pc_value_get_op()) {
                 1 => self.set_value(3, self.get_value(1, 0) + self.get_value(2, 0)),
                 2 => self.set_value(3, self.get_value(1, 0) * self.get_value(2, 0)),
                 3 => self.set_value(1, self.input_value),
-                4 => self.output = self.get_value(1, 2),
+                4 => return @intCast(self.get_value(1, 2)),
                 5 => self.pc = if (self.get_value(1, 0) != 0) @intCast(self.get_value(2, 0)) else self.pc + 3,
                 6 => self.pc = if (self.get_value(1, 0) == 0) @intCast(self.get_value(2, 0)) else self.pc + 3,
                 7 => self.set_value(3, if (self.get_value(1, 0) < self.get_value(2, 0)) 1 else 0),
                 8 => self.set_value(3, if (self.get_value(1, 0) == self.get_value(2, 0)) 1 else 0),
                 9 => self.relative_base += self.get_value(1, 2),
-                else => return self.output, // 99
+                else => return null, // 99
             }
         }
     }
 };
 
-fn runner(registers: std.ArrayList(ProgT), input_value: ProgT) !ProgT {
-    var regs = try registers.clone();
-    defer regs.deinit();
-    var machine = Machine{ .input_value = input_value, .registers = regs.items };
-    return machine.run();
+fn part1(allocator: Allocator, machine: *Machine) !struct { count: usize, map: Map } {
+    var map = Map.init(allocator);
+    var position = Point.init(0, 0);
+    while (true) {
+        if (machine.run()) |col| position.col = col else break;
+        if (machine.run()) |row| position.row = row else break;
+        if (machine.run()) |tile_id| try map.put(position, @enumFromInt(tile_id)) else break;
+    }
+
+    var count: usize = 0;
+    for (map.values()) |v| {
+        if (v == .Block) count += 1;
+    }
+    return .{ .count = count, .map = map };
 }
+
+fn part2(_: Allocator, machine: *Machine, map: *Map) !usize {
+    machine.registers[0] = 2;
+
+    _ = map;
+
+    // while (true) {
+    //     if (machine.run()) |col| position.col = col else break;
+    //     if (machine.run()) |row| position.row = row else break;
+    //     if (machine.run()) |tile_id| try map.put(position, @enumFromInt(tile_id)) else break;
+    // }
+    return 1;
+}
+
 pub fn main() !void {
     const start = std.time.nanoTimestamp();
     const writer = std.io.getStdOut().writer();
@@ -94,27 +160,23 @@ pub fn main() !void {
     defer inline for (.{ filename, target_file, input }) |res| allocator.free(res);
     // End setup
 
-    var op_list = std.ArrayList(ProgT).init(allocator);
-    try op_list.ensureTotalCapacityPrecise(10000);
-    defer op_list.deinit();
+    var registers = std.ArrayList(ProgT).init(allocator);
+    try registers.ensureTotalCapacityPrecise(10000);
+    defer registers.deinit();
 
     var in_iter = std.mem.tokenizeScalar(u8, std.mem.trimRight(u8, input, "\r\n"), ',');
-    while (in_iter.next()) |raw_value| op_list.appendAssumeCapacity(try std.fmt.parseInt(ProgT, raw_value, 10));
-    for (0..10000 - op_list.items.len) |_| op_list.appendAssumeCapacity(0);
+    while (in_iter.next()) |raw_value| registers.appendAssumeCapacity(try std.fmt.parseInt(ProgT, raw_value, 10));
+    for (0..10000 - registers.items.len) |_| registers.appendAssumeCapacity(0);
 
-    _ = try runner(op_list, 1);
+    var machine = Machine{ .input_value = 0, .registers = registers.items };
+
+    var result = try part1(allocator, &machine);
+    defer result.map.deinit();
+    print(result.count);
+
+    _ = try part2(allocator, &machine, &result.map);
 
     // std.debug.print("{s}\n", .{input});
     // try writer.print("Part 1: {d}\nPart 2: {d}\n", .{ 1, 2 });
 
-}
-
-test "example" {
-    const allocator = std.testing.allocator;
-    var list = std.ArrayList(i8).init(allocator);
-    defer list.deinit();
-
-    const input = @embedFile("in/d14t.txt");
-    const input_attributes = try myf.getInputAttributes(input);
-    print(input_attributes);
 }
