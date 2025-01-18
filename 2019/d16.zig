e7fa8690e05897e9cb653974c0b88cba484391ad
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-01-18 17:03:57 +0100

    d16

--- 2019/d16.zig
+++ 2019/d16.zig
@@ -1,45 +1,13 @@
 const std = @import("std");
 const myf = @import("mylib/myfunc.zig");
-const Deque = @import("mylib/deque.zig").Deque;
-const PriorityQueue = std.PriorityQueue;
-const printd = std.debug.print;
-const print = myf.printAny;
-const prints = myf.printStr;
 const expect = std.testing.expect;
 const Allocator = std.mem.Allocator;
 
-pub fn main() !void {
-    const start = std.time.nanoTimestamp();
-    const writer = std.io.getStdOut().writer();
-    defer {
-        const end = std.time.nanoTimestamp();
-        const elapsed = @as(f128, @floatFromInt(end - start)) / @as(f128, 1_000_000);
-        writer.print("\nTime taken: {d:.7}ms\n", .{elapsed}) catch {};
-    }
-    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    // defer if (gpa.deinit() == .leak) expect(false) catch @panic("TEST FAIL");
-    // const allocator = gpa.allocator();
-    // var buffer: [70_000]u8 = undefined;
-    // var fba = std.heap.FixedBufferAllocator.init(&buffer);
-    // const allocator = fba.allocator();
-
-    // const filename = try myf.getAppArg(allocator, 1);
-    // const target_file = try std.mem.concat(allocator, u8, &.{ "in/", filename });
-    // const input = try myf.readFile(allocator, target_file);
-    // defer inline for (.{ filename, target_file, input }) |res| allocator.free(res);
-    // const input_attributes = try myf.getInputAttributes(input);
-    // End setup
-
-    // std.debug.print("{s}\n", .{input});
-    // try writer.print("Part 1: {d}\nPart 2: {d}\n", .{ 1, 2 });
-
-}
-
-fn FFT(allocator: Allocator, values: []const i16, phase: u32) ![]i16 {
-    var output = try allocator.alloc(i16, values.len);
+fn FFT(allocator: Allocator, values: []const i32, phase: u32) ![]i32 {
+    var output = try allocator.alloc(i32, values.len);
     @memcpy(output, values);
 
-    var iter_arr = try allocator.alloc(i16, values.len);
+    var iter_arr = try allocator.alloc(i32, values.len);
     defer allocator.free(iter_arr);
 
     const pattern: []const i8 = &[_]i8{ 0, 1, 0, -1 };
@@ -47,7 +15,7 @@ fn FFT(allocator: Allocator, values: []const i16, phase: u32) ![]i16 {
     for (0..phase) |_| {
         for (1..output.len + 1) |iter| {
             var pidx: u8 = 0;
-            var sum: i16 = 0;
+            var sum: i32 = 0;
             var skip_first = true;
 
             var oidx: u16 = 0;
@@ -73,7 +41,7 @@ fn FFT(allocator: Allocator, values: []const i16, phase: u32) ![]i16 {
     return output;
 }
 
-fn part1(allocator: Allocator, values: []const i16, phase: u32) ![8]u8 {
+fn part1(allocator: Allocator, values: []const i32, phase: u32) ![8]u8 {
     var return_value: [8]u8 = undefined;
     const output = try FFT(allocator, values, phase);
     defer allocator.free(output);
@@ -82,18 +50,66 @@ fn part1(allocator: Allocator, values: []const i16, phase: u32) ![8]u8 {
     return return_value;
 }
 
-test "example" {
-    const allocator = std.testing.allocator;
-    var list = std.ArrayList(i8).init(allocator);
-    defer list.deinit();
+fn part2(allocator: Allocator, values: []const i32, speedup_fac: u8) ![8]u8 {
+    const msg_offset = blk: {
+        var offset: u32 = 0;
+        for (values[0..7]) |v| {
+            offset *= 10;
+            offset += @intCast(v);
+        }
+        break :blk offset;
+    };
 
-    const input = @embedFile("in/d16.txt");
+    var output = try allocator.alloc(i32, values.len * 10_000);
+    defer allocator.free(output);
+    for (0..output.len) |i| output[i] = values[i % values.len];
+
+    var iter_arr = try allocator.alloc(i32, output.len);
+    defer allocator.free(iter_arr);
+
+    for (0..100) |_| {
+        var cumsum: i32 = 0;
+        for (0..output.len / speedup_fac) |i| {
+            const j = output.len - i - 1;
+            cumsum += output[j];
+            iter_arr[j] = @mod(cumsum, 10);
+        }
+        const tmp = output;
+        output = iter_arr;
+        iter_arr = tmp;
+    }
+
+    var return_value: [8]u8 = undefined;
+    for (output[msg_offset .. msg_offset + 8], 0..) |v, i| return_value[i] = @intCast(v + '0');
+    return return_value;
+}
+
+pub fn main() !void {
+    const start = std.time.nanoTimestamp();
+    const writer = std.io.getStdOut().writer();
+    defer {
+        const end = std.time.nanoTimestamp();
+        const elapsed = @as(f128, @floatFromInt(end - start)) / @as(f128, 1_000_000);
+        writer.print("\nTime taken: {d:.7}ms\n", .{elapsed}) catch {};
+    }
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer if (gpa.deinit() == .leak) expect(false) catch @panic("TEST FAIL");
+    const allocator = gpa.allocator();
+
+    const filename = try myf.getAppArg(allocator, 1);
+    const target_file = try std.mem.concat(allocator, u8, &.{ "in/", filename });
+    const input = try myf.readFile(allocator, target_file);
+    defer inline for (.{ filename, target_file, input }) |res| allocator.free(res);
+    // End setup
 
     const input_trim = std.mem.trimRight(u8, input, "\r\n");
 
-    var values = try allocator.alloc(i16, input_trim.len);
+    var values = try allocator.alloc(i32, input_trim.len);
     defer allocator.free(values);
     for (input_trim, 0..) |v, i| values[i] = @intCast(v - '0');
 
-    prints(try part1(allocator, values, 100));
+    try writer.print("Part 1: {s}\nPart 2: {s}\n", .{
+        try part1(allocator, values, 100),
+        try part2(allocator, values, 12), // adjust until it gives correct result
+    });
 }
