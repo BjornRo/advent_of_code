6f4624354060746483b50807ca73b5cbec5f960c
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-01-21 22:42:22 +0100

    d20 cleaning

--- 2019/d20.zig
+++ 2019/d20.zig
@@ -2,92 +2,8 @@ const std = @import("std");
 const myf = @import("mylib/myfunc.zig");
 const Deque = @import("mylib/deque.zig").Deque;
 const PriorityQueue = std.PriorityQueue;
-const printd = std.debug.print;
-const print = myf.printAny;
-const prints = myf.printStr;
 const expect = std.testing.expect;
 const Allocator = std.mem.Allocator;
-
-fn part1(allocator: Allocator, graph: *const Graph, start: u16, target: u16) !u16 {
-    const FrontierState = struct {
-        pos: u16,
-        steps: u16 = 0,
-
-        const Self = @This();
-        fn cmp(_: void, a: Self, b: Self) std.math.Order {
-            if (a.steps < b.steps) return .lt;
-            if (a.steps > b.steps) return .gt;
-            return .eq;
-        }
-    };
-
-    var distances = std.AutoHashMap(u16, u16).init(allocator);
-    var pqueue = PriorityQueue(FrontierState, void, FrontierState.cmp).init(allocator, undefined);
-    defer inline for (.{ pqueue, &distances }) |i| i.deinit();
-
-    try pqueue.add(.{ .pos = start });
-    while (pqueue.removeOrNull()) |*const_state| {
-        if (const_state.pos == target) return const_state.steps - 1;
-
-        for ([_]u16{ const_state.pos, const_state.pos ^ (1 << 15) }) |key| {
-            if (graph.get(key)) |neighbors| {
-                for (neighbors.slice()) |neighbor| {
-                    const neighbor_symbol = neighbor.symbol & 0x7FFF;
-
-                    const new_cost = const_state.steps + neighbor.steps;
-                    if (new_cost >= distances.get(neighbor_symbol) orelse ~@as(u16, 0)) continue;
-                    prints(Convert.u16_twoChar(const_state.pos));
-                    prints(Convert.u16_twoChar(neighbor.symbol));
-                    prints("");
-                    try distances.put(neighbor_symbol, new_cost);
-                    try pqueue.add(.{ .pos = neighbor_symbol, .steps = new_cost + 1 });
-                }
-            }
-        }
-    }
-    return 0;
-}
-
-pub fn main() !void {
-    const start = std.time.nanoTimestamp();
-    const writer = std.io.getStdOut().writer();
-    defer {
-        const end = std.time.nanoTimestamp();
-        const elapsed = @as(f128, @floatFromInt(end - start)) / @as(f128, 1_000_000);
-        writer.print("\nTime taken: {d:.7}ms\n", .{elapsed}) catch {};
-    }
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer if (gpa.deinit() == .leak) expect(false) catch @panic("TEST FAIL");
-    const allocator = gpa.allocator();
-
-    const filename = try myf.getAppArg(allocator, 1);
-    const target_file = try std.mem.concat(allocator, u8, &.{ "in/", filename });
-    const input = try myf.readFile(allocator, target_file);
-    defer inline for (.{ filename, target_file, input }) |res| allocator.free(res);
-    const input_attributes = try myf.getInputAttributes(input);
-    // End setup
-
-    var matrix = std.ArrayList([]const u8).init(allocator);
-    defer matrix.deinit();
-
-    var in_iter = std.mem.tokenizeSequence(u8, input, input_attributes.delim);
-    while (in_iter.next()) |row| try matrix.append(row);
-
-    var graph = try genGraph(allocator, matrix.items);
-    defer graph.deinit();
-    // print(graph.get(Convert.symbol("XF", .Outside)));
-    // print(isOutside(Convert.symbol("ZH", .Outside)));
-    // print(isOutside(Convert.symbol("ZH", .Inside)));
-
-    // for (graph.get(Convert.symbol("ZH", .Inside)).?.slice()) |neighbors| print(neighbors);
-
-    // print(try part1(allocator, &graph, Convert.twoChar_u16("AA"), Convert.twoChar_u16("ZZ")));
-    print(try part2(allocator, &graph, Convert.symbol("AA", .Outside), Convert.symbol("ZZ", .Outside)));
-
-    // std.debug.print("{s}\n", .{input});
-    // try writer.print("Part 1: {d}\nPart 2: {d}\n", .{ 1, 2 });
-
-}
 
 const CT = i16;
 
@@ -96,18 +12,13 @@ const Graph = std.AutoHashMap(u16, GraphValue);
 const Side = enum(u1) { Inside = 0, Outside = 1 };
 
 const Convert = struct {
-    fn u16_twoChar(value: u16) [2]u8 {
-        return @bitCast(value);
-    }
     pub fn twoChar_u16(chars: []const u8) u16 {
         return @bitCast([2]u8{ chars[0], chars[1] });
     }
     pub fn rawSymbol(offset: Point, slice: []const u8, side: Side) u16 {
         var buf: [2]u8 = undefined;
         @memcpy(&buf, slice[0..2]);
-        if (offset.row == -1 or offset.col == -1) {
-            std.mem.reverse(u8, &buf);
-        }
+        if (offset.row == -1 or offset.col == -1) std.mem.reverse(u8, &buf);
         const val: u16 = @intCast(@intFromEnum(side));
         return Convert.twoChar_u16(&buf) | (val << 15);
     }
@@ -131,9 +42,7 @@ const Point = struct {
     fn add(self: Self, o: Point) Point {
         return Self.init(self.row + o.row, self.col + o.col);
     }
-    fn addOffset(self: Self, off: [2]CT) Self {
-        return Self.init(self.row + off[0], self.col + off[1]);
-    }
+
     fn eq(self: Self, o: Self) bool {
         return self.row == o.row and self.col == o.col;
     }
@@ -153,11 +62,6 @@ const Point = struct {
         }
     };
 };
-
-fn isOutside(symbol: u16) bool {
-    const value: u16 = @intCast(@intFromEnum(Side.Outside));
-    return (symbol >> 15) == value;
-}
 
 fn genGraph(allocator: Allocator, matrix: []const []const u8) !Graph {
     var queue = try Deque(struct { pos: Point, steps: u16 = 0 }).init(allocator);
@@ -237,155 +141,115 @@ fn genGraph(allocator: Allocator, matrix: []const []const u8) !Graph {
     return graph;
 }
 
-//1358 too low
-fn part2(allocator: Allocator, graph: *const Graph, start: u16, target: u16) !u32 {
-    const VisitedCtx = struct {
-        pub inline fn init(symbol: u16, next_symbol: u16, depth: u16) u64 {
-            const symbol_: u64 = @intCast(symbol);
-            const next_symbol_: u64 = @intCast(next_symbol);
-            const depth_: u64 = @intCast(depth);
-            return (symbol_ << 32) | (next_symbol_ << 16) | depth_;
-        }
-        pub fn hash(_: @This(), key: u64) u64 {
-            return myf.hash(@intCast(key));
-        }
-        pub fn eql(_: @This(), a: u64, b: u64) bool {
-            return a == b;
+fn part1(allocator: Allocator, graph: *const Graph, start: u16, target: u16) !u16 {
+    const State = struct {
+        pos: u16,
+        steps: u16 = 0,
+
+        const Self = @This();
+        fn cmp(_: void, a: Self, b: Self) std.math.Order {
+            if (a.steps < b.steps) return .lt;
+            if (a.steps > b.steps) return .gt;
+            return .eq;
         }
     };
-    const Visited = std.HashMap(u64, u32, VisitedCtx, 90);
-    const FrontierState = struct {
+
+    var distances = std.AutoHashMap(u16, u16).init(allocator);
+    var pqueue = PriorityQueue(State, void, State.cmp).init(allocator, undefined);
+    defer inline for (.{ pqueue, &distances }) |i| i.deinit();
+
+    try pqueue.add(.{ .pos = start });
+    while (pqueue.removeOrNull()) |*const_state| {
+        if (const_state.pos == target) return const_state.steps - 1;
+
+        for ([_]u16{ const_state.pos, const_state.pos ^ (1 << 15) }) |key| {
+            if (graph.get(key)) |neighbors| {
+                for (neighbors.slice()) |neighbor| {
+                    const neighbor_symbol = neighbor.symbol & 0x7FFF;
+
+                    const new_cost = const_state.steps + neighbor.steps;
+                    if (new_cost >= distances.get(neighbor_symbol) orelse ~@as(u16, 0)) continue;
+                    try distances.put(neighbor_symbol, new_cost);
+                    try pqueue.add(.{ .pos = neighbor_symbol, .steps = new_cost + 1 });
+                }
+            }
+        }
+    }
+    unreachable;
+}
+
+fn part2(allocator: Allocator, graph: *const Graph, start: u16, target: u16) !u32 {
+    const State = struct {
         symbol: u16,
         steps: u32 = 0,
         depth: u16 = 0,
 
         const Self = @This();
         fn cmp(_: void, a: Self, b: Self) std.math.Order {
-            if (a.depth > b.depth) return .gt;
             if (a.depth < b.depth) return .lt;
+            if (a.depth > b.depth) return .gt;
             if (a.steps < b.steps) return .lt;
             if (a.steps > b.steps) return .gt;
             return .eq;
         }
     };
-    var distances = Visited.init(allocator);
-    var pqueue = PriorityQueue(FrontierState, void, FrontierState.cmp).init(allocator, undefined);
-    defer inline for (.{ pqueue, &distances }) |i| i.deinit();
-
-    // const mask = 0x7fff;
+    var pqueue = PriorityQueue(State, void, State.cmp).init(allocator, undefined);
+    defer pqueue.deinit();
 
     try pqueue.add(.{ .symbol = start });
-    while (pqueue.removeOrNull()) |*cstate| {
-
-        // const came_from_outside = isOutside(cstate.symbol);
-        // if (cstate.depth == 0 and cstate.symbol == target) {
-        //     prints("here");
-        //     std.debug.print("{s}->{s} {d} {any}\n", .{
-        //         Convert.u16_twoChar(cstate.symbol & 0x7fff), "X",
-        //         // Convert.u16_twoChar(neighbor.symbol & 0x7fff),
-        //         cstate.depth,                                came_from_outside,
-        //     });
-        //     return cstate.steps - 1;
-        // }
-
-        if (graph.get(cstate.symbol)) |neighbors| {
+    while (pqueue.removeOrNull()) |*state| {
+        if (graph.get(state.symbol)) |neighbors| {
             for (neighbors.slice()) |neighbor| {
-                if (cstate.depth == 0) {
-                    prints(Convert.u16_twoChar(neighbor.symbol & 0x7fff));
-                    if (neighbor.symbol & 0x7fff == target & 0x7fff) return cstate.steps + neighbor.steps;
-                    // if (cstate.symbol != start and cstate.symbol != target) continue;
-                }
-                const new_cost = cstate.steps + neighbor.steps + 1;
+                if (state.depth == 0 and neighbor.symbol == target)
+                    return state.steps + neighbor.steps;
 
-                if (cstate.depth != 0) {
-                    if (neighbor.symbol == start or neighbor.symbol == target) continue;
-                } else {
-                    // if (neighbor.symbol != start and neighbor.symbol != target) continue;
-                }
-                // std.debug.print("b: {s} -> {s}\n", .{ Convert.u16_twoChar(cstate.symbol & 0x7fff), Convert.u16_twoChar(neighbor.symbol & 0x7fff) });
-                // std.debug.print("out: {any}, out: {any}\n", .{ isOutside(cstate.symbol), isOutside(neighbor.symbol) });
-                // std.debug.print("depth: {d}, steps: {d}\n", .{ cstate.depth, cstate.steps });
-                // prints("");
-                // const going_to_outside = isOutside(neighbor.symbol);
-                // if (!came_from_outside and !going_to_outside) continue;
+                var new_depth = state.depth;
+                if (neighbor.symbol >> 15 == @intFromEnum(Side.Outside)) {
+                    if (state.depth == 0) continue;
+                    new_depth -= 1;
+                } else new_depth += 1;
 
-                // if (cstate.depth == 0 and came_from_outside and going_to_outside) continue;
-
-                // if ((const_state.symbol & 0x7fff) == Convert.twoChar_u16("IC")) {
-                //     std.debug.print("{s} -> {s}\n", .{ Convert.u16_twoChar(const_state.symbol & 0x7fff), Convert.u16_twoChar(neighbor.symbol & 0x7fff) });
-                //     // std.debug.print("out: {any}, out: {any}\n", .{ isOutside(const_state.symbol), isOutside(next_symbol) });
-                //     print(const_state.depth + 1);
-                //     prints("");
-                // }
-
-                const came_from_outside = isOutside(cstate.symbol);
-                const going_to_outside = isOutside(neighbor.symbol);
-
-                var new_depth = cstate.depth;
-                if (!came_from_outside and going_to_outside or came_from_outside and going_to_outside) {
-                    if (cstate.depth == 0) continue;
-                    new_depth -|= 1;
-                } else if (came_from_outside and !going_to_outside or !came_from_outside and !going_to_outside) {
-                    new_depth += 1;
-                }
-
-                // const map_key = VisitedCtx.init(cstate.symbol, neighbor.symbol, cstate.depth);
-                // if (new_cost >= distances.get(map_key) orelse ~@as(u32, 0)) {
-                //     continue;
-                // }
-                // if (cstate.depth == 1 and Convert.twoChar_u16("XQ") == (cstate.symbol & 0x7fff)) {
-                //     std.debug.print("m: {s} -> {s}\n", .{ Convert.u16_twoChar(cstate.symbol & 0x7fff), Convert.u16_twoChar(neighbor.symbol & 0x7fff) });
-                //     std.debug.print("out: {any}, out: {any}\n", .{ isOutside(cstate.symbol), isOutside(neighbor.symbol) });
-                //     std.debug.print("depth: {d}, steps: {d} -> {d}\n", .{ new_depth, cstate.steps, new_cost });
-                //     prints("");
-                //     //     //
-                // }
-
-                // try distances.put(map_key, new_cost);
                 try pqueue.add(.{
                     .symbol = neighbor.symbol ^ (1 << 15),
-                    .steps = new_cost,
+                    .steps = state.steps + neighbor.steps + 1,
                     .depth = new_depth,
                 });
-                // for ([2]u16{ neighbor.symbol, neighbor.symbol ^ (1 << 15) }) |next_symbol| {
-                //     var new_depth = cstate.depth;
-                //     if (!came_from_outside and going_to_outside)
-                //         new_depth -|= 1
-                //     else if (came_from_outside and !going_to_outside) new_depth += 1;
-
-                //     // if (new_depth == 0 and came_from_outside and isOutside(next_symbol)) continue;
-                //     if (new_depth == 10 and Convert.twoChar_u16("XQ") == (cstate.symbol & mask)) {
-                //         std.debug.print("b: {s} -> {s}\n", .{ Convert.u16_twoChar(cstate.symbol & 0x7fff), Convert.u16_twoChar(neighbor.symbol & 0x7fff) });
-                //         std.debug.print("out: {any}, out: {any}\n", .{ isOutside(cstate.symbol), isOutside(next_symbol) });
-                //         std.debug.print("depth: {d}, steps: {d} -> {d}\n", .{ new_depth, cstate.steps, new_cost });
-                //         prints("");
-                //         //     //
-                //     }
-                //     // if (new_depth == 111) { // and Convert.twoChar_u16("ZH") == cstate.symbol & 0x7fff
-                //     //     //     //
-                //     //     std.debug.print("b: {s} -> {s}\n", .{ Convert.u16_twoChar(cstate.symbol & 0x7fff), Convert.u16_twoChar(neighbor.symbol & 0x7fff) });
-                //     //     std.debug.print("out: {any}, out: {any}\n", .{ isOutside(cstate.symbol), isOutside(next_symbol) });
-                //     //     std.debug.print("depth: {d}, steps: {d}\n", .{ new_depth, new_cost });
-                //     //     prints("");
-                //     // }
-                //     try pqueue.add(.{
-                //         .symbol = next_symbol,
-                //         .steps = new_cost,
-                //         .depth = new_depth,
-                //     });
-                // }
-                // try pqueue.add(.{
-                //     .symbol = next_symbol,
-                //     .steps = new_cost,
-                //     .depth = if (isOutside(const_state.symbol)) const_state.depth + 1 else const_state.depth -| 1,
-                // });
             }
-
-            // prints(Convert.u16_twoChar(const_state.symbol & 0x7fff));
-            // prints(Convert.u16_twoChar(neighbor.symbol & 0x7fff));
-            // prints("");
-
         }
     }
-    return 123;
+    unreachable;
+}
+
+pub fn main() !void {
+    const start = std.time.nanoTimestamp();
+    const writer = std.io.getStdOut().writer();
+    defer {
+        const end = std.time.nanoTimestamp();
+        const elapsed = @as(f128, @floatFromInt(end - start)) / @as(f128, 1_000_000);
+        writer.print("\nTime taken: {d:.7}ms\n", .{elapsed}) catch {};
+    }
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer if (gpa.deinit() == .leak) expect(false) catch @panic("TEST FAIL");
+    const allocator = gpa.allocator();
+
+    const filename = try myf.getAppArg(allocator, 1);
+    const target_file = try std.mem.concat(allocator, u8, &.{ "in/", filename });
+    const input = try myf.readFile(allocator, target_file);
+    defer inline for (.{ filename, target_file, input }) |res| allocator.free(res);
+    const input_attributes = try myf.getInputAttributes(input);
+    // End setup
+
+    var matrix = std.ArrayList([]const u8).init(allocator);
+    defer matrix.deinit();
+
+    var in_iter = std.mem.tokenizeSequence(u8, input, input_attributes.delim);
+    while (in_iter.next()) |row| try matrix.append(row);
+
+    var graph = try genGraph(allocator, matrix.items);
+    defer graph.deinit();
+
+    try writer.print("Part 1: {d}\nPart 2: {d}\n", .{
+        try part1(allocator, &graph, Convert.twoChar_u16("AA"), Convert.twoChar_u16("ZZ")),
+        try part2(allocator, &graph, Convert.symbol("AA", .Outside), Convert.symbol("ZZ", .Outside)),
+    });
 }
