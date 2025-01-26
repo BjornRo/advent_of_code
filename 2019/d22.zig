fa8f18f26b00cb8e8310097bde3e73d66dab468b
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-01-26 14:09:04 +0100

    d22

--- 2019/d22.zig
+++ 2019/d22.zig
@@ -8,6 +8,69 @@ const prints = myf.printStr;
 const expect = std.testing.expect;
 const Allocator = std.mem.Allocator;
 
+const MoveType = union(enum) {
+    deal_with_inc: i64,
+    cut: i64,
+    deal_into,
+};
+
+fn part1(procedures: *const []const MoveType, len: i128, n_shuffles: usize, card_index: usize) !i128 {
+    var index: i128 = @intCast(card_index);
+    for (0..n_shuffles) |_| {
+        for (procedures.*) |proc| {
+            index = switch (proc) {
+                .deal_into => @mod(len - 1 - index, len),
+                .cut => |value| @mod(index - value, len),
+                .deal_with_inc => |value| @mod(index * value, len),
+            };
+        }
+    }
+    return index;
+}
+
+fn part2(
+    allocator: Allocator,
+    procedures: *const []const MoveType,
+    len: i128,
+    shuffles: i128,
+    card_index: usize,
+) !u64 {
+    var prods_fac: i128 = 1;
+    var adds_fac: i128 = 0;
+
+    var rev_it = std.mem.reverseIterator(procedures.*);
+    while (rev_it.next()) |proc| {
+        switch (proc) {
+            .deal_into => adds_fac = len - 1 - adds_fac,
+            .cut => |value| adds_fac = @mod(adds_fac + value, len),
+            .deal_with_inc => |value| {
+                const inv = try myf.modInv(value, len);
+                prods_fac = @mod(prods_fac * inv, len);
+                adds_fac = @mod(adds_fac * inv, len);
+            },
+        }
+    }
+
+    const python_code =
+        "muls={d};adds={d};index={d};shuffles={d};len={d};\n" ++
+        "mul_pow = pow(muls, shuffles, len)\n" ++
+        "adds = (adds * (1 - mul_pow) * pow(1 - muls, -1, len)) % len\n" ++
+        "print((mul_pow * index + adds) % len, end='')";
+
+    const str = try std.fmt.allocPrint(allocator, python_code, .{ prods_fac, adds_fac, card_index, shuffles, len });
+    defer allocator.free(str);
+
+    var child = std.process.Child.init(&[_][]const u8{ "python", "-c", str }, allocator);
+    child.stdout_behavior = .Pipe;
+    try child.spawn();
+
+    const result = try child.stdout.?.reader().readAllAlloc(allocator, 20);
+    defer allocator.free(result);
+
+    _ = try child.wait();
+    return std.fmt.parseInt(u64, result, 10);
+}
+
 pub fn main() !void {
     const start = std.time.nanoTimestamp();
     const writer = std.io.getStdOut().writer();
@@ -16,39 +79,16 @@ pub fn main() !void {
         const elapsed = @as(f128, @floatFromInt(end - start)) / @as(f128, 1_000_000);
         writer.print("\nTime taken: {d:.7}ms\n", .{elapsed}) catch {};
     }
-    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    // defer if (gpa.deinit() == .leak) expect(false) catch @panic("TEST FAIL");
-    // const allocator = gpa.allocator();
-    // var buffer: [70_000]u8 = undefined;
-    // var fba = std.heap.FixedBufferAllocator.init(&buffer);
-    // const allocator = fba.allocator();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer if (gpa.deinit() == .leak) expect(false) catch @panic("TEST FAIL");
+    const allocator = gpa.allocator();
 
-    // const filename = try myf.getAppArg(allocator, 1);
-    // const target_file = try std.mem.concat(allocator, u8, &.{ "in/", filename });
-    // const input = try myf.readFile(allocator, target_file);
-    // defer inline for (.{ filename, target_file, input }) |res| allocator.free(res);
-    // const input_attributes = try myf.getInputAttributes(input);
-    // End setup
-
-    // std.debug.print("{s}\n", .{input});
-    // try writer.print("Part 1: {d}\nPart 2: {d}\n", .{ 1, 2 });
-
-}
-
-const MoveType = union(enum) {
-    deal_with_inc: i64,
-    cut: i64,
-    deal_into,
-};
-
-// too high 75756230842694, 38515460445925
-test "example" {
-    const allocator = std.testing.allocator;
-    var list = std.ArrayList(i8).init(allocator);
-    defer list.deinit();
-
-    const input = @embedFile("in/d22.txt");
+    const filename = try myf.getAppArg(allocator, 1);
+    const target_file = try std.mem.concat(allocator, u8, &.{ "in/", filename });
+    const input = try myf.readFile(allocator, target_file);
+    defer inline for (.{ filename, target_file, input }) |res| allocator.free(res);
     const input_attributes = try myf.getInputAttributes(input);
+    // End setup
 
     var procedure_list = std.ArrayList(MoveType).init(allocator);
     defer procedure_list.deinit();
@@ -65,57 +105,8 @@ test "example" {
         try procedure_list.append(move);
     }
 
-    var buf: [10007]i128 = undefined;
-    for (0..10007) |i| buf[@intCast(try part1(&procedure_list.items, 10007, 3, i))] = i;
-    std.debug.print("{any}, exp: {d}\n\n", .{ buf[2019..2022], buf[2020] });
-    // 2020(3 shuffles): 7033
-
-    // print(try part1(&procedure_list.items, 10007, 0));
-    try part2(&procedure_list.items, 10007, 3, 2020);
-    print(myf.egcd(21, 5));
-    // try part2(&procedure_list.items, 119315717514047, 101741582076661, 2020);
-}
-
-fn part2(procedures: *const []const MoveType, len: i128, shuffles: i128, card_index: usize) !void {
-    var index: i128 = 0;
-
-    for (procedures.*) |proc| {
-        switch (proc) {
-            .deal_into => {
-                index = len - 1 - index;
-            },
-            .cut => |value| {
-                index = @mod(index - value, len);
-            },
-            .deal_with_inc => |value| {
-                // std.debug.print("{d} {d}\n\n", .{ value, value_ });
-                index = @mod(index * try myf.modInv(value, len), len);
-            },
-        }
-    }
-
-    _ = card_index;
-    _ = shuffles;
-
-    // 19
-    // 119315717514047
-
-    // const xx = @mod(card_index * inc, len) - 1;
-    // print(xx);
-
-    // print(offset);
-}
-
-fn part1(procedures: *const []const MoveType, len: i128, n_shuffles: usize, card_index: usize) !i128 {
-    var index: i128 = @intCast(card_index);
-    for (0..n_shuffles) |_| {
-        for (procedures.*) |proc| {
-            index = switch (proc) {
-                .deal_into => @mod(len - 1 - index, len),
-                .cut => |value| @mod(index - value, len),
-                .deal_with_inc => |value| @mod(index * value, len),
-            };
-        }
-    }
-    return index;
+    try writer.print("Part 1: {d}\nPart 2: {d}\n", .{
+        try part1(&procedure_list.items, 10007, 1, 2019),
+        try part2(allocator, &procedure_list.items, 119315717514047, 101741582076661, 2020),
+    });
 }
