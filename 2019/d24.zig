3c4bf4b3183be662c7ef794fe4f1ca8ece9d1040
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-01-26 21:33:18 +0100

    d24 p2 starting

--- 2019/d24.zig
+++ 2019/d24.zig
@@ -8,45 +8,6 @@ const prints = myf.printStr;
 const expect = std.testing.expect;
 const Allocator = std.mem.Allocator;
 
-pub fn main() !void {
-    const start = std.time.nanoTimestamp();
-    const writer = std.io.getStdOut().writer();
-    defer {
-        const end = std.time.nanoTimestamp();
-        const elapsed = @as(f128, @floatFromInt(end - start)) / @as(f128, 1_000_000);
-        writer.print("\nTime taken: {d:.7}ms\n", .{elapsed}) catch {};
-    }
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer if (gpa.deinit() == .leak) expect(false) catch @panic("TEST FAIL");
-    const allocator = gpa.allocator();
-
-    const filename = try myf.getAppArg(allocator, 1);
-    const target_file = try std.mem.concat(allocator, u8, &.{ "in/", filename });
-    const input = try myf.readFile(allocator, target_file);
-    defer inline for (.{ filename, target_file, input }) |res| allocator.free(res);
-    const input_attributes = try myf.getInputAttributes(input);
-    // End setup
-
-    const dim = input_attributes.row_len;
-
-    var in_iter = std.mem.tokenizeSequence(u8, input, input_attributes.delim);
-    const matrix = blk: {
-        var i: u8 = 0;
-        const matrix = try myf.initValueMatrix(allocator, dim, dim, @as(u8, '.'));
-        while (in_iter.next()) |row| {
-            @memcpy(matrix[i], row);
-            i += 1;
-        }
-        break :blk matrix;
-    };
-    defer myf.freeMatrix(allocator, matrix);
-    try part1(allocator, matrix);
-
-    // std.debug.print("{s}\n", .{input});
-    // try writer.print("Part 1: {d}\nPart 2: {d}\n", .{ 1, 2 });
-
-}
-
 pub fn matrixToKey(mat: []const []const u8) u32 {
     var key: u32 = 0;
     for (mat) |row| for (row) |col| {
@@ -64,7 +25,6 @@ const HashCtx = struct {
     }
 };
 const Visited = std.HashMap(u32, void, HashCtx, 90);
-
 fn part1(allocator: Allocator, const_matrix: []const []const u8) !void {
     var matrix = try myf.copyMatrix(allocator, const_matrix);
     var tmp = try myf.copyMatrix(allocator, const_matrix);
@@ -108,6 +68,106 @@ fn part1(allocator: Allocator, const_matrix: []const []const u8) !void {
             }
         }
     }
+}
 
-    //
+pub fn main() !void {
+    const start = std.time.nanoTimestamp();
+    const writer = std.io.getStdOut().writer();
+    defer {
+        const end = std.time.nanoTimestamp();
+        const elapsed = @as(f128, @floatFromInt(end - start)) / @as(f128, 1_000_000);
+        writer.print("\nTime taken: {d:.7}ms\n", .{elapsed}) catch {};
+    }
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer if (gpa.deinit() == .leak) expect(false) catch @panic("TEST FAIL");
+    const allocator = gpa.allocator();
+
+    const filename = try myf.getAppArg(allocator, 1);
+    const target_file = try std.mem.concat(allocator, u8, &.{ "in/", filename });
+    const input = try myf.readFile(allocator, target_file);
+    defer inline for (.{ filename, target_file, input }) |res| allocator.free(res);
+    const input_attributes = try myf.getInputAttributes(input);
+    // End setup
+
+    const matrix = allocator.alloc([]const u8, input_attributes.row_len);
+    defer allocator.free(matrix);
+
+    var in_iter = std.mem.tokenizeSequence(u8, input, input_attributes.delim);
+    for (matrix) |*row| row.* = in_iter.next().?;
+
+    // try part1(allocator, matrix);
+
+    var depth = Depth.init(allocator);
+    defer {
+        var v_it = depth.valueIterator();
+        while (v_it.next()) |m| myf.freeMatrix(allocator, m);
+        depth.deinit();
+    }
+    try depth.put(0, try myf.copyMatrix(allocator, matrix));
+
+    const neighbors = DepthNeighbors.init(0, 0, 0, 0);
+    try part2(allocator, @intCast(matrix.len), &depth, 0, neighbors, neighbors);
+
+    // std.debug.print("{s}\n", .{input});
+    // try writer.print("Part 1: {d}\nPart 2: {d}\n", .{ 1, 2 });
+
+}
+
+const Depth = std.AutoHashMap(i16, [][]u8, HashCtx, 90);
+const DepthNeighbors = struct {
+    up: u8,
+    down: u8,
+    left: u8,
+    right: u8,
+
+    const Self = @This();
+    fn init(up: u8, down: u8, left: u8, right: u8) Self {
+        return DepthNeighbors{ .up = up, .down = down, .left = left, .right = right };
+    }
+};
+
+fn part2(
+    allocator: Allocator,
+    dim: u8,
+    depth_map: *Depth,
+    depth: i16,
+    depth_in: DepthNeighbors,
+    depth_out: DepthNeighbors,
+) !void {
+    const map_result = try depth_map.getOrPut(depth);
+    if (!map_result.found_existing) {
+        map_result.value_ptr.* = myf.initValueMatrix(allocator, dim, dim, @as(u8, '.'));
+    }
+    const matrix = map_result.value_ptr;
+    var tmp = try myf.copyMatrix(allocator, matrix.*);
+    defer {
+        myf.freeMatrix(allocator, map_result.value_ptr.*);
+        map_result.value_ptr.* = tmp;
+    }
+
+    const half_dim = dim / 2;
+    for (0..matrix.len) |i| {
+        for (matrix[i], 0..) |elem, j| {
+            if (i == half_dim and j == half_dim) continue;
+            var bugs: u8 = 0;
+            for (myf.getNextPositions(@as(i8, @intCast(i)), @as(i8, @intCast(j)))) |next_pos| {
+                const nr, const nc = next_pos;
+                if (nr < 0) {
+                    if (depth_out.up == '#') bugs += 1;
+                } else if (dim < nr) {
+                    if (depth_out.down == '#') bugs += 1;
+                } else if (nc < 0) {
+                    if (depth_out.left == '#') bugs += 1;
+                } else if (dim < nc) {
+                    if (depth_out.right == '#') bugs += 1;
+                }
+            }
+            tmp[i][j] = if (elem == '#' and bugs != 1)
+                '.'
+            else if (elem == '.' and (bugs == 1 or bugs == 2))
+                '#'
+            else
+                elem;
+        }
+    }
 }
