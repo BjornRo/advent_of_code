99bca0f9c02dbf3f35d2e83a7353b7a6d839491f
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-01-26 22:41:02 +0100

    d24 p2 starting

--- 2019/d24.zig
+++ 2019/d24.zig
@@ -89,7 +89,7 @@ pub fn main() !void {
     const input_attributes = try myf.getInputAttributes(input);
     // End setup
 
-    const matrix = allocator.alloc([]const u8, input_attributes.row_len);
+    const matrix = try allocator.alloc([]const u8, input_attributes.row_len);
     defer allocator.free(matrix);
 
     var in_iter = std.mem.tokenizeSequence(u8, input, input_attributes.delim);
@@ -100,7 +100,7 @@ pub fn main() !void {
     var depth = Depth.init(allocator);
     defer {
         var v_it = depth.valueIterator();
-        while (v_it.next()) |m| myf.freeMatrix(allocator, m);
+        while (v_it.next()) |m| myf.freeMatrix(allocator, m.*);
         depth.deinit();
     }
     try depth.put(0, try myf.copyMatrix(allocator, matrix));
@@ -113,7 +113,7 @@ pub fn main() !void {
 
 }
 
-const Depth = std.AutoHashMap(i16, [][]u8, HashCtx, 90);
+const Depth = std.AutoHashMap(i16, [][]u8);
 // const DepthNeighbors = struct {
 //     up: u8,
 //     down: u8,
@@ -135,43 +135,92 @@ fn part2(
 ) !void {
     const map_result = try depth_map.getOrPut(depth);
     if (!map_result.found_existing) {
-        map_result.value_ptr.* = myf.initValueMatrix(allocator, dim, dim, @as(u8, '.'));
+        // map_result.value_ptr.* = myf.initValueMatrix(allocator, dim, dim, @as(u8, '.'));
+        return;
     }
-    const matrix = map_result.value_ptr;
-    var tmp = try myf.copyMatrix(allocator, matrix.*);
+    const matrix = map_result.value_ptr.*;
+    var tmp = try myf.copyMatrix(allocator, matrix);
     defer {
         myf.freeMatrix(allocator, map_result.value_ptr.*);
         map_result.value_ptr.* = tmp;
     }
 
-    const depth_plus: ?[]const []const u8 = null;
-    const depth_minus: ?[]const []const u8 = null;
-
-    if (@abs(depth) <= (minutes / 2)) {}
+    var depth_plus: ?[]const []const u8 = null;
+    var depth_minus: ?[]const []const u8 = null;
+    if (@abs(depth + 1) <= (minutes / 2)) {
+        const depth_res = try depth_map.getOrPut(depth + 1);
+        if (!depth_res.found_existing) {
+            depth_res.value_ptr.* = try myf.initValueMatrix(allocator, dim, dim, @as(u8, '.'));
+        }
+        depth_plus = depth_res.value_ptr.*;
+    }
+    if (@abs(depth - 1) <= (minutes / 2)) {
+        const depth_res = try depth_map.getOrPut(depth - 1);
+        if (!depth_res.found_existing) {
+            depth_res.value_ptr.* = try myf.initValueMatrix(allocator, dim, dim, @as(u8, '.'));
+        }
+        depth_minus = depth_res.value_ptr.*;
+    }
+    std.debug.print("{d} {any} {any}\n", .{ depth, depth_plus, depth_minus });
 
     const half_dim = dim / 2;
     for (0..matrix.len) |i| {
         for (matrix[i], 0..) |elem, j| {
             if (i == half_dim and j == half_dim) continue;
             var bugs: u8 = 0;
-            for (myf.getNextPositions(@as(i8, @intCast(i)), @as(i8, @intCast(j)))) |next_pos| {
-                const nr, const nc = next_pos;
-                if (nr < 0) {
-                    if (depth_out.up == '#') bugs += 1;
-                } else if (dim < nr) {
-                    if (depth_out.down == '#') bugs += 1;
-                } else if (nc < 0) {
-                    if (depth_out.left == '#') bugs += 1;
-                } else if (dim < nc) {
-                    if (depth_out.right == '#') bugs += 1;
+            defer {
+                tmp[i][j] = if (elem == '#' and bugs != 1)
+                    '.'
+                else if (elem == '.' and (bugs == 1 or bugs == 2))
+                    '#'
+                else
+                    elem;
+            }
+            const ii = @as(i8, @intCast(i));
+            const jj = @as(i8, @intCast(j));
+            for (myf.getNeighborOffset(i8)) |offset| {
+                const dr, const dc = offset;
+                const nr = ii + dr;
+                const nc = jj + dc;
+                if (0 <= nr and nr < dim and 0 <= nc and nc < dim) {
+                    if (nc == half_dim and nr == half_dim) {
+                        if (depth_minus) |next_grid| {
+                            if (dc == -1) {
+                                for (0..5) |k| {
+                                    if (next_grid[k][dim - 1] == '#') bugs += 1;
+                                }
+                            } else if (dc == 1) {
+                                for (0..5) |k| {
+                                    if (next_grid[k][0] == '#') bugs += 1;
+                                }
+                            } else if (dr == -1) {
+                                for (0..5) |k| {
+                                    if (next_grid[dim - 1][k] == '#') bugs += 1;
+                                }
+                            } else if (dr == 1) {
+                                for (0..5) |k| {
+                                    if (next_grid[0][k] == '#') bugs += 1;
+                                }
+                            }
+                        }
+                    } else if (matrix[@intCast(nr)][@intCast(nc)] == '#') bugs += 1;
+                } else {
+                    if (depth_plus) |next_grid| {
+                        if (nr < 0) { // up
+                            if (next_grid[half_dim - 1][half_dim] == '#') bugs += 1;
+                        } else if (dim < nr) { // down
+                            if (next_grid[half_dim + 1][half_dim] == '#') bugs += 1;
+                        } else if (nc < 0) { // left
+                            if (next_grid[half_dim][half_dim - 1] == '#') bugs += 1;
+                        } else if (dim < nc) { // right
+                            if (next_grid[half_dim][half_dim + 1] == '#') bugs += 1;
+                        }
+                    }
                 }
             }
-            tmp[i][j] = if (elem == '#' and bugs != 1)
-                '.'
-            else if (elem == '.' and (bugs == 1 or bugs == 2))
-                '#'
-            else
-                elem;
         }
     }
+
+    try part2(allocator, dim, depth_map, depth + 1, minutes);
+    try part2(allocator, dim, depth_map, depth - 1, minutes);
 }
