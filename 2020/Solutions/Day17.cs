fdf80305928773259246d80d1675bdc076a7aad2
BjornRo <shovitswe@gmail.com>
2025-01-30 16:28:59 +0100

    d17

--- 2020/Solutions/Day17.cs
+++ 2020/Solutions/Day17.cs
@@ -7,7 +7,7 @@ public class Day17
         HashSet<(int, int, int)> cube = InitCube(File.ReadAllLines("in/d17.txt"));
 
         Console.WriteLine($"Part 1: {Part1([.. cube], 6)}");
-        // Console.WriteLine($"Part 2: {Part2([.. matrix.Select(row => row.ToCharArray())])}");
+        Console.WriteLine($"Part 2: {Part2(InitHypercube(cube), 6)}");
     }
 
     static HashSet<(int, int, int)> InitCube(string[] matrix)
@@ -19,19 +19,25 @@ public class Day17
         return cube;
     }
 
-
-    static IEnumerable<(int, int, int)> GetNeighbors((int, int, int) t)
+    static HashSet<(int, int, int, int)> InitHypercube(HashSet<(int, int, int)> cube)
     {
-        var (i, j, k) = t;
-        for (int di = -1; di <= 1; di++)
-            for (int dj = -1; dj <= 1; dj++)
-                for (int dk = -1; dk <= 1; dk++)
-                    if (di != 0 || dj != 0 || dk != 0)
-                        yield return (i + di, j + dj, k + dk);
+        HashSet<(int, int, int, int)> hypercube = [];
+        foreach (var (i, j, k) in cube) hypercube.Add((i, j, k, 0));
+        return hypercube;
     }
 
     static int Part1(HashSet<(int, int, int)> cube, int cycles)
     {
+        static IEnumerable<(int, int, int)> GetNeighbors((int, int, int) t)
+        {
+            var (i, j, k) = t;
+            for (int di = -1; di <= 1; di++)
+                for (int dj = -1; dj <= 1; dj++)
+                    for (int dk = -1; dk <= 1; dk++)
+                        if (di != 0 || dj != 0 || dk != 0)
+                            yield return (i + di, j + dj, k + dk);
+        }
+
         HashSet<(int, int, int)> tmp_cube = [];
 
         for (int cycle = 0; cycle < cycles; cycle++)
@@ -71,37 +77,61 @@ public class Day17
         }
         return cube.Count;
     }
+
+
+    static int Part2(HashSet<(int, int, int, int)> hypercube, int cycles)
+    {
+        static IEnumerable<(int, int, int, int)> GetNeighbors((int, int, int, int) t)
+        {
+            var (i, j, k, w) = t;
+            for (int di = -1; di <= 1; di++)
+                for (int dj = -1; dj <= 1; dj++)
+                    for (int dk = -1; dk <= 1; dk++)
+                        for (int dw = -1; dw <= 1; dw++)
+                            if (di != 0 || dj != 0 || dk != 0 || dw != 0)
+                                yield return (i + di, j + dj, k + dk, w + dw);
+        }
+
+        HashSet<(int, int, int, int)> tmp_hypercube = [];
+
+        for (int cycle = 0; cycle < cycles; cycle++)
+        {
+            tmp_hypercube.Clear();
+            int mini, maxi, minj, maxj, mink, maxk, minw, maxw;
+            mini = minj = mink = minw = int.MaxValue;
+            maxi = maxj = maxk = maxw = int.MinValue;
+
+            foreach (var (i, j, k, w) in hypercube)
+            {
+                if (i < mini) mini = i;
+                if (j < minj) minj = j;
+                if (k < mink) mink = k;
+                if (w < minw) minw = w;
+                if (i > maxi) maxi = i;
+                if (j > maxj) maxj = j;
+                if (k > maxk) maxk = k;
+                if (w > maxw) maxw = w;
+            }
+            for (int i = mini - 1; i <= maxi + 1; i++)
+                for (int j = minj - 1; j <= maxj + 1; j++)
+                    for (int k = mink - 1; k <= maxk + 1; k++)
+                        for (int w = minw - 1; w <= maxw + 1; w++)
+                        {
+                            var neighbors = 0;
+                            var pos = (i, j, k, w);
+                            foreach (var neighbor in GetNeighbors(pos))
+                                if (hypercube.Contains(neighbor))
+                                    neighbors += 1;
+                            var active = hypercube.Contains(pos);
+                            if (active)
+                            {
+                                if (neighbors == 2 || neighbors == 3) tmp_hypercube.Add(pos);
+                            }
+                            else
+                                if (neighbors == 3) tmp_hypercube.Add(pos);
+                        }
+            (tmp_hypercube, hypercube) = (hypercube, tmp_hypercube);
+        }
+        return hypercube.Count;
+    }
 }
-
-
-// static List<(int, int)>[,] GetVisibleSeats(char[][] grid)
-// {
-//     int rows = grid.Length;
-//     int cols = grid[0].Length;
-//     var directions = new (int, int)[]
-//         { (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1) };
-
-//     var visibleSeats = new List<(int, int)>[rows, cols];
-//     for (int r = 0; r < rows; r++)
-//         for (int c = 0; c < cols; c++)
-//         {
-//             if (grid[r][c] == '.') continue;
-//             visibleSeats[r, c] = [];
-
-//             foreach (var (dr, dc) in directions)
-//             {
-//                 int nr = r + dr, nc = c + dc;
-//                 while (0 <= nr && nr < rows && 0 <= nc && nc < cols)
-//                 {
-//                     if (grid[nr][nc] == 'L' || grid[nr][nc] == '#')
-//                     {
-//                         visibleSeats[r, c].Add((nr, nc));
-//                         break;
-//                     }
-//                     nr += dr;
-//                     nc += dc;
-//                 }
-//             }
-//         }
-//     return visibleSeats;
-// }
