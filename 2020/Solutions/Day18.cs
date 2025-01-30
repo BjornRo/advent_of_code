ae76a8e45b4f06d7e94cc8e53395d724ad4cbd0f
BjornRo <shovitswe@gmail.com>
2025-01-30 18:31:22 +0100

    d18

--- 2020/Solutions/Day18.cs
+++ 2020/Solutions/Day18.cs
@@ -1,18 +1,70 @@
+using System.Text.RegularExpressions;
+
 namespace aoc.Solutions;
 
-public class Day18
+public partial class Day18
 {
     public static void Solve()
     {
-        string[] data = File.ReadAllLines("in/d17.txt");
+        string[] data = File.ReadAllLines("in/d18.txt");
 
-        Console.WriteLine($"Part 1: {Part1(data)}");
-        // Console.WriteLine($"Part 2: {Part2(InitHypercube(data), 6)}");
+        Console.WriteLine($"Part 1: {data.Aggregate(0L, (acc, str) => acc + Eval(str, true))}");
+        Console.WriteLine($"Part 2: {data.Aggregate(0L, (acc, str) => acc + Eval(str, false))}");
     }
 
-    static int Part1(in string[] data)
+    static long Eval(string expr, bool ignorePrecedence)
     {
+        int GetPrecedence(string op)
+        {
+            if (ignorePrecedence) return 0;
+            if (op == "*") return 1;
+            if (op == "+") return 2;
+            return 0;
+        }
+        string[] tokens = [.. MyRegex().Matches(expr).Select(e => e.Value)];
 
-        return 1;
+        Stack<string> stack = [];
+        List<string> output = [];
+
+        foreach (var token in tokens)
+        {
+            if (char.IsDigit(token[0])) output.Add(token);
+            else if (token.Equals("(")) stack.Push(token);
+            else if (token == ")")
+            {
+                while (stack.Count > 0 && stack.Peek() != "(") output.Add(stack.Pop());
+                stack.Pop();
+            }
+            else
+            {
+                while (stack.Count > 0 && stack.Peek() != "("
+                    && GetPrecedence(stack.Peek()) >= GetPrecedence(token))
+                    output.Add(stack.Pop());
+                stack.Push(token);
+            }
+        }
+        while (stack.Count > 0) output.Add(stack.Pop());
+
+        Stack<long> evalStack = [];
+        foreach (var token in output)
+            if (char.IsDigit(token[0])) evalStack.Push(long.Parse(token));
+            else
+            {
+                var b = evalStack.Pop();
+                var a = evalStack.Pop();
+                switch (token)
+                {
+                    case "+":
+                        evalStack.Push(a + b);
+                        break;
+                    case "*":
+                        evalStack.Push(a * b);
+                        break;
+                }
+            }
+        return evalStack.Pop();
     }
+
+    [GeneratedRegex(@"\d+(\.\d*)?|\+|\-|\*|\/|\(|\)")]
+    private static partial Regex MyRegex();
 }
