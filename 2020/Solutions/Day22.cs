bb132b600fc8b6dcb539a9d0f15a72c4e6e0866a
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-02-01 01:29:07 +0100

    d22

--- 2020/Solutions/Day22.cs
+++ 2020/Solutions/Day22.cs
@@ -29,29 +29,8 @@ public partial class Day22
         string[] data = File.ReadAllText("in/d22.txt").Split(["\r\n\r\n", "\n\n"], SPLITOPT);
         var players = Parse(data);
 
-        // var p1 = Part1(players);
-        var p2 = Part2(players);
-        // Console.WriteLine($"Part 1: {p1}");
-        Console.WriteLine($"Part 2: {p2}");
-    }
-
-    static int Part2(in (int[] p1, int[] p2) players)
-    {
-        Queue<int> p1 = new([.. players.p1]);
-        Queue<int> p2 = new([.. players.p2]);
-
-        while (p1.Count != 0 && p2.Count != 0)
-        {
-            var play1 = p1.Dequeue();
-            var play2 = p2.Dequeue();
-            List<int> plays = [play1, play2];
-            var roundWinner = plays[0] > plays[1] ? p1 : p2;
-
-            plays.Sort((a, b) => b.CompareTo(a));
-            foreach (var c in plays) roundWinner.Enqueue(c);
-        }
-        var winner = p1.Count != 0 ? p1 : p2;
-        return winner.Reverse().Select((e, i) => e * (i + 1)).Sum();
+        Console.WriteLine($"Part 1: {Part1(players)}");
+        Console.WriteLine($"Part 2: {Part2(players).Item2}");
     }
 
     static int Part1(in (int[] p1, int[] p2) players)
@@ -71,5 +50,49 @@ public partial class Day22
         }
         var winner = p1.Count != 0 ? p1 : p2;
         return winner.Reverse().Select((e, i) => e * (i + 1)).Sum();
+    }
+
+    static (int, int) Part2(in (int[] p1, int[] p2) players)
+    {
+        Queue<int> p1 = new([.. players.p1]);
+        Queue<int> p2 = new([.. players.p2]);
+
+        HashSet<string> visited = [];
+        while (p1.Count != 0 && p2.Count != 0)
+        {
+            var key = string.Join(",", p1) + "|" + string.Join(",", p2);
+            if (visited.Contains(key)) return (1, 0);
+            visited.Add(key);
+
+            var play1 = p1.Dequeue();
+            var play2 = p2.Dequeue();
+
+            int[] plays;
+            Queue<int> roundWinner;
+            if (play1 <= p1.Count && play2 <= p2.Count)
+                if (Part2((p1.ToArray()[0..play1], p2.ToArray()[0..play2])).Item1 == 1)
+                {
+                    plays = [play1, play2];
+                    roundWinner = p1;
+                }
+                else
+                {
+                    plays = [play2, play1];
+                    roundWinner = p2;
+                }
+            else if (play1 > play2)
+            {
+                plays = [play1, play2];
+                roundWinner = p1;
+            }
+            else
+            {
+                plays = [play2, play1];
+                roundWinner = p2;
+            }
+            foreach (var c in plays) roundWinner.Enqueue(c);
+        }
+        var (winnerID, winner) = p1.Count != 0 ? (1, p1) : (2, p2);
+        return (winnerID, winner.Reverse().Select((e, i) => e * (i + 1)).Sum());
     }
 }
