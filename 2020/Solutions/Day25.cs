20354241da245d2e387eee89c65d1d052769faa1
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-02-02 00:05:18 +0100

    2020 done

--- 2020/Solutions/Day25.cs
+++ 2020/Solutions/Day25.cs
@@ -10,23 +10,18 @@ public partial class Day25
 
     static long Cracker(long[] keys)
     {
-        List<long> loopSizes = [];
         long value = 1;
-        foreach (var key in keys)
+        long i = 0;
+        while (true)
         {
-            long i = 0;
-            while (true)
-            {
-                value *= 7;
-                value %= 20201227;
-                i++;
-                if (value == key) break;
-            }
-            loopSizes.Add(i);
+            value *= 7;
+            value %= 20201227;
+            i++;
+            if (value == keys[0]) break;
         }
 
         value = 1;
-        for (int j = 0; j < loopSizes[0]; j++)
+        for (int j = 0; j < i; j++)
         {
             value *= keys[1];
             value %= 20201227;
