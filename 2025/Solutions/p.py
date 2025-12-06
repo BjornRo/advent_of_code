0870cc5f38581ce5abf7878a06d80d1955eb31d3
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-12-06 15:43:26 +0100

    golfing it harder

--- 2025/Solutions/p.py
+++ 2025/Solutions/p.py
@@ -3,6 +3,4 @@ from itertools import groupby
 with open("in/d06.txt") as f:
     d = f.read().strip().split("\n")
 
-total = 0
-for o, v in zip(d[-1].replace(" ", ""), [list(x) for b,x in groupby(map(lambda x: "".join(x).strip(), (zip(*d[:-1]))), str.isdigit) if b]):
-    total += eval(o.join(v))
+total = sum(eval(o.join(v)) for o,v in zip(d[-1].replace(" ", ""), [list(x) for b,x in groupby(map(lambda x: "".join(x).strip(), (zip(*d[:-1]))), str.isdigit) if b]))
