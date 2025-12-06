28db0af662c75a364fc3dbd31295a836791ce7c5
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-12-06 15:41:15 +0100

    golfing it harder

--- 2025/Solutions/p.py
+++ 2025/Solutions/p.py
@@ -1,10 +1,8 @@
-from functools import reduce
 from itertools import groupby
-from operator import add, mul
 
 with open("in/d06.txt") as f:
     d = f.read().strip().split("\n")
 
 total = 0
-for o, v in zip(d[-1].replace(" ", ""), [[int(y) for y in x] for b,x in groupby(map(lambda x: "".join(x).strip(), (zip(*d[:-1]))), str.isdigit) if b]):
-    total += reduce(add, v, 0) if o == "+" else reduce(mul, v, 1)
+for o, v in zip(d[-1].replace(" ", ""), [list(x) for b,x in groupby(map(lambda x: "".join(x).strip(), (zip(*d[:-1]))), str.isdigit) if b]):
+    total += eval(o.join(v))
