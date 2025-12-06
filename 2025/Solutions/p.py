752d48b38ff53a87bb091aecd84bcde75e9ee611
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-12-06 14:49:32 +0100

    golfing python d6

--- 2025/Solutions/p.py
+++ 2025/Solutions/p.py
@@ -1,5 +1,13 @@
+from functools import reduce
+from itertools import batched
+from operator import add, mul
+
 with open("in/d06t.txt") as f:
     d = f.read().strip()
 
-for i in list(map(lambda x: "".join(x),(zip(*d.split("\n"))))):
-    print(list(i))
+n, ops = d.strip().rsplit("\n", 1)
+op = ops.replace(" ", "")
+
+total = 0
+for o, v in zip(op, [[int(y) for y in x if y.strip()] for x in batched(map("".join, (zip(*n.split("\n")))), len(op))]):
+    total += reduce(add, v, 0) if o == "+" else reduce(mul, v, 1)
