f2f1db56c8f9e07f4b9f22895109f72590da84dd
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-12-08 15:37:16 +0100

    d8

--- 2025/Solutions/Day08.cs
+++ 2025/Solutions/Day08.cs
@@ -1,94 +1,118 @@
+using System.Collections.Immutable;
+
 namespace aoc.Solutions;
 
 public class Day08
 {
-    enum OpCode
+    readonly struct Junction(int id, in long x, long y, long z)
     {
-        Acc,
-        Jmp,
-        Nop,
+        public readonly int id = id;
+        public readonly long x = x;
+        public readonly long y = y;
+        public readonly long z = z;
 
-    }
-
-    readonly struct Instruction
-    {
-        public readonly OpCode Op;
-        public readonly int Value;
-
-        public Instruction(string op, string value)
+        public double Euclidean(Junction o)
         {
-            Op = op switch
-            {
-                "acc" => OpCode.Acc,
-                "jmp" => OpCode.Jmp,
-                _ => OpCode.Nop,
-            };
-            Value = int.Parse(value);
+            long dx = o.x - x;
+            long dy = o.y - y;
+            long dz = o.z - z;
+
+            return Math.Sqrt(dx * dx + dy * dy + dz * dz);
         }
 
-        public Instruction(OpCode op, int value)
+        public (Junction, Junction) Key(Junction o)
         {
-            Op = op;
-            Value = value;
+            var a = this;
+            var b = o;
+            if (b.id < a.id) (a, b) = (b, a);
+            return (a, b);
+        }
+
+
+        public void P()
+        {
+            Console.WriteLine($"{x},{y},{z}");
+        }
+        public string W()
+        {
+            return $"{x},{y},{z}";
         }
     }
 
     public static void Solve()
     {
-        List<Instruction> instructions = [];
-        foreach (var rawInstruction in File.ReadAllLines("in/d08.txt"))
-        {
-            string[] splitRawIns = rawInstruction.Split(" ");
-            instructions.Add(new Instruction(splitRawIns[0], splitRawIns[1]));
-        }
+        ImmutableArray<Junction> junctions = [.. File.ReadAllLines("in/d08.txt")
+            .Select((x, index) => {
+                var result = x.Split(",").Select(long.Parse).ToArray();
+                return new Junction(index, result[0], result[1], result[2]);
+            })
+        ];
 
-        Console.WriteLine($"Part 1: {Machine(instructions).Item2}");
-        Console.WriteLine($"Part 2: {Part2(instructions)}");
+        var (p1, p2) = Solution(junctions, 1000);
+        Console.WriteLine($"Part 1: {p1}");
+        Console.WriteLine($"Part 2: {p2}");
     }
 
-    static (bool, int) Machine(in List<Instruction> instructions)
+    static HashSet<Junction> Find(
+    Dictionary<Junction, HashSet<Junction>> graph,
+    Junction node,
+    HashSet<Junction> visited
+)
     {
-        HashSet<int> visited = [];
-        int accumulator = 0;
-
-        int pc = 0;
-        while (pc < instructions.Count)
+        if (!visited.TryGetValue(node, out _))
         {
-            if (visited.Contains(pc)) return (false, accumulator);
-            visited.Add(pc);
-
-            var ins = instructions[pc];
-            switch (ins.Op)
-            {
-                case OpCode.Jmp:
-                    pc += ins.Value;
-                    continue;
-                case OpCode.Acc:
-                    accumulator += ins.Value;
-                    break;
-            }
-            pc += 1;
+            visited.Add(node);
+            foreach (var neighbor in graph[node]) Find(graph, neighbor, visited);
         }
-        return (true, accumulator);
+        return visited;
     }
-
-    static int Part2(List<Instruction> instructions)
+    static (long, long) Solution(ImmutableArray<Junction> junctions, int nConnections)
     {
-        for (int i = 0; i < instructions.Count; i++)
+        List<(Junction, Junction, double)> connections = [];
+
+        for (int i = 0; i < junctions.Length - 1; i += 1)
+            for (int j = i + 1; j < junctions.Length; j += 1)
+                connections.Add((junctions[i], junctions[j], junctions[i].Euclidean(junctions[j])));
+
+        connections.Sort((x, y) => x.Item3.CompareTo(y.Item3));
+
+        long part1 = 0;
+
+        Dictionary<Junction, HashSet<Junction>> graph = [];
+        foreach (var (i, (a, b, _)) in connections.Select((x, index) => (index, x)))
         {
-            var ins = instructions[i];
-            if (ins.Op.Equals(OpCode.Jmp) || ins.Op.Equals(OpCode.Nop))
+            if (!graph.TryGetValue(a, out _)) graph[a] = [];
+            graph[a].Add(b);
+            if (!graph.TryGetValue(b, out _)) graph[b] = [];
+            graph[b].Add(a);
+
+            if (i == nConnections - 1)
             {
-                var newOp = ins.Op.Equals(OpCode.Jmp) ? OpCode.Nop : OpCode.Jmp;
-                instructions[i] = new Instruction(newOp, ins.Value);
-
-                var result = Machine(instructions);
-                if (result.Item1) return result.Item2;
-
-                instructions[i] = ins;
+                List<HashSet<Junction>> counts = [];
+                foreach (var node in graph.Keys)
+                {
+                    var result = Find(graph, node, []);
+                    bool equal = false;
+                    foreach (var set in counts)
+                    {
+                        if (set.SetEquals(result))
+                        {
+                            equal = true;
+                            break;
+                        }
+                    }
+                    if (!equal) counts.Add(result);
+                }
+                List<int> values = [.. counts.Select(x => x.Count)];
+                values.Sort();
+                part1 = values[^3] * values[^2] * values[^1];
             }
-
+            else if (i >= (nConnections * 2) && Find(graph, a, []).Count == nConnections)
+            {
+                return (part1, a.x * b.x);
+            }
         }
-        return 0;
+
+        return (0, 0);
     }
 }
