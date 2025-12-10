060c8d01176c1840f92dcbc5c9ea85e9739837e1
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-12-10 10:35:07 +0100

    d10 p1

--- 2025/Solutions/Day10.cs
+++ 2025/Solutions/Day10.cs
@@ -1,29 +1,58 @@
+using System.Collections.Immutable;
+
 namespace aoc.Solutions;
 
 public class Day10
 {
+    record Row(ImmutableArray<bool> Indicator, int[][] Buttons, int[] JoltReq);
+    static Row Parse(string row)
+    {
+        var s = row.Split();
+        var ind = s[0][1..^1].Select(x => x == '#').ToImmutableArray();
+        var wiring = s[1..^1].Select(x => x[1..^1].Split(",").Select(int.Parse).ToArray()).ToArray();
+        var joltreq = s[^1][1..^1].Split(",").Select(int.Parse).ToArray();
+        return new Row(ind, wiring, joltreq);
+    }
     public static void Solve()
     {
-        List<int> list = [.. File.ReadAllLines("in/d10.txt").Select(int.Parse)];
-        list.Sort();
+        Row[] list = [.. File.ReadAllLines("in/d10.txt").Select(Parse)];
 
         Console.WriteLine($"Part 1: {Part1(list)}");
-        Console.WriteLine($"Part 2: {Part2(list, 0, 0, [])}");
+        // Console.WriteLine($"Part 2: {Part2(list, 0, 0, [])}");
     }
 
-    static int Part1(in List<int> list)
+    // record State(bool[] lights)
+    static int Part1Solve(ImmutableArray<bool> target, int[][] buttons)
     {
-        int jolt = 0;
-        int diff1 = 0;
-        int diff3 = 1;
-        foreach (var nextAdapter in list)
+        bool[][] states = [new bool[target.Length]];
+        int generation = 0;
+        while (true)
         {
-            var diff = nextAdapter - jolt;
-            if (diff == 1) diff1 += 1;
-            else if (diff == 3) diff3 += 1;
-            jolt = nextAdapter;
+            List<bool[]> next_states = [];
+            generation += 1;
+            foreach (var state in states)
+            {
+                foreach (var presses in buttons)
+                {
+                    bool[] new_state = [.. state];
+                    foreach (var i in presses)
+                    {
+                        new_state[i] = !new_state[i];
+                    }
+                    if (new_state.SequenceEqual(target))
+                    {
+                        return generation;
+                    }
+                    next_states.Add(new_state);
+                }
+            }
+            states = [.. next_states];
         }
-        return diff1 * diff3;
+    }
+
+    static int Part1(Row[] list)
+    {
+        return list.Aggregate(0, (sum, elem) => sum + Part1Solve(elem.Indicator, elem.Buttons));
     }
 
     static long Part2(in List<int> list, int jolt, int index, Dictionary<(int, int), long> memo)
