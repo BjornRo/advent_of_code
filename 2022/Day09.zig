b1d7abe2e4b31f4a69ecbcd6e148ab6237239dc2
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-12-25 12:48:31 +0100

    d9 cont

--- 2022/Day09.zig
+++ 2022/Day09.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 const utils = @import("utils.zig");
 const Allocator = std.mem.Allocator;
 
+const Set = std.AutoHashMap(struct { isize, isize }, void);
+
 pub fn main() !void {
     var da = std.heap.DebugAllocator(.{}).init;
     const alloc = da.allocator();
@@ -22,11 +24,86 @@ fn delta(hrow: isize, hcol: isize, trow: isize, tcol: isize) isize {
 fn valid(hrow: isize, hcol: isize, trow: isize, tcol: isize) bool {
     return delta(hrow, hcol, trow, tcol) <= 1;
 }
+const Node = struct {
+    row: isize = 0,
+    col: isize = 0,
+    parent: ?*Self,
+    child: ?*Self,
+
+    const Self = @This();
+    fn init(alloc: Allocator, parent: ?*Self, child: ?*Self) !*Self {
+        var new = try alloc.create(Self);
+        new.parent = parent;
+        new.child = child;
+        return new;
+    }
+    fn right(self: *Self, visited: *Set) !void {
+        if (self.parent) |p| {
+            if (!p.valid(self)) {
+                self.col = p.col - 1;
+                self.row = p.row;
+            }
+            try visited.put(.{ self.row, self.col }, {});
+        }
+        self.col += 1;
+        if (self.child) |c| c.right(visited);
+    }
+    fn left(self: *Self, visited: *Set) !void {
+        if (self.parent) |p| {
+            if (!p.valid(self)) {
+                self.col = p.col + 1;
+                self.row = p.row;
+            }
+            try visited.put(.{ self.row, self.col }, {});
+        }
+        self.col -= 1;
+        if (self.child) |c| c.left(visited);
+    }
+    fn up(self: *Self, visited: *Set) !void {
+        if (self.parent) |p| {
+            if (!p.valid(self)) {
+                self.row = p.row + 1;
+                self.col = p.col;
+            }
+            try visited.put(.{ self.row, self.col }, {});
+        }
+        self.row -= 1;
+        if (self.child) |c| c.up(visited);
+    }
+    fn down(self: *Self, visited: *Set) !void {
+        if (self.parent) |p| {
+            if (!p.valid(self)) {
+                self.row = p.row - 1;
+                self.col = p.col;
+            }
+            try visited.put(.{ self.row, self.col }, {});
+        }
+        self.row += 1;
+        if (self.child) |c| c.up(visited);
+    }
+    fn valid(self: Self, child: *Self) bool {
+        return delta(self.row, self.col, child.row, child.col) <= 1;
+    }
+    fn deinit(self: *Self, alloc: Allocator) void {
+        if (self.child) |c| c.deinit();
+        alloc.destroy(self);
+    }
+};
 fn solve(alloc: Allocator, data: []const u8) !struct { p1: usize, p2: usize } {
     var splitIter = std.mem.splitScalar(u8, data, '\n');
 
-    var visited = std.AutoHashMap(struct { isize, isize }, void).init(alloc);
+    var visited = Set.init(alloc);
     defer visited.deinit();
+
+    const head = blk: {
+        const head = try Node.init(alloc, null, null);
+        var curr: ?*Node = head;
+        for (0..2) |_| {
+            const next = try Node.init(alloc, curr, null);
+            curr = next;
+        }
+        break :blk head;
+    };
 
     var head_row: isize = 0;
     var head_col: isize = 0;
