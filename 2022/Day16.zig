b01cb56ac144d58736aab6f083f7fef252072c34
BjornRo <54636059+BjornRo@users.noreply.github.com>
2025-12-27 14:36:02 +0100

    d16 cleanup

--- 2022/Day16.zig
+++ 2022/Day16.zig
@@ -2,18 +2,19 @@ const std = @import("std");
 const utils = @import("utils.zig");
 const Allocator = std.mem.Allocator;
 
-const Key = struct { u8, u8, u64 };
+const Map = std.HashMap(Entry, u12, HashCtx, 90);
+const BitSet = std.bit_set.IntegerBitSet(50);
+const Entry = packed struct { node: u6, minutes: u6, valves: u50, __padding: u2 = 0 };
 const HashCtx = struct {
-    pub fn hash(_: @This(), key: Key) u64 {
-        return utils.hashU64((@as(u64, @intCast(key.@"0")) << 58) | (@as(u64, @intCast(key.@"1")) << 52) | key.@"2");
+    pub fn hash(_: @This(), key: Entry) u64 {
+        return utils.hashU64(@bitCast(key));
     }
-    pub fn eql(_: @This(), a: Key, b: Key) bool {
-        return a.@"0" == b.@"0" and a.@"1" == b.@"1" and a.@"2" == b.@"2";
+    pub fn eql(_: @This(), a: Entry, b: Entry) bool {
+        return a.node == b.node and a.minutes == b.minutes and a.valves == b.valves;
     }
 };
-const Map = std.HashMap(Key, u32, HashCtx, 90);
-const BitSet = std.bit_set.IntegerBitSet(64);
-const Graph = std.AutoHashMap(u8, struct { flow: u32, valves: BitSet });
+
+const GraphValue = struct { flow: u12, valves: BitSet };
 pub fn main() !void {
     var da = std.heap.DebugAllocator(.{}).init;
     const alloc = da.allocator();
@@ -27,56 +28,57 @@ pub fn main() !void {
     std.debug.print("Part 2: {d}\n", .{result.p2});
 }
 fn solve(alloc: Allocator, data: []const u8) !struct { p1: usize, p2: usize } {
-    var map: std.AutoHashMap([2]u8, u8) = .init(alloc);
-    var graph: Graph = .init(alloc);
-    defer map.deinit();
-    defer graph.deinit();
-    {
-        var parsing_step: std.ArrayList(struct { key: [2]u8, flow: u32, neighbors: []const u8 }) = .empty;
+    const start, const graph = blk: {
+        var map: std.AutoHashMap([2]u8, u6) = .init(alloc);
+        defer map.deinit();
+        var parsing_step: std.ArrayList(struct { key: [2]u8, flow: u12, neighbors: []const u8 }) = .empty;
         defer parsing_step.deinit(alloc);
-        var k: u8 = 0;
-        var split_iter = std.mem.splitScalar(u8, data, '\n');
-        while (split_iter.next()) |*e| : (k += 1) {
-            const i = if (std.mem.lastIndexOf(u8, e.*, "valve ")) |i| i else std.mem.lastIndexOf(u8, e.*, "valves ").? + 1;
-            const key = e.*[6..8][0..2].*;
-            try parsing_step.append(alloc, .{ .key = key, .flow = utils.firstNumber(u32, 10, e.*).?.value, .neighbors = e.*[i + 6 ..] });
-            try map.put(key, k);
+        {
+            var k: u6 = 0;
+            var split_iter = std.mem.splitScalar(u8, data, '\n');
+            while (split_iter.next()) |*e| : (k += 1) {
+                const i = if (std.mem.lastIndexOf(u8, e.*, "valve ")) |i| i else std.mem.lastIndexOf(u8, e.*, "valves ").? + 1;
+                const key = e.*[6..8][0..2].*;
+                try parsing_step.append(alloc, .{ .key = key, .flow = utils.firstNumber(u12, 10, e.*).?.value, .neighbors = e.*[i + 6 ..] });
+                try map.put(key, k);
+            }
         }
+        var graph = try alloc.alloc(GraphValue, parsing_step.items.len);
         for (parsing_step.items) |item| {
             var neighbors: BitSet = .initEmpty();
             var iter = std.mem.splitSequence(u8, item.neighbors, ", ");
             while (iter.next()) |*n| neighbors.set(map.get(n.*[0..2].*).?);
-            try graph.put(map.get(item.key).?, .{ .flow = item.flow, .valves = neighbors });
+            graph[map.get(item.key).?] = .{ .flow = item.flow, .valves = neighbors };
         }
-    }
-    const start = map.get(.{ 'A', 'A' }).?;
+        break :blk .{ map.get(.{ 'A', 'A' }).?, graph };
+    };
+    defer alloc.free(graph);
     var memo = Map.init(alloc);
     defer memo.deinit();
-    return .{ .p1 = try valver(&graph, start, 30, .initEmpty(), &memo), .p2 = try valver2(alloc, &graph, start) };
+    return .{ .p1 = try valver(graph, start, 30, .initEmpty(), &memo), .p2 = try valver2(alloc, graph, start) };
 }
-fn valver(graph: *Graph, node: u8, minutes: u8, valves: BitSet, memo: *Map) !u32 {
+fn valver(graph: []GraphValue, node: u6, minutes: u6, valves: BitSet, memo: *Map) !u12 {
     if (minutes <= 0) return 0;
-    const key: Key = .{ node, minutes, valves.mask };
+    const key: Entry = .{ .node = node, .minutes = minutes, .valves = @truncate(valves.mask) };
     if (memo.get(key)) |val| return val;
 
-    var max_pressure: u32 = 0;
-    const value = graph.get(node).?;
-    if (value.flow > 0 and !valves.isSet(node)) {
+    var max_pressure: u12 = 0;
+    if (graph[node].flow > 0 and !valves.isSet(node)) {
         var new_bitset = valves;
         new_bitset.set(node);
         max_pressure = @max(max_pressure, try valver(graph, node, minutes - 1, new_bitset, memo));
     }
-    var neighbors_iter = value.valves.iterator(.{});
+    var neighbors_iter = graph[node].valves.iterator(.{});
     while (neighbors_iter.next()) |next_valve|
         max_pressure = @max(max_pressure, try valver(graph, @intCast(next_valve), minutes - 1, valves, memo));
 
     var valve_iter = valves.iterator(.{});
-    while (valve_iter.next()) |i| max_pressure += graph.get(@intCast(i)).?.flow;
+    while (valve_iter.next()) |i| max_pressure += graph[i].flow;
     try memo.put(key, max_pressure);
     return max_pressure;
 }
-fn valver2(alloc: Allocator, graph: *Graph, start: u8) !u32 {
-    const State = struct { u8, u8, BitSet, u32 };
+fn valver2(alloc: Allocator, graph: []GraphValue, start: u6) !u12 {
+    const State = struct { u6, u6, BitSet, u12 };
     var states: std.ArrayList(State) = .empty;
     var next_states: std.ArrayList(State) = .empty;
     var visited: Map = .init(alloc);
@@ -84,7 +86,7 @@ fn valver2(alloc: Allocator, graph: *Graph, start: u8) !u32 {
     defer next_states.deinit(alloc);
     defer visited.deinit();
 
-    var best_pressure: u32 = 0;
+    var best_pressure: u12 = 0;
     try states.append(alloc, .{ start, start, .initEmpty(), 0 });
     for (0..26) |_| {
         defer {
@@ -97,57 +99,50 @@ fn valver2(alloc: Allocator, graph: *Graph, start: u8) !u32 {
             const n1, const n2, const open_valves, const pressure = state;
             if (n1 == start and n2 != start) continue;
             {
-                const vkey: Key = .{ n1, n2, open_valves.mask };
-                const res = try visited.getOrPut(vkey);
-                if (res.found_existing) {
-                    if (res.value_ptr.* >= pressure) continue;
-                    res.value_ptr.* = pressure;
-                } else res.key_ptr.* = vkey;
+                const res = try visited.getOrPut(.{ .node = n1, .minutes = n2, .valves = @truncate(open_valves.mask) });
+                if (res.found_existing and res.value_ptr.* >= pressure) continue;
+                res.value_ptr.* = pressure;
             }
-            var new_pressure: u32 = pressure;
+            var new_pressure: u12 = pressure;
             {
                 var valve_iter = open_valves.iterator(.{});
-                while (valve_iter.next()) |i| new_pressure += graph.get(@intCast(i)).?.flow;
+                while (valve_iter.next()) |i| new_pressure += graph[i].flow;
                 var potential = new_pressure + 10; // Minor tweak here, otherwise testdata fails, +10
-                var giter = graph.iterator();
-                while (giter.next()) |kv| {
-                    if (!open_valves.isSet(kv.key_ptr.*) and kv.value_ptr.flow > 0)
-                        potential += kv.value_ptr.flow;
+                for (0.., graph) |k, v| {
+                    if (!open_valves.isSet(k) and v.flow > 0) potential += v.flow;
                 }
                 if (potential <= best_pressure) continue;
             }
-            const n1_valve = graph.get(n1).?;
-            const n2_valve = graph.get(n2).?;
-            if (n1_valve.flow > 0 and n2_valve.flow > 0 and !open_valves.isSet(n1) and !open_valves.isSet(n2)) {
+            if (graph[n1].flow + graph[n2].flow > 1 and !open_valves.isSet(n1) and !open_valves.isSet(n2)) {
                 var new_bitset = open_valves;
                 new_bitset.set(n1);
                 new_bitset.set(n2);
-                best_pressure = @max(best_pressure, new_pressure + n1_valve.flow + n2_valve.flow);
+                best_pressure = @max(best_pressure, new_pressure + graph[n1].flow + graph[n2].flow);
                 try next_states.append(alloc, .{ n1, n2, new_bitset, new_pressure });
             }
-            if (n1_valve.flow > 0 and !open_valves.isSet(n1)) {
-                var neighbors_iter = n2_valve.valves.iterator(.{});
+            if (graph[n1].flow > 0 and !open_valves.isSet(n1)) {
+                var neighbors_iter = graph[n2].valves.iterator(.{});
                 while (neighbors_iter.next()) |next_n2| {
                     var new_bitset = open_valves;
                     new_bitset.set(n1);
-                    best_pressure = @max(best_pressure, new_pressure + n1_valve.flow);
-                    try next_states.append(alloc, .{ n1, @intCast(next_n2), new_bitset, new_pressure });
+                    best_pressure = @max(best_pressure, new_pressure + graph[n1].flow);
+                    try next_states.append(alloc, .{ n1, @truncate(next_n2), new_bitset, new_pressure });
                 }
             }
-            if (n2_valve.flow > 0 and !open_valves.isSet(n2)) {
-                var neighbors_iter = n1_valve.valves.iterator(.{});
+            if (graph[n2].flow > 0 and !open_valves.isSet(n2)) {
+                var neighbors_iter = graph[n1].valves.iterator(.{});
                 while (neighbors_iter.next()) |next_n1| {
                     var new_bitset = open_valves;
                     new_bitset.set(n2);
-                    best_pressure = @max(best_pressure, new_pressure + n2_valve.flow);
-                    try next_states.append(alloc, .{ @intCast(next_n1), n2, new_bitset, new_pressure });
+                    best_pressure = @max(best_pressure, new_pressure + graph[n2].flow);
+                    try next_states.append(alloc, .{ @truncate(next_n1), n2, new_bitset, new_pressure });
                 }
             }
-            var iter1 = n1_valve.valves.iterator(.{});
+            var iter1 = graph[n1].valves.iterator(.{});
             while (iter1.next()) |next_n1| {
-                var iter2 = n2_valve.valves.iterator(.{});
+                var iter2 = graph[n2].valves.iterator(.{});
                 while (iter2.next()) |next_n2|
-                    try next_states.append(alloc, .{ @intCast(next_n1), @intCast(next_n2), open_valves, new_pressure });
+                    try next_states.append(alloc, .{ @truncate(next_n1), @truncate(next_n2), open_valves, new_pressure });
             }
         }
     }
