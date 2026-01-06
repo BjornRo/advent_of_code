45fe5721040d5861f6ee0442b14f52dc5fa0f115
BjornRo <54636059+BjornRo@users.noreply.github.com>
2026-01-06 02:18:32 +0100

    cleanup

--- 2022/Day10.zig
+++ 2022/Day10.zig
@@ -4,61 +4,51 @@ const Allocator = std.mem.Allocator;
 
 const WIDTH = 40;
 const HEIGHT = 6;
-
+const CRT = [HEIGHT][WIDTH]u8;
+var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
 pub fn main() !void {
-    var da = std.heap.DebugAllocator(.{}).init;
-    const alloc = da.allocator();
-    defer _ = da.deinit();
+    const alloc, const is_debug = switch (@import("builtin").mode) {
+        .Debug => .{ debug_allocator.allocator(), true },
+        else => .{ std.heap.smp_allocator, false },
+    };
+    const start = std.time.microTimestamp();
+    defer {
+        std.debug.print("Time: {any}s\n", .{@as(f64, @floatFromInt(std.time.microTimestamp() - start)) / 1000_000});
+        if (is_debug) _ = debug_allocator.deinit();
+    }
 
     const data = try utils.read(alloc, "in/d10.txt");
     defer alloc.free(data);
 
     const result = try solve(alloc, data);
-    defer alloc.free(result.p2);
     std.debug.print("Part 1: {d}\n", .{result.p1});
     std.debug.print("Part 2:\n", .{});
     for (result.p2) |row| std.debug.print("{s}\n", .{row});
 }
-
-fn solve(alloc: Allocator, data: []const u8) !struct { p1: isize, p2: *[HEIGHT][WIDTH]u8 } {
-    var splitIter = std.mem.splitScalar(u8, data, '\n');
-
+fn solve(alloc: Allocator, data: []const u8) !struct { p1: isize, p2: CRT } {
+    var rows = std.mem.splitScalar(u8, data, '\n');
     var program: std.ArrayList(?isize) = .empty;
     defer program.deinit(alloc);
+    while (rows.next()) |item| try program.append(alloc, if (utils.firstNumber(isize, 0, item)) |v| v.value else null);
 
-    while (splitIter.next()) |item| {
-        var rowIter = std.mem.splitScalar(u8, item, ' ');
-        if (rowIter.next().?[0] == 'n')
-            try program.append(alloc, null)
-        else
-            try program.append(alloc, try std.fmt.parseInt(isize, rowIter.next().?, 10));
-    }
-
-    var part1: [7]isize = .{ -1, 220, 180, 140, 100, 60, 20 };
-    var p1cycles: std.ArrayList(isize) = .fromOwnedSlice(&part1);
-    var crt: std.ArrayList(u8) = try .initCapacity(alloc, WIDTH * HEIGHT);
-    defer crt.deinit(alloc);
+    var __part1: [7]isize = .{ -1, 220, 180, 140, 100, 60, 20 };
+    var p1cycles: std.ArrayList(isize) = .fromOwnedSlice(&__part1);
+    var buf: [WIDTH * HEIGHT]u8 = undefined;
+    var screen: std.ArrayList(u8) = .initBuffer(&buf);
 
     var total_p1: isize = 0;
-
     var cycles: isize = 0;
     var reg: isize = 1;
-    while (cycles < WIDTH * HEIGHT) {
-        for (program.items) |op| {
-            crt.appendAssumeCapacity(if (@abs(@mod(cycles, WIDTH) - reg) <= 1) '#' else ' ');
+    while (cycles < WIDTH * HEIGHT) for (program.items) |op| {
+        screen.appendAssumeCapacity(if (@abs(@mod(cycles, WIDTH) - reg) <= 1) '#' else ' ');
+        cycles += 1;
+        if (cycles == p1cycles.getLast()) total_p1 += reg * p1cycles.pop().?;
+        if (op) |value| {
+            screen.appendAssumeCapacity(if (@abs(@mod(cycles, WIDTH) - reg) <= 1) '#' else ' ');
             cycles += 1;
-            if (cycles == p1cycles.items[p1cycles.items.len - 1]) {
-                if (p1cycles.pop()) |res| total_p1 += reg * res;
-            }
-            if (op) |value| {
-                crt.appendAssumeCapacity(if (@abs(@mod(cycles, WIDTH) - reg) <= 1) '#' else ' ');
-                cycles += 1;
-                if (cycles == p1cycles.items[p1cycles.items.len - 1]) {
-                    if (p1cycles.pop()) |res| total_p1 += reg * res;
-                }
-                reg += value;
-            }
+            if (cycles == p1cycles.getLast()) total_p1 += reg * p1cycles.pop().?;
+            reg += value;
         }
-    }
-    return .{ .p1 = total_p1, .p2 = @as(*[HEIGHT][WIDTH]u8, @ptrCast(try crt.toOwnedSlice(alloc))) };
+    };
+    return .{ .p1 = total_p1, .p2 = @as(CRT, @bitCast(buf)) };
 }
